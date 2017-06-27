4004facf5c5ed5f599697d7530384d15c4a7790f
Patrick Kilgore <pckilgore@users.noreply.github.com>
2017-06-27 14:49:03 -0500

    Basic refactor for consistent docs, check-in with @pipermerriam on approach.

---
+++ evm/__version__.py
@@ -0,0 +1,15 @@
+'''
+   This is going to DRY up the docs and setup.
+'''
+
+__title__ = 'py-evm'
+__description__ = 'Python implementation of the Ethereum Virtual Machine'
+__url__ = 'Piper Merriam'
+__version__ = "0.1.0"
+__author__ = 'Piper Merriam'
+__author_email__ = 'pipermerriam@gmail.com'
+__license__ = 'MIT'
+
+# Manually bump for now...
+# __build__ = '[TODO] STUB'
+__copyright__ = 'Copyright 2017 ' + __author__
