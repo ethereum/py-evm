73e71ece15dd3bf1391923e22b189c11fad9f8ce
eric gong <egong82@gmail.com>
2017-08-01 21:35:59 -0400

    added create message factory as commented

--- tests/core/evm/vm/test_message.py
+++ tests/core/evm/vm/test_message.py
@@ -1,32 +1,21 @@
+from __future__ import unicode_literals
 import pytest
 import evm.vm.message as m
 
-message_object = m.Message(
-    1,                      #gas
-    1,                      #gas_price
-    b'',                    #to
-    '11111111111111111111', #sender
-    1,                      #value
-    '',                     #data
-    'asdf',                 #code
-    '22222222222222222222'  #origin
-    )                    
 
-def test_initilizes_properly():
-    # all __init__ parameters are properly validated.
-    assert message_object.gas == 1
-    assert message_object.gas_price == 1
-    assert message_object.to == b''
-    assert message_object.sender == '11111111111111111111'
-    assert message_object.value == 1
-    assert message_object.data == ''
-    assert message_object.code == 'asdf'
-    assert message_object.origin == '22222222222222222222'
+def _create_message(gas=1, gas_price=1, to=b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xff", sender=b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xff", value=1, data=b"", code=b"", origin="\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xff"):
+    return m.Message(gas=gas, gas_price=gas_price, to=to, sender=sender, value=value, data=data, code=code, origin=origin)
 
-def test_is_origin():
-    #correctly returns True/False for whether this message is the origin message
-    assert message_object.is_origin == False
+# def test_initializes_properly():
+#     """
+#     # all __init__ parameters are properly validated.
+#     """
+#     assert 
 
-def origin():
-    #property returns correct value.
-    assert message_object.origin == '22222222222222222222'
+# def test_is_origin():
+#     #correctly returns True/False for whether this message is the origin message
+#     assert _create_message().is_origin == True
+
+# def test_origin():
+#     #property returns correct value.
+#     assert _create_message().origin == "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xff"
