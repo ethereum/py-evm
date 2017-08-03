4ccf91dc0c26fd6118b3e4cf94e07fcf538cdf66
njgheorghita <nickgheorghita@gmail.com>
2017-08-03 11:42:38 -0600

    Refactor unit tests

--- tests/core/memory/test_memory.py
+++ tests/core/memory/test_memory.py
@@ -20,96 +20,54 @@ def memory32():
     return memory
 
 
-@pytest.mark.parametrize(
-    "start_position,size,value,is_valid",
-    (
-        (0, 4, b'1010', True),
-        ('a', 4, b'1010', False),
-        (0, 'a', b'1010', False),
-        (-1, 4, b'1010', False),
-        (0, -1, b'1010', False),
-        (2**256, 4, b'1010', False),
-        (0, 2**256, b'1010', False),
-    )
-)
-def test_start_position_and_size_valid_on_write(memory32, start_position, size, value, is_valid):
-    if is_valid:
+@pytest.mark.parametrize("start_position,size,value", ((0, 4, b'1010'),))
+def test_write(memory32, start_position, size, value):
+    memory32.write(start_position, size, value)
+    assert memory32.bytes == value + bytearray(32 - size)
+
+
+@pytest.mark.parametrize("start_position,size,value", ((-1, 4, b'1010'),))
+def test_write_rejects_invalid_start_position(memory32, start_position, size, value):
+    with pytest.raises(ValidationError):
         memory32.write(start_position, size, value)
-        assert memory32.bytes == value + bytearray(b'\x00\x00') * 14
-    else:
-        with pytest.raises(ValidationError):
-            memory32.write(start_position, size, value)
 
 
-@pytest.mark.parametrize(
-    "start_position,size,value,is_valid",
-    (
-        (0, 4, b'1010', True),
-        (0, 4, '1010', False),
-        (0, 4, 1234, False),
-        (0, 4, True, False),
-    )
-)
-def test_value_is_bytes_on_write(memory32, start_position, size, value, is_valid):
-    if is_valid:
+@pytest.mark.parametrize("start_position,size,value", ((0, 2**256, b'1010'),))
+def test_write_rejects_invalid_size(memory32, start_position, size, value):
+    with pytest.raises(ValidationError):
         memory32.write(start_position, size, value)
-        assert memory32.bytes == value + bytearray(b'\x00\x00') * 14
-    else:
-        with pytest.raises(ValidationError):
-            memory32.write(start_position, size, value)
 
 
-@pytest.mark.parametrize(
-    "start_position,size,value,is_valid",
-    (
-        (0, 4, b'1010', True),
-        (5, 3, b'1010', False),
-        (10, 5, b'1010', False),
-    )
-)
-def test_value_length_equals_size_on_write(memory32, start_position, size, value, is_valid):
-    if is_valid:
+@pytest.mark.parametrize("start_position,size,value", ((0, 4, '1010'),))
+def test_write_rejects_invalid_value(memory32, start_position, size, value):
+    with pytest.raises(ValidationError):
         memory32.write(start_position, size, value)
-        assert memory32.bytes == value + bytearray(b'\x00\x00') * 14
-    else:
-        with pytest.raises(ValidationError):
-            memory32.write(start_position, size, value)
 
 
-@pytest.mark.parametrize(
-    "start_position,size,value,is_valid",
-    (
-        (0, 4, b'1010', True),
-        (30, 4, b'1010', False),
-    )
-)
-def test_write_cant_extend_beyond_memory_size(memory32, start_position, size, value, is_valid):
-    if is_valid:
+@pytest.mark.parametrize("start_position,size,value", ((0, 5, '1010'),))
+def test_write_rejects_size_not_equal_to_value_length(memory32, start_position, size, value):
+    with pytest.raises(ValidationError):
         memory32.write(start_position, size, value)
-        assert memory32.bytes == value + bytearray(b'\x00\x00') * 14
-    else:
-        with pytest.raises(ValidationError):
-            memory32.write(start_position, size, value)
+
+
+@pytest.mark.parametrize("start_position,size,value", ((30, 4, b'1010'),))
+def test_write_rejects_values_beyond_memory_size(memory32, start_position, size, value):
+    with pytest.raises(ValidationError):
+        memory32.write(start_position, size, value)
 
 
 def test_extend_appropriately_extends_memory(memory):
     memory.extend(0, 32)
-    assert memory.bytes == bytearray(b'\x00\x00') * 16
+    assert memory.bytes == bytearray(32)
     memory.extend(30, 32)
-    assert memory.bytes == bytearray(b'\x00\x00') * 32
+    assert memory.bytes == bytearray(64)
     memory.extend(48, 10)
-    assert memory.bytes == bytearray(b'\x00\x00') * 32
+    assert memory.bytes == bytearray(64)
     memory.extend(60, 16)
-    assert memory.bytes == bytearray(b'\x00\x00') * 48
+    assert memory.bytes == bytearray(96)
 
 
-@pytest.mark.parametrize(
-    "start_position,size,value",
-    (
-        (0, 4, b'1010'),
-        (10, 10, b'1010101010'),
-    )
-)
+@pytest.mark.parametrize("start_position,size,value", ((5, 4, b'1010'),))
 def test_read_returns_correct_bytes_from_memory(memory32, start_position, size, value):
     memory32.write(start_position, size, value)
     assert memory32.read(start_position, size) == value
