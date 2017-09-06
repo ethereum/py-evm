d707fa02bb3e153f601ecaf08122552bc6a12f32
Guilherme Salgado <gsalgado@gmail.com>
2017-09-06 15:39:22 +0100

    New tests for the KademliaProto.wait_* methods

--- evm/p2p/test_kademlia.py
+++ evm/p2p/test_kademlia.py
@@ -16,21 +16,105 @@ def short_timeout(monkeypatch):
 @pytest.mark.asyncio
 def test_protocol_bootstrap():
     proto = get_wired_protocol()
+    node1, node2 = [random_node(), random_node()]
 
     @asyncio.coroutine
     def bond(node):
-        proto.routing.add_node(node)
+        assert proto.routing.add_node(node) is None
         return True
 
+    # Pretend we bonded successfully with our bootstrap nodes.
     proto.bond = bond
-    node1, node2 = [random_node(), random_node()]
+
     yield from proto.bootstrap([node1, node2])
+
     assert len(proto.wire.messages) == 2
     # We don't care in which order the bootstrap nodes are contacted, so we sort them both in the
     # assert here.
     assert sorted(proto.wire.messages) == sorted([
         (node1, 'find_node', proto.routing.this_node.id),
         (node2, 'find_node', proto.routing.this_node.id)])
+
+
+@pytest.mark.asyncio
+def test_wait_ping():
+    proto = get_wired_protocol()
+    node = random_node()
+    echo = "echo"
+
+    # Schedule a call to proto.recv_ping() simulating a ping from the node we expect.
+    recv_ping_coroutine = asyncio.coroutine(lambda: proto.recv_ping(node, echo))
+    asyncio.ensure_future(recv_ping_coroutine())
+
+    got_ping = yield from proto.wait_ping(node)
+
+    assert got_ping
+    # Ensure wait_ping() cleaned up after itself.
+    assert node not in proto.ping_callbacks
+
+    # If we waited for a ping from a different node, wait_ping() would timeout and thus return
+    # false.
+    recv_ping_coroutine = asyncio.coroutine(lambda: proto.recv_ping(node, echo))
+    asyncio.ensure_future(recv_ping_coroutine())
+
+    node2 = random_node()
+    got_ping = yield from proto.wait_ping(node2)
+
+    assert not got_ping
+    assert node2 not in proto.ping_callbacks
+
+
+@pytest.mark.asyncio
+def test_wait_pong():
+    proto = get_wired_protocol()
+    node = random_node()
+    echoed = "echoed"
+    pingid = proto._mkpingid(echoed, node)
+
+    # Schedule a call to proto.recv_pong() simulating a pong from the node we expect.
+    recv_pong_coroutine = asyncio.coroutine(lambda: proto.recv_pong(node, echoed))
+    asyncio.ensure_future(recv_pong_coroutine())
+
+    got_pong = yield from proto.wait_pong(pingid)
+
+    assert got_pong
+    # Ensure wait_pong() cleaned up after itself.
+    assert pingid not in proto.pong_callbacks
+
+    # If the remote node echoed something different than what we expected, wait_pong() would
+    # timeout.
+    wrong_echo = "foo"
+    recv_pong_coroutine = asyncio.coroutine(lambda: proto.recv_pong(node, wrong_echo))
+    asyncio.ensure_future(recv_pong_coroutine())
+
+    got_pong = yield from proto.wait_pong(pingid)
+
+    assert not got_pong
+    assert pingid not in proto.pong_callbacks
+
+
+@pytest.mark.asyncio
+def test_wait_neighbours():
+    proto = get_wired_protocol()
+    node = random_node()
+
+    # Schedule a call to proto.recv_neighbours() simulating a neighbours response from the node we
+    # expect.
+    neighbours = [random_node(), random_node(), random_node()]
+    recv_neighbours_coroutine = asyncio.coroutine(lambda: proto.recv_neighbours(node, neighbours))
+    asyncio.ensure_future(recv_neighbours_coroutine())
+
+    received_neighbours = yield from proto.wait_neighbours(node)
+
+    assert neighbours == received_neighbours
+    # Ensure wait_neighbours() cleaned up after itself.
+    assert node not in proto.neighbours_callbacks
+
+    # If wait_neighbours() times out, we get an empty list of neighbours.
+    received_neighbours = yield from proto.wait_neighbours(node)
+
+    assert received_neighbours == []
+    assert node not in proto.neighbours_callbacks
 
 
 def test_routingtable_split_bucket():
