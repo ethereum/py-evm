f6f6267c8dfa11496e9e896f90f5b36dccd6c9dd
Piper Merriam <pipermerriam@gmail.com>
2017-09-28 15:07:10 -0600

    0.2.0-alpha.2 Release

--- evm/__version__.py
+++ evm/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'py-evm'
 __description__ = 'Python implementation of the Ethereum Virtual Machine'
 __url__ = 'https://github.com/pipermerriam/py-evm'
-__version__ = "0.2.0-alpha.1"
+__version__ = "0.2.0-alpha.2"
 __author__ = 'Piper Merriam'
 __author_email__ = 'pipermerriam@gmail.com'
 __license__ = 'MIT'
