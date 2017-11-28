ebe6c0c3188b66d2ff57871767b85f9a85b3bcd1
Piper Merriam <pipermerriam@gmail.com>
2017-11-28 11:06:57 -0700

    Misc cleanup

--- evm/__version__.py
+++
@@ -1,9 +0,0 @@
-__title__ = 'py-evm'
-__description__ = 'Python implementation of the Ethereum Virtual Machine'
-__url__ = 'https://github.com/pipermerriam/py-evm'
-__version__ = "0.2.0-alpha.6"
-__author__ = 'Piper Merriam'
-__author_email__ = 'pipermerriam@gmail.com'
-__license__ = 'MIT'
-__copyright__ = 'Copyright 2017 %s' % __author__
-__build__ = '[TODO] STUB'
