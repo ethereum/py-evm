bbb0607679b06a6abc13c6e578c634ed12600014
Jannik Luhn <jannik.luhn@brainbot.com>
2017-12-07 14:16:27 +0100

    Various minor improvements

--- evm/utils/state_access_restriction.py
+++ evm/utils/state_access_restriction.py
@@ -1,3 +1,7 @@
+from eth_utils import (
+    to_set,
+)
+
 from evm.constants import (
     NONCE_TRIE_PREFIX,
     BALANCE_TRIE_PREFIX,
@@ -19,18 +23,17 @@ def is_accessible(address, slot_as_key, access_prefix_list):
     return False
 
 
+@to_set
 def to_prefix_list_form(access_list):
     """Expand an access list to a flat list of storage key prefixes.
 
     As input a list of the form `[[address, prefix1, prefix2, ...], ...]` is expected.
     """
-    prefix_list = []
     for obj in access_list:
-        address, storage_prefixes = obj[0], obj[1:]
-        prefix_list.append(keccak(address) + NONCE_TRIE_PREFIX)
-        prefix_list.append(keccak(address) + BALANCE_TRIE_PREFIX)
-        prefix_list.append(keccak(address) + CODE_TRIE_PREFIX)
+        address, *storage_prefixes = obj
+        yield keccak(address) + NONCE_TRIE_PREFIX
+        yield keccak(address) + BALANCE_TRIE_PREFIX
+        yield keccak(address) + CODE_TRIE_PREFIX
         for prefix in storage_prefixes:
-            prefix_list.append(keccak(address) + STORAGE_TRIE_PREFIX + prefix)
-    # TODO: remove duplicates/redundancies?
-    return prefix_list
+            yield keccak(address) + STORAGE_TRIE_PREFIX + prefix
+    # TODO: remove redundant prefixes?
