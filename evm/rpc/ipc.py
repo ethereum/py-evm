8b7ba7e6c34cb72df26f5c7d763982db80af47d6
Jason Carver <ut96caarrs@snkmail.com>
2017-12-11 13:47:42 -0800

    ipc server: strip non-json prefix

--- evm/rpc/ipc.py
+++ evm/rpc/ipc.py
@@ -37,23 +37,47 @@ async def connection_handler(execute_rpc, reader, writer):
 
         raw_request += request_bytes.decode()
 
+        bad_prefix, raw_request = strip_non_json_prefix(raw_request)
+        if bad_prefix:
+            writer.write(error_message('Cannot parse json: ' + bad_prefix))
+            await writer.drain()
+
         try:
             request = json.loads(raw_request)
         except json.JSONDecodeError:
             # invalid json request, keep reading data until a valid json is formed
             continue
 
-        if request:
-            result = execute_rpc(request)
-            writer.write(result.encode())
-        else:
-            break
+        # reset the buffer for the next message
+        raw_request = ''
+
+        if not request:
+            continue
 
         try:
-            raw_request = ''
+            result = execute_rpc(request)
+        except Exception as e:
+            writer.write(error_message("unknown failure: " + str(e)))
+        else:
+            writer.write(result.encode())
+
+        try:
             await writer.drain()
         except ConnectionResetError:
             break
+
+
+def strip_non_json_prefix(raw_request):
+    if raw_request and raw_request[0] != '{':
+        prefix, bracket, rest = raw_request.partition('{')
+        return prefix.strip(), bracket + rest
+    else:
+        return '', raw_request
+
+
+def error_message(message):
+    json_error = '{"error": "%s"}\n' % message
+    return json_error.encode()
 
 
 def start(path, chain):
