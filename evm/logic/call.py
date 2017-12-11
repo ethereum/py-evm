8bf88a6daa542548c819386fd6029780d85eff2b
Jannik Luhn <jannik.luhn@brainbot.com>
2017-12-11 11:14:52 +0100

    Use single access list instead of read+write lists

--- evm/logic/call.py
+++ evm/logic/call.py
@@ -60,7 +60,7 @@ class BaseCall(Opcode):
         # Pre-call checks
         _state_db = computation.vm_state.state_db(
             read_only=True,
-            read_and_write_list=computation.msg.read_and_write_list,
+            access_list=computation.msg.access_list,
         )
         with _state_db as state_db:
             sender_balance = state_db.get_balance(computation.msg.storage_address)
@@ -90,7 +90,7 @@ class BaseCall(Opcode):
         else:
             _state_db = computation.vm_state.state_db(
                 read_only=True,
-                read_and_write_list=computation.msg.read_and_write_list,
+                access_list=computation.msg.access_list,
             )
             with _state_db as state_db:
                 if code_address:
@@ -136,7 +136,7 @@ class Call(BaseCall):
     def compute_msg_extra_gas(self, computation, gas, to, value):
         _state_db = computation.vm_state.state_db(
             read_only=True,
-            read_and_write_list=computation.msg.read_and_write_list,
+            access_list=computation.msg.access_list,
         )
         with _state_db as state_db:
             account_exists = state_db.account_exists(to)
@@ -298,7 +298,7 @@ class CallEIP161(CallEIP150):
     def compute_msg_extra_gas(self, computation, gas, to, value):
         _state_db = computation.vm_state.state_db(
             read_only=True,
-            read_and_write_list=computation.msg.read_and_write_list,
+            access_list=computation.msg.access_list,
         )
         with _state_db as state_db:
             account_is_dead = (
