e864f5f473197ebcba154adf8df0fed1bc66136c
Jannik Luhn <jannik.luhn@brainbot.com>
2017-12-12 17:58:01 +0100

    Add single layer state backend

--- evm/db/state.py
+++ evm/db/state.py
@@ -149,6 +149,9 @@ class TwoLayerBackend:
     def account_exists(self, address):
         return bool(self._trie[address])
 
+    def account_has_code_or_nonce(self, address):
+        return self.get_code_hash(address) != EMPTY_SHA3 or self.get_nonce(address) != 0
+
     def touch_account(self, address):
         account = self._get_account(address)
         self._set_account(address, account)
@@ -167,6 +170,155 @@ class TwoLayerBackend:
 
     def _set_account(self, address, account):
         self._trie[address] = rlp.encode(account, sedes=Account)
+
+
+class OneLayerBackend:
+    def __init__(self, db, root_hash=BLANK_ROOT_HASH, access_list=None):
+        self._trie = HexaryTrie(db, root_hash)
+        self.is_access_restricted = access_list is not None
+        self.access_list = access_list
+
+    def decommission(self):
+        self._trie = None
+
+    @property
+    def root_hash(self):
+        return self._trie.root_hash
+
+    @root_hash.setter
+    def root_hash(self, value):
+        self._trie.root_hash = value
+
+    @staticmethod
+    def storage_key(address, slot):
+        return keccak(address) + STORAGE_TRIE_PREFIX + pad32(int_to_big_endian(slot))
+
+    @staticmethod
+    def full_storage_key(address):
+        return keccak(address) + STORAGE_TRIE_PREFIX
+
+    @staticmethod
+    def balance_key(address):
+        return keccak(address) + BALANCE_TRIE_PREFIX
+
+    @staticmethod
+    def nonce_key(address):
+        return keccak(address) + NONCE_TRIE_PREFIX
+
+    @staticmethod
+    def code_key(address):
+        return keccak(address) + CODE_TRIE_PREFIX
+
+    def _check_accessibility(self, key):
+        if self.is_access_restricted:
+            if not is_accessible(key, self.access_list):
+                raise UnannouncedStateAccess("Attempted state access outside of access set")
+
+    def set_storage(self, address, slot, value):
+        key = self.storage_key(address, slot)
+        self._check_accessibility(key)
+
+        if value:
+            encoded_value = rlp.encode(value)
+            self._trie[key] = encoded_value
+        else:
+            del self._trie[key]
+
+    def get_storage(self, address, slot):
+        key = self.storage_key(address, slot)
+        self._check_accessibility(key)
+
+        if key in self._trie:
+            encoded_value = self._trie[key]
+            return rlp.decode(encoded_value, sedes=rlp.sedes.big_endian_int)
+        else:
+            return 0
+
+    def delete_storage(self, address):
+        key = self.full_storage_key(address)
+        self._check_accessibility(key)
+
+        self._trie.delete_branch(key)
+
+    def set_balance(self, address, balance):
+        key = self.balance_key(address)
+        self._check_accessibility(key)
+
+        self._trie[key] = rlp.encode(balance)
+
+    def get_balance(self, address):
+        key = self.balance_key(address)
+        self._check_accessibility(key)
+
+        if key in self._trie:
+            encoded_value = self._trie[key]
+            return rlp.decode(encoded_value, sedes=rlp.sedes.big_endian_int)
+        else:
+            return 0
+
+    def set_nonce(self, address, nonce):
+        key = self.nonce_key(address)
+        self._check_accessibility(key)
+
+        self._trie[key] = rlp.encode(nonce)
+
+    def get_nonce(self, address):
+        key = self.nonce_key(address)
+        self._check_accessibility(key)
+
+        if key in self._trie:
+            encoded_value = self._trie[key]
+            return rlp.decode(encoded_value, sedes=rlp.sedes.big_endian_int)
+        else:
+            return 0
+
+    def set_code(self, address, code):
+        key = self.code_key(address)
+        self._check_accessibility(key)
+
+        self._trie[key] = rlp.encode(code)
+
+    def get_code(self, address):
+        key = self.code_key(address)
+        self._check_accessibility(key)
+
+        if key in self._trie:
+            encoded_value = self._trie[key]
+            return rlp.decode(encoded_value)
+        else:
+            return b''
+
+    def delete_code(self, address):
+        key = self.code_key(address)
+        self._check_accessibility(key)
+
+        del self._trie[key]
+
+    #
+    # Account Methods
+    #
+    def delete_account(self, address):
+        keys = [
+            self.nonce_key(address),
+            self.balance_key(address),
+            self.code_key(address)
+        ]
+        for key in keys:
+            self._check_accessibility(key)
+            del self._trie[key]
+        self.delete_storage(address)
+
+    def account_exists(self, address):
+        return self._trie.branch_exists(address)
+
+    def account_has_code_or_nonce(self, address):
+        return self.get_code(address) != b'' or self.get_nonce(address) != 0
+
+    def touch_account(self, address):
+        if not self.account_exists(address):
+            self.set_nonce(address, 0)
+            self.set_balance(address, 0)
+            self.set_code(address, b'')
 
 
 class AccountStateDB:
@@ -252,10 +404,6 @@ class AccountStateDB:
         validate_canonical_address(address, title="Storage Address")
         return self.backend.get_code(address)
 
-    def get_code_hash(self, address):
-        validate_canonical_address(address, title="Storage Address")
-        return self.backend.get_code_hash(address)
-
     def delete_code(self, address):
         validate_canonical_address(address, title="Storage Address")
         self.backend.delete_code(address)
@@ -272,10 +420,11 @@ class AccountStateDB:
         return self.backend.account_exists(address)
 
     def account_has_code_or_nonce(self, address):
-        return self.get_code_hash(address) != EMPTY_SHA3 or self.get_nonce(address) != 0
+        validate_canonical_address(address, title="Storage Address")
+        return self.backend.account_has_code_or_nonce(address)
 
     def account_is_empty(self, address):
-        return not self.account_has_code_or_nonce(address) and self.get_balance(address) != 0
+        return not self.account_has_code_or_nonce(address) and self.get_balance(address) == 0
 
     def touch_account(self, address):
         validate_canonical_address(address, title="Storage Address")
