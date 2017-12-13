90b62b70f1184e1dbb9f1664bf0d48de62254e98
Jannik Luhn <jannik.luhn@brainbot.com>
2017-12-13 11:06:56 +0100

    Fix account existence check

--- evm/vm_state.py
+++ evm/vm_state.py
@@ -91,6 +91,7 @@ class BaseVMState(Configurable):
             read_only,
             access_list=access_list
         )
+        yield state
 
         if read_only:
             # This acts as a secondary check that no mutation took place for
@@ -99,8 +100,8 @@ class BaseVMState(Configurable):
         elif self.state_root != state.root_hash:
             self.set_state_root(state.root_hash)
 
-        self.access_logs.reads.update(state.db.access_logs.reads)
-        self.access_logs.writes.update(state.db.access_logs.writes)
+            self.access_logs.reads.update(state.db.access_logs.reads)
+            self.access_logs.writes.update(state.db.access_logs.writes)
 
         # remove the reference to the underlying `db` object to ensure that no
         # further modifications can occur using the `State` object after
