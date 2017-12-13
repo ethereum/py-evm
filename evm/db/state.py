286689e477b7966631f0d31cc401a6dc44099463
Jannik Luhn <jannik.luhn@brainbot.com>
2017-12-13 11:08:41 +0100

    Simplify state encoding
    Encode integers as big endian and code as bytes, instead both as RLP

--- evm/db/state.py
+++ evm/db/state.py
@@ -39,6 +39,7 @@ from evm.utils.state_access_restriction import (
     is_accessible,
 )
 from evm.utils.numeric import (
+    big_endian_to_int,
     int_to_big_endian,
 )
 from evm.utils.padding import (
@@ -169,6 +170,151 @@ class TwoLayerBackend:
 
     def _set_account(self, address, account):
         self._trie[address] = rlp.encode(account, sedes=Account)
+
+
+class OneLayerBackend:
+    def __init__(self, db, root_hash=BLANK_ROOT_HASH, access_list=None):
+        self._trie = HexaryTrie(db, root_hash)
+        self.is_access_restricted = access_list is not None
+        self.access_list = access_list
+
+    def decommission(self):
+        self._trie = None
+
+    @property
+    def root_hash(self):
+        return self._trie.root_hash
+
+    @root_hash.setter
+    def root_hash(self, value):
+        self._trie.root_hash = value
+
+    @staticmethod
+    def storage_key(address, slot):
+        return keccak(address) + STORAGE_TRIE_PREFIX + pad32(int_to_big_endian(slot))
+
+    @staticmethod
+    def full_storage_key(address):
+        return keccak(address) + STORAGE_TRIE_PREFIX
+
+    @staticmethod
+    def balance_key(address):
+        return keccak(address) + BALANCE_TRIE_PREFIX
+
+    @staticmethod
+    def nonce_key(address):
+        return keccak(address) + NONCE_TRIE_PREFIX
+
+    @staticmethod
+    def code_key(address):
+        return keccak(address) + CODE_TRIE_PREFIX
+
+    def _check_accessibility(self, key):
+        if self.is_access_restricted:
+            if not is_accessible(key, self.access_list):
+                raise UnannouncedStateAccess("Attempted state access outside of access set")
+
+    def set_storage(self, address, slot, value):
+        key = self.storage_key(address, slot)
+        self._check_accessibility(key)
+
+        if value:
+            self._trie[key] = int_to_big_endian(value)
+        else:
+            del self._trie[key]
+
+    def get_storage(self, address, slot):
+        key = self.storage_key(address, slot)
+        self._check_accessibility(key)
+
+        if key in self._trie:
+            return big_endian_to_int(self._trie[key])
+        else:
+            return 0
+
+    def delete_storage(self, address):
+        key = self.full_storage_key(address)
+        self._check_accessibility(key)
+
+        self._trie.delete_branch(key)
+
+    def set_balance(self, address, balance):
+        key = self.balance_key(address)
+        self._check_accessibility(key)
+
+        self._trie[key] = int_to_big_endian(balance)
+
+    def get_balance(self, address):
+        key = self.balance_key(address)
+        self._check_accessibility(key)
+
+        if key in self._trie:
+            return big_endian_to_int(self._trie[key])
+        else:
+            return 0
+
+    def set_nonce(self, address, nonce):
+        key = self.nonce_key(address)
+        self._check_accessibility(key)
+
+        self._trie[key] = int_to_big_endian(nonce)
+
+    def get_nonce(self, address):
+        key = self.nonce_key(address)
+        self._check_accessibility(key)
+
+        if key in self._trie:
+            return big_endian_to_int(self._trie[key])
+        else:
+            return 0
+
+    def set_code(self, address, code):
+        key = self.code_key(address)
+        self._check_accessibility(key)
+
+        self._trie[key] = code
+
+    def get_code(self, address):
+        key = self.code_key(address)
+        self._check_accessibility(key)
+
+        if key in self._trie:
+            return self._trie[key]
+        else:
+            return b''
+
+    def delete_code(self, address):
+        key = self.code_key(address)
+        self._check_accessibility(key)
+
+        del self._trie[key]
+
+    #
+    # Account Methods
+    #
+    def delete_account(self, address):
+        keys = [
+            self.nonce_key(address),
+            self.balance_key(address),
+            self.code_key(address)
+        ]
+        for key in keys:
+            self._check_accessibility(key)
+            del self._trie[key]
+        self.delete_storage(address)
+
+    def account_exists(self, address):
+        key = keccak(address)
+        return self._trie.branch_exists(key)
+
+    def account_has_code_or_nonce(self, address):
+        return self.get_code(address) != b'' or self.get_nonce(address) != 0
+
+    def touch_account(self, address):
+        if not self.account_exists(address):
+            self.set_nonce(address, 0)
+            self.set_balance(address, 0)
+            self.set_code(address, b'')
 
 
 class AccountStateDB:
