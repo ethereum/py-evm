70a2f59ba60fd8262e41f2b6aef978e53c433396
Adhityaa <c.adhityaa@gmail.com>
2017-12-13 22:55:55 +0530

    Remove redundant prefixes in prefix list form
    This operates in a O(n*logn) time complexity because of the sort.
    
    Closes https://github.com/ethereum/py-evm/issues/212

--- evm/utils/state_access_restriction.py
+++ evm/utils/state_access_restriction.py
@@ -24,6 +24,32 @@ def is_accessible(address, slot_as_key, access_prefix_list):
 
 
 @to_set
+def remove_redundant_prefixes(prefix_list):
+    """Given a list of strings, this removes redundant strings that are covered
+    by other strings in the list. For example, given `["eth", "ethereum"]`,
+    this function will return just `["eth"]` as it's sufficient to match all
+    strings that are covered by both `eth` and `ethereum`.
+    """
+    root = {}
+
+    for prefix in sorted(prefix_list, key=lambda prefix: len(prefix)):
+        cur = root
+
+        for i in range(len(prefix)):
+            if None in cur:
+                break
+
+            if prefix[i] not in cur:
+                cur[prefix[i]] = {}
+
+            if i == len(prefix)-1:
+                cur[prefix[i]][None] = {}
+                yield prefix
+
+            cur = cur[prefix[i]]
+
+
+@to_set
 def to_prefix_list_form(access_list):
     """Expand an access list to a flat list of storage key prefixes.
 
@@ -34,6 +60,5 @@ def to_prefix_list_form(access_list):
         yield keccak(address) + NONCE_TRIE_PREFIX
         yield keccak(address) + BALANCE_TRIE_PREFIX
         yield keccak(address) + CODE_TRIE_PREFIX
-        for prefix in storage_prefixes:
+        for prefix in remove_redundant_prefixes(storage_prefixes):
             yield keccak(address) + STORAGE_TRIE_PREFIX + prefix
-    # TODO: remove redundant prefixes?
