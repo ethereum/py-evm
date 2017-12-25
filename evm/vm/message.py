ecf37f45517a768470a2cb0bcb81f343e99d22b3
NIC619 <twedusuck@gmail.com>
2017-12-25 22:27:59 +0900

    add ShardingMessage

--- evm/vm/message.py
+++ evm/vm/message.py
@@ -1,6 +1,7 @@
 import logging
 
 from evm.constants import (
+    CREATE_CONTRACT_ADDRESS,
     ENTRY_POINT,
 )
 from evm.validation import (
@@ -52,11 +53,11 @@ class Message(object):
         validate_uint256(gas, title="Message.gas")
         self.gas = gas
 
-        validate_canonical_address(to, title="Message.to")
+        if to != CREATE_CONTRACT_ADDRESS:
+            validate_canonical_address(to, title="Message.to")
         self.to = to
 
-        if sender != ENTRY_POINT:
-            validate_canonical_address(sender, title="Message.sender")
+        validate_canonical_address(sender, title="Message.sender")
         self.sender = sender
 
         validate_uint256(value, title="Message.value")
@@ -111,6 +112,76 @@ class Message(object):
     @storage_address.setter
     def storage_address(self, value):
         self._storage_address = value
+
+    @property
+    def is_create(self):
+        return self.to == CREATE_CONTRACT_ADDRESS
+
+
+class ShardingMessage(Message):
+
+    def __init__(self,
+                 gas,
+                 gas_price,
+                 to,
+                 sender,
+                 value,
+                 data,
+                 code,
+                 origin=None,
+                 access_list=None,
+                 depth=0,
+                 create_address=None,
+                 code_address=None,
+                 should_transfer_value=True,
+                 is_static=False):
+        validate_uint256(gas, title="Message.gas")
+        self.gas = gas
+
+        validate_uint256(gas_price, title="Message.gas_price")
+        self.gas_price = gas_price
+
+        validate_canonical_address(to, title="Message.to")
+        self.to = to
+
+        if sender != ENTRY_POINT:
+            validate_canonical_address(sender, title="Message.sender")
+        self.sender = sender
+
+        validate_uint256(value, title="Message.value")
+        self.value = value
+
+        validate_is_bytes(data, title="Message.data")
+        self.data = data
+
+        if origin is not None:
+            validate_canonical_address(origin, title="Message.origin")
+        self.origin = origin
+
+        if access_list is not None:
+            validate_access_list(access_list)
+        self.access_list = access_list
+
+        validate_is_integer(depth, title="Message.depth")
+        validate_gte(depth, minimum=0, title="Message.depth")
+        self.depth = depth
+
+        validate_is_bytes(code, title="Message.code")
+        self.code = code
+
+        if create_address is not None:
+            validate_canonical_address(create_address, title="Message.storage_address")
+        self.storage_address = create_address
+
+        if code_address is not None:
+            validate_canonical_address(code_address, title="Message.code_address")
+        self.code_address = code_address
+
+        validate_is_boolean(should_transfer_value, title="Message.should_transfer_value")
+        self.should_transfer_value = should_transfer_value
+
+        validate_is_boolean(is_static, title="Message.is_static")
+        self.is_static = is_static
 
     @property
     def is_create(self):
