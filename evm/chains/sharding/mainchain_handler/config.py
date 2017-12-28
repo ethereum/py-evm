22acfe607696fcd0ddb8be15f58a6f95d624253e
mhchia <kevin.mh.chia@gmail.com>
2017-12-28 14:50:12 +0800

    Add description for variables in config.py

--- evm/chains/sharding/mainchain_handler/config.py
+++ evm/chains/sharding/mainchain_handler/config.py
@@ -1,5 +1,7 @@
-DEPOSIT_SIZE = 10 ** 20
-PERIOD_LENGTH = 5
+# vmc specific
+DEPOSIT_SIZE = 10 ** 20  # 100 ETH as defined in vmc
+PERIOD_LENGTH = 5  # as defined in vmc
 
-TX_GAS = 510000
-GASPRICE = 1
+# transaction related
+TX_GAS = 510000  # default gas, just a large enough gas for vmc transactions
+GASPRICE = 1  # default gas_price
