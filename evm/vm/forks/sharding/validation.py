41197fc4d2afb8db967a4b2953f06034d95aff39
NIC619 <twedusuck@gmail.com>
2017-12-30 10:40:56 +0900

    remove comment and replace validate_frontier_transaction with Sharding validation logic

--- evm/vm/forks/sharding/validation.py
+++ evm/vm/forks/sharding/validation.py
@@ -1,14 +1,25 @@
-from evm.vm.forks.frontier.validation import (
-    validate_frontier_transaction,
+from evm.exceptions import (
+    ValidationError,
+)
+from evm.validation import (
+    validate_transaction_access_list,
 )
 
 
-def validate_sharding_transaction(evm, transaction):
-    # TODO:Update transaction validation logic in Sharding
-    # e.g. checking shard_id < SHARD_COUNT
+def validate_sharding_transaction(vm, transaction):
+    gas_cost = transaction.gas * transaction.gas_price
+    with vm.state_db(read_only=True) as state_db:
+        txn_initiator_balance = state_db.get_balance(transaction.to)
 
-    # `validate_homestead_transaction` assumes underlying signature scheme
-    # to be ECDSA and checks signatures accordingly. So it's skipped for
-    # the reason that there won't be built-in signature scheme in Account
-    # Abstraction.
-    validate_frontier_transaction(evm, transaction)
+    if txn_initiator_balance < gas_cost:
+        raise ValidationError(
+            "Transaction initiator account balance cannot afford txn gas: `{0}`".format(transaction.to)  # noqa: E501
+        )
+
+    if vm.block.header.gas_used + transaction.gas > vm.block.header.gas_limit:
+        raise ValidationError("Transaction exceeds gas limit")
+
+    validate_transaction_access_list(transaction.access_list)
+
+    # TODO:Add transaction validation logic for Sharding
+    # e.g. checking shard_id < SHARD_COUNT
