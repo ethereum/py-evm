f68e279f66ace12d9fe011d6ad0060aaea3b50d1
NIC619 <twedusuck@gmail.com>
2018-01-08 17:45:17 +0800

    add sharding transaction test

--- tests/core/vm/test_vm.py
+++ tests/core/vm/test_vm.py
@@ -1,9 +1,16 @@
 from eth_utils import decode_hex
 
 from evm import constants
+from evm.utils.address import generate_create2_contract_address
 
-from tests.core.fixtures import chain_without_block_validation  # noqa: F401
-from tests.core.helpers import new_transaction
+from tests.core.fixtures import (  # noqa: F401
+    chain_without_block_validation,
+    shard_chain_without_block_validation,
+)
+from tests.core.helpers import (
+    new_transaction,
+    new_sharding_transaction,
+)
 
 
 def test_apply_transaction(chain_without_block_validation):  # noqa: F811
@@ -46,3 +53,28 @@ def test_import_block(chain_without_block_validation):  # noqa: F811
     parent_vm = chain.get_chain_at_block_parent(vm.block).get_vm()
     block = parent_vm.import_block(vm.block)
     assert block.transactions == [tx]
+
+
+def test_sharding_transaction(shard_chain_without_block_validation):  # noqa: F811
+    # Target contract, i.e. funded_address, is a simple ether transfer contract which transfer
+    # specified amount of ether to recipient.
+    # TODO: update contract_deployment_code
+    contract_deployment_code = b''
+    contract_addr = generate_create2_contract_address(b'', contract_deployment_code)
+
+    chain = shard_chain_without_block_validation(contract_addr)
+    deploy_tx = new_sharding_transaction(contract_addr, b'', 0, b'', b'', contract_deployment_code)
+
+    vm = chain.get_vm()
+    computation = vm.apply_transaction(deploy_tx)
+    assert not computation.is_error
+
+    # Transfer ether to recipient
+    recipient = decode_hex('0xa94f5374fce5edbc8e2a8697c15331677e6ebf0c')
+    amount = 100
+    tx_initiator = chain.funded_address
+    transfer_tx = new_sharding_transaction(tx_initiator, recipient, amount, b'', b'', b'')
+    computation = vm.execute_transaction(transfer_tx)
+    assert not computation.is_error
+    with vm.state.state_db(read_only=True) as state_db:
+        assert state_db.get_balance(recipient) == amount
