9e9ed3930423b9fdda1016e63e93bf246e13a5bb
NIC619 <twedusuck@gmail.com>
2018-01-11 23:26:27 +0800

    add CREATE2 opcode

--- evm/vm/forks/sharding/opcodes.py
+++ evm/vm/forks/sharding/opcodes.py
@@ -1,15 +1,17 @@
 import copy
 from cytoolz import merge
 
+from evm import constants
 from evm import opcode_values
 from evm import mnemonics
 
 from evm.opcode import as_opcode
 from evm.logic import (
     context,
+    system,
 )
 
-from evm.vm.forks.byzantium import BYZANTIUM_OPCODES
+from evm.vm.forks.byzantium.opcodes import BYZANTIUM_OPCODES
 
 
 NEW_OPCODES = {
@@ -18,6 +20,11 @@ NEW_OPCODES = {
         mnemonic=mnemonics.SIGHASH,
         gas_cost=0,
     ),
+    opcode_values.CREATE2: system.Create2.configure(
+        name='opcode:CREATE2',
+        mnemonic=mnemonics.CREATE2,
+        gas_cost=constants.GAS_CREATE2,
+    )(),
 }
 
 
