78dca333f2805cfbb9137c86e3fc3121b5dd2528
Jannik Luhn <jannik.luhn@brainbot.com>
2018-01-12 22:36:26 +0100

    Make transaction immutable, add validation

--- evm/auxiliary/user_account_contract/transaction.py
+++ evm/auxiliary/user_account_contract/transaction.py
@@ -6,6 +6,9 @@ from evm.vm.forks.sharding.transactions import (
 
 from evm.validation import (
     validate_uint256,
+    validate_lt_secpk1n,
+    validate_lte,
+    validate_gte,
     validate_canonical_address,
     validate_is_bytes,
 )
@@ -14,49 +17,36 @@ from evm.utils.transactions import (
     V_OFFSET,
 )
 from evm.utils.numeric import (
-    big_endian_to_int,
     int_to_big_endian,
 )
 from evm.utils.padding import (
     pad32,
 )
 
-from .contract import (
-    CALLDATA_SIGNATURE,
-    CALLDATA_V,
-    CALLDATA_R,
-    CALLDATA_S,
-    CALLDATA_NONCE,
-    CALLDATA_GASPRICE,
-    CALLDATA_VALUE,
-    CALLDATA_MIN_BLOCK,
-    CALLDATA_MAX_BLOCK,
-    CALLDATA_DESTINATION,
-    CALLDATA_DATA,
-)
+
+def assemble_data_field(user_account_transaction, include_signature=True):
+    if include_signature:
+        signature = b"".join([
+            pad32(int_to_big_endian(user_account_transaction.v)),
+            pad32(int_to_big_endian(user_account_transaction.r)),
+            pad32(int_to_big_endian(user_account_transaction.s)),
+        ])
+    else:
+        signature = b""
+
+    return b''.join([
+        signature,
+        pad32(int_to_big_endian(user_account_transaction.nonce)),
+        pad32(int_to_big_endian(user_account_transaction.int_gas_price)),
+        pad32(int_to_big_endian(user_account_transaction.value)),
+        pad32(int_to_big_endian(user_account_transaction.min_block)),
+        pad32(int_to_big_endian(user_account_transaction.max_block)),
+        pad32(user_account_transaction.destination),
+        user_account_transaction.msg_data,
+    ])
 
 
-def slice32(start):
-    return slice(start, start + 32)
-
-
-SIGNATURE_SLICE = slice(CALLDATA_SIGNATURE, CALLDATA_SIGNATURE + 96)
-SIGNATURE_V_SLICE = slice32(CALLDATA_V)
-SIGNATURE_R_SLICE = slice32(CALLDATA_R)
-SIGNATURE_S_SLICE = slice32(CALLDATA_S)
-NONCE_SLICE = slice32(CALLDATA_NONCE)
-GASPRICE_SLICE = slice32(CALLDATA_GASPRICE)
-VALUE_SLICE = slice32(CALLDATA_VALUE)
-MIN_BLOCK_SLICE = slice32(CALLDATA_MIN_BLOCK)
-MAX_BLOCK_SLICE = slice32(CALLDATA_MAX_BLOCK)
-DESTINATION_SLICE = slice32(CALLDATA_DESTINATION)
-DATA_SLICE = slice(CALLDATA_DATA, None)  # open ended
-
-EMPTY_DATA = b'\x00' * DESTINATION_SLICE.stop
-
-
-class ForwardingTransaction(ShardingTransaction):
-    """A transaction calling the standard user account contract which forwards the call."""
+class UserAccountTransaction(ShardingTransaction):
 
     def __init__(
         self,
@@ -71,18 +61,11 @@ class ForwardingTransaction(ShardingTransaction):
         min_block,
         max_block,
         gas_price,
-        msg_data
+        msg_data,
+        v,
+        r,
+        s,
     ):
-        super().__init__(
-            chain_id=chain_id,
-            shard_id=shard_id,
-            to=to,
-            data=EMPTY_DATA,
-            gas=gas,
-            gas_price=gas_price,
-            access_list=access_list,
-            code=b'',
-        )
         self.destination = destination
         self.value = value
         self.min_block = min_block
@@ -91,120 +74,139 @@ class ForwardingTransaction(ShardingTransaction):
         self.int_gas_price = gas_price
         self.msg_data = msg_data
 
-    #
-    # Properties for fields encoded in data
-    #
+        self.v = v
+        self.r = r
+        self.s = s
 
-    @property
-    def vrs(self):
-        v = big_endian_to_int(self.data[SIGNATURE_V_SLICE])
-        r = big_endian_to_int(self.data[SIGNATURE_R_SLICE])
-        s = big_endian_to_int(self.data[SIGNATURE_S_SLICE])
-        return (v, r, s)
+        data = assemble_data_field(self, include_signature=True)
 
-    @vrs.setter
-    def vrs(self, value):
-        for i in value:
-            validate_uint256(i)
+        super().__init__(
+            chain_id=chain_id,
+            shard_id=shard_id,
+            to=to,
+            data=data,
+            gas=gas,
+            gas_price=gas_price,
+            access_list=access_list,
+            code=b'',
+        )
 
-        b = b''.join([
-            pad32(int_to_big_endian(i)) for i in value
-        ])
+    def create_unsigned_transaction(self):
+        return UnsignedUserAccountTransaction(
+            chain_id=self.chain_id,
+            shard_id=self.shard_id,
+            to=self.to,
+            gas=self.gas,
+            access_list=self.access_list,
+            destination=self.destination,
+            value=self.value,
+            nonce=self.nonce,
+            min_block=self.min_block,
+            max_block=self.max_block,
+            gas_price=self.gas_price,
+            msg_data=self.msg_data,
+        )
 
-        self.data = self.data[:SIGNATURE_SLICE.start] + b + self.data[SIGNATURE_SLICE.stop:]
+    def get_sender(self):
+        signature = keys.Signature(vrs=(self.v - V_OFFSET, self.r, self.s))
+        message_for_signing = self.create_unsigned_transaction().get_message_for_signing()
+        public_key = signature.recover_public_key_from_msg(message_for_signing)
+        return public_key.to_canonical_address()
 
-    @property
-    def nonce(self):
-        return big_endian_to_int(self.data[NONCE_SLICE])
+    def validate(self):
+        super().validate()  # includes validation of `(int_)gas_price`
 
-    @nonce.setter
-    def nonce(self, value):
-        validate_uint256(value)
-        b = pad32(int_to_big_endian(value))
-        self.data = self.data[:NONCE_SLICE.start] + b + self.data[NONCE_SLICE.stop:]
+        validate_canonical_address(self.destination, "Transaction.destination")
+        validate_uint256(self.value, "Transaction.value")
+        validate_uint256(self.min_block, "Transaction.min_block")
+        validate_uint256(self.max_block, "Transaction.max_block")
+        validate_uint256(self.nonce, "Transaction.nonce")
+        validate_is_bytes(self.msg_data, "Transaction.msg_data")
 
-    @property
-    def min_block(self):
-        return big_endian_to_int(self.data[MIN_BLOCK_SLICE])
+        validate_uint256(self.v, title="Transaction.v")
+        validate_uint256(self.r, title="Transaction.r")
+        validate_uint256(self.s, title="Transaction.s")
 
-    @min_block.setter
-    def min_block(self, value):
-        validate_uint256(value)
-        b = pad32(int_to_big_endian(value))
-        self.data = self.data[:MIN_BLOCK_SLICE.start] + b + self.data[MIN_BLOCK_SLICE.stop:]
+        validate_lt_secpk1n(self.r, title="Transaction.r")
+        validate_gte(self.r, minimum=1, title="Transaction.r")
+        validate_lt_secpk1n(self.s, title="Transaction.s")
+        validate_gte(self.s, minimum=1, title="Transaction.s")
 
-    @property
-    def max_block(self):
-        return big_endian_to_int(self.data[MAX_BLOCK_SLICE])
+        validate_gte(self.v, minimum=27, title="Transaction.v")
+        validate_lte(self.v, maximum=28, title="Transaction.v")
 
-    @max_block.setter
-    def max_block(self, value):
-        validate_uint256(value)
-        b = pad32(int_to_big_endian(value))
-        self.data = self.data[:MAX_BLOCK_SLICE.start] + b + self.data[MAX_BLOCK_SLICE.stop:]
 
-    # TODO: rename to gas_price once gas price is removed from ShardingTransaction
-    @property
-    def int_gas_price(self):
-        return big_endian_to_int(self.data[GASPRICE_SLICE])
+class UnsignedUserAccountTransaction(ShardingTransaction):
 
-    @int_gas_price.setter
-    def int_gas_price(self, value):
-        validate_uint256(value)
-        b = pad32(int_to_big_endian(value))
-        self.data = self.data[:GASPRICE_SLICE.start] + b + self.data[GASPRICE_SLICE.stop:]
+    def __init__(
+        self,
+        chain_id,
+        shard_id,
+        to,
+        gas,
+        access_list,
+        destination,
+        value,
+        nonce,
+        min_block,
+        max_block,
+        gas_price,
+        msg_data,
+    ):
+        self.destination = destination
+        self.value = value
+        self.min_block = min_block
+        self.max_block = max_block
+        self.nonce = nonce
+        self.int_gas_price = gas_price
+        self.msg_data = msg_data
 
-    @property
-    def value(self):
-        return big_endian_to_int(self.data[VALUE_SLICE])
+        # will be used for signing
+        data = assemble_data_field(self, include_signature=False)
 
-    @value.setter
-    def value(self, value_):
-        validate_uint256(value_)
-        b = pad32(int_to_big_endian(value_))
-        self.data = self.data[:VALUE_SLICE.start] + b + self.data[VALUE_SLICE.stop:]
+        super().__init__(
+            chain_id=chain_id,
+            shard_id=shard_id,
+            to=to,
+            data=data,
+            gas=gas,
+            gas_price=gas_price,
+            access_list=access_list,
+            code=b"",
+        )
 
-    @property
-    def destination(self):
-        destination_padded = self.data[DESTINATION_SLICE]
-        assert destination_padded.startswith(b'\x00' * 12)
-        return destination_padded[12:]
+    def validate(self):
+        super().validate()  # includes validation of `(int_)gas_price`
 
-    @destination.setter
-    def destination(self, value):
-        validate_canonical_address(value)
-        b = pad32(value)
-        self.data = self.data[:DESTINATION_SLICE.start] + b + self.data[DESTINATION_SLICE.stop:]
-
-    @property
-    def msg_data(self):
-        return self.data[DATA_SLICE]
-
-    @msg_data.setter
-    def msg_data(self, value):
-        validate_is_bytes(value)
-        self.data = self.data[:DATA_SLICE.start] + value
-
-    #
-    # Signing
-    #
-
-    @property
-    def is_signed(self):
-        return self.vrs != (0, 0, 0)
+        validate_canonical_address(self.destination, "Transaction.destination")
+        validate_uint256(self.value, "Transaction.value")
+        validate_uint256(self.min_block, "Transaction.min_block")
+        validate_uint256(self.max_block, "Transaction.max_block")
+        validate_uint256(self.nonce, "Transaction.nonce")
+        validate_is_bytes(self.msg_data, "Transaction.msg_data")
 
     def get_message_for_signing(self):
-        return b''.join([
-            self.data[SIGNATURE_SLICE.stop:],
+        return b"".join([
+            self.data,  # does not include the signature
             self.sig_hash,
         ])
 
-    def sign(self, private_key):
+    def as_signed_transaction(self, private_key):
         signature = private_key.sign_msg(self.get_message_for_signing())
-        self.vrs = (signature.v + V_OFFSET, signature.r, signature.s)
-        return self
-
-    def get_sender(self):
-        signature = keys.Signature(vrs=(self.vrs[0] - V_OFFSET, self.vrs[1], self.vrs[2]))
-        public_key = signature.recover_public_key_from_msg(self.get_message_for_signing())
-        return public_key.to_canonical_address()
+        return UserAccountTransaction(
+            chain_id=self.chain_id,
+            shard_id=self.shard_id,
+            to=self.to,
+            gas=self.gas,
+            access_list=self.access_list,
+            destination=self.destination,
+            value=self.value,
+            nonce=self.nonce,
+            min_block=self.min_block,
+            max_block=self.max_block,
+            gas_price=self.gas_price,
+            msg_data=self.msg_data,
+            v=signature.v + V_OFFSET,
+            r=signature.r,
+            s=signature.s,
+        )
