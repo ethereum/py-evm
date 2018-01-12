fa1e628aad851437dc145fdcf21fbcdfb1f626d4
NIC619 <twedusuck@gmail.com>
2018-01-12 18:15:03 +0800

    update get_db_backend comment, remove commented state_root and make contract_address computed value

--- tests/core/vm/contract_fixture.py
+++ tests/core/vm/contract_fixture.py
@@ -1,3 +1,5 @@
+from evm.utils.address import generate_create2_contract_address
+
 # contract code to be deployed
 contract_lll_code = ['seq',
                         ['return',  # noqa: E127
@@ -15,4 +17,4 @@ contract_lll_code = ['seq',
 contract_bytecode = b'0x61003e567401000000000000000000000000000000000000000060205260003560205181101558575060006000600060006020356000356000f1155857005b61000461003e0361000460003961000461003e036000f3'  # noqa: E501
 
 # address where this contract will be deployed
-contract_address = b'\xdb\xcd\xfc\xf2\xea!\xcd\x0c\xa5d\xaay\x8b;\xf0\xe4\xe2\x98S\xca'
+contract_address = generate_create2_contract_address(b'', contract_bytecode)
