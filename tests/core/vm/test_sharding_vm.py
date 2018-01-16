b5a93c7a323c5397981aefe2df830d7a4e43feaf
NIC619 <twedusuck@gmail.com>
2018-01-16 13:24:47 +0800

    update consumed gas check in test_sharding_vm.py

--- tests/core/vm/test_sharding_vm.py
+++ tests/core/vm/test_sharding_vm.py
@@ -35,6 +35,9 @@ def test_sharding_apply_transaction(shard_chain_without_block_validation):  # no
     vm = chain.get_vm()
     computation = vm.apply_transaction(first_deploy_tx)
     assert not computation.is_error
+    gas_used = vm.block.header.gas_used
+    assert gas_used > first_deploy_tx.intrinsic_gas
+    last_gas_used = gas_used
 
     # Transfer ether to recipient
     recipient = decode_hex('0xa94f5374fce5edbc8e2a8697c15331677e6ebf0c')
@@ -44,7 +47,9 @@ def test_sharding_apply_transaction(shard_chain_without_block_validation):  # no
 
     computation = vm.apply_transaction(transfer_tx)
     assert not computation.is_error
-    assert vm.block.header.gas_used
+    gas_used = vm.block.header.gas_used - last_gas_used
+    assert gas_used > transfer_tx.intrinsic_gas
+    last_gas_used = vm.block.header.gas_used
     with vm.state.state_db(read_only=True) as state_db:
         assert state_db.get_balance(recipient) == amount
 
@@ -60,7 +65,9 @@ def test_sharding_apply_transaction(shard_chain_without_block_validation):  # no
 
     computation = vm.apply_transaction(second_deploy_tx)
     assert not computation.is_error
-    assert vm.block.header.gas_used
+    gas_used = vm.block.header.gas_used - last_gas_used
+    assert gas_used > second_deploy_tx.intrinsic_gas
+    last_gas_used = vm.block.header.gas_used
 
     # Invoke the contract to deploy new contract
     tx_initiator = CREATE2_contract_address
@@ -68,7 +75,8 @@ def test_sharding_apply_transaction(shard_chain_without_block_validation):  # no
 
     computation = vm.apply_transaction(invoke_tx)
     assert not computation.is_error
-    assert vm.block.header.gas_used
+    gas_used = vm.block.header.gas_used - last_gas_used
+    assert gas_used > invoke_tx.intrinsic_gas
     with vm.state.state_db(read_only=True) as state_db:
         newly_deployed_contract_address = generate_CREATE2_contract_address(
             int_to_big_endian(0),
@@ -93,7 +101,9 @@ def test_CREATE2_deploy_contract_edge_cases(shard_chain_without_block_validation
     vm = chain.get_vm()
     computation = vm.apply_transaction(first_failed_deploy_tx)
     assert computation.is_error
-    assert vm.block.header.gas_used
+    gas_used = vm.block.header.gas_used
+    assert gas_used > first_failed_deploy_tx.intrinsic_gas
+    last_gas_used = gas_used
 
     # Next, complete deploying the contract
     successful_deploy_tx = new_sharding_transaction(
@@ -106,10 +116,13 @@ def test_CREATE2_deploy_contract_edge_cases(shard_chain_without_block_validation
     )
     computation = vm.apply_transaction(successful_deploy_tx)
     assert not computation.is_error
-    assert vm.block.header.gas_used
+    gas_used = vm.block.header.gas_used - last_gas_used
+    assert gas_used > successful_deploy_tx.intrinsic_gas
+    last_gas_used = gas_used
 
     # Second case: deploy to existing account
     second_failed_deploy_tx = successful_deploy_tx
     computation = vm.apply_transaction(second_failed_deploy_tx)
     assert computation.is_error
-    assert vm.block.header.gas_used
+    gas_used = vm.block.header.gas_used - last_gas_used
+    assert gas_used > second_failed_deploy_tx.intrinsic_gas
