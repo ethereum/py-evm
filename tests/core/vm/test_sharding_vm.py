72b3c56db634ba14c7f2a3d7e5302d6ba95fb506
NIC619 <twedusuck@gmail.com>
2018-01-16 11:58:10 +0800

    add edge case test cases for CREATE2 contract deployment

--- tests/core/vm/test_sharding_vm.py
+++ tests/core/vm/test_sharding_vm.py
@@ -20,7 +20,7 @@ from tests.core.vm.contract_fixture import (
 )
 
 
-def test_sharding_transaction(shard_chain_without_block_validation):  # noqa: F811
+def test_sharding_apply_transaction(shard_chain_without_block_validation):  # noqa: F811
     chain = shard_chain_without_block_validation
     # First test: simple ether transfer contract
     first_deploy_tx = new_sharding_transaction(
@@ -44,6 +44,7 @@ def test_sharding_transaction(shard_chain_without_block_validation):  # noqa: F8
 
     computation = vm.apply_transaction(transfer_tx)
     assert not computation.is_error
+    assert vm.block.header.gas_used
     with vm.state.state_db(read_only=True) as state_db:
         assert state_db.get_balance(recipient) == amount
 
@@ -59,6 +60,7 @@ def test_sharding_transaction(shard_chain_without_block_validation):  # noqa: F8
 
     computation = vm.apply_transaction(second_deploy_tx)
     assert not computation.is_error
+    assert vm.block.header.gas_used
 
     # Invoke the contract to deploy new contract
     tx_initiator = CREATE2_contract_address
@@ -66,6 +68,7 @@ def test_sharding_transaction(shard_chain_without_block_validation):  # noqa: F8
 
     computation = vm.apply_transaction(invoke_tx)
     assert not computation.is_error
+    assert vm.block.header.gas_used
     with vm.state.state_db(read_only=True) as state_db:
         newly_deployed_contract_address = generate_CREATE2_contract_address(
             int_to_big_endian(0),
@@ -73,3 +76,40 @@ def test_sharding_transaction(shard_chain_without_block_validation):  # noqa: F8
         )
         assert state_db.get_code(newly_deployed_contract_address) == b'\xbe\xef'
         assert state_db.get_storage(CREATE2_contract_address, 0) == 1
+
+
+def test_CREATE2_deploy_contract_edge_cases(shard_chain_without_block_validation):  # noqa: F811
+    # First case: computed contract address not the same as provided in `transaction.to`
+    chain = shard_chain_without_block_validation
+    first_failed_deploy_tx = new_sharding_transaction(
+        simple_transfer_contract_address,
+        b'',
+        0,
+        b'',
+        b'',
+        '0xf3',
+    )
+
+    vm = chain.get_vm()
+    computation = vm.apply_transaction(first_failed_deploy_tx)
+    assert computation.is_error
+    assert vm.block.header.gas_used
+
+    # Next, complete deploying the contract
+    successful_deploy_tx = new_sharding_transaction(
+        simple_transfer_contract_address,
+        b'',
+        0,
+        b'',
+        b'',
+        simple_transfer_contract_bytecode
+    )
+    computation = vm.apply_transaction(successful_deploy_tx)
+    assert not computation.is_error
+    assert vm.block.header.gas_used
+
+    # Second case: deploy to existing account
+    second_failed_deploy_tx = successful_deploy_tx
+    computation = vm.apply_transaction(second_failed_deploy_tx)
+    assert computation.is_error
+    assert vm.block.header.gas_used
