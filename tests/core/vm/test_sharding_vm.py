214f5cb759d89c0638ffbcc2028aa91bc1be1b52
Jannik Luhn <jannik.luhn@brainbot.com>
2018-01-18 12:40:20 +0100

    Provide proper access lists in sharding VM tests

--- tests/core/vm/test_sharding_vm.py
+++ tests/core/vm/test_sharding_vm.py
@@ -2,6 +2,7 @@ from eth_utils import (
     int_to_big_endian,
     decode_hex,
 )
+from evm.utils.padding import pad32
 
 from evm.exceptions import (
     IncorrectContractCreationAddress,
@@ -33,7 +34,7 @@ def test_sharding_apply_transaction(shard_chain_without_block_validation):  # no
         data_value=0,
         data_msgdata=b'',
         data_vrs=b'',
-        code=simple_transfer_contract_bytecode,
+        code=simple_transfer_contract_bytecode
     )
 
     vm = chain.get_vm()
@@ -47,7 +48,15 @@ def test_sharding_apply_transaction(shard_chain_without_block_validation):  # no
     recipient = decode_hex('0xa94f5374fce5edbc8e2a8697c15331677e6ebf0c')
     amount = 100
     tx_initiator = simple_transfer_contract_address
-    transfer_tx = new_sharding_transaction(tx_initiator, recipient, amount, b'', b'', b'')
+    transfer_tx = new_sharding_transaction(
+        tx_initiator,
+        recipient,
+        amount,
+        b'',
+        b'',
+        b'',
+        access_list=[[tx_initiator], [recipient]]
+    )
 
     computation = vm.apply_transaction(transfer_tx)
     assert not computation.is_error
@@ -58,13 +67,14 @@ def test_sharding_apply_transaction(shard_chain_without_block_validation):  # no
         assert state_db.get_balance(recipient) == amount
 
     # Second test: contract that deploy new contract with CREATE2
+
     second_deploy_tx = new_sharding_transaction(
         tx_initiator=CREATE2_contract_address,
         data_destination=b'',
         data_value=0,
         data_msgdata=b'',
         data_vrs=b'',
-        code=CREATE2_contract_bytecode,
+        code=CREATE2_contract_bytecode
     )
 
     computation = vm.apply_transaction(second_deploy_tx)
@@ -75,17 +85,25 @@ def test_sharding_apply_transaction(shard_chain_without_block_validation):  # no
 
     # Invoke the contract to deploy new contract
     tx_initiator = CREATE2_contract_address
-    invoke_tx = new_sharding_transaction(tx_initiator, b'', 0, b'', b'', b'')
+    newly_deployed_contract_address = generate_CREATE2_contract_address(
+        int_to_big_endian(0),
+        simple_contract_factory_bytecode
+    )
+    invoke_tx = new_sharding_transaction(
+        tx_initiator,
+        b'',
+        0,
+        b'',
+        b'',
+        b'',
+        access_list=[[tx_initiator, pad32(b"")], [newly_deployed_contract_address]]
+    )
 
     computation = vm.apply_transaction(invoke_tx)
     assert not computation.is_error
     gas_used = vm.block.header.gas_used - last_gas_used
     assert gas_used > invoke_tx.intrinsic_gas
     with vm.state.state_db(read_only=True) as state_db:
-        newly_deployed_contract_address = generate_CREATE2_contract_address(
-            int_to_big_endian(0),
-            simple_contract_factory_bytecode
-        )
         assert state_db.get_code(newly_deployed_contract_address) == b'\xbe\xef'
         assert state_db.get_storage(CREATE2_contract_address, 0) == 1
 
@@ -93,13 +111,16 @@ def test_sharding_apply_transaction(shard_chain_without_block_validation):  # no
 def test_CREATE2_deploy_contract_edge_cases(shard_chain_without_block_validation):  # noqa: F811
     # First case: computed contract address not the same as provided in `transaction.to`
     chain = shard_chain_without_block_validation
+    code = b"0xf3"
+    computed_address = generate_CREATE2_contract_address(b"", decode_hex(code))
     first_failed_deploy_tx = new_sharding_transaction(
         tx_initiator=simple_transfer_contract_address,
         data_destination=b'',
         data_value=0,
         data_msgdata=b'',
         data_vrs=b'',
-        code='0xf3',
+        code=code,
+        access_list=[[simple_transfer_contract_address], [computed_address]]
     )
 
     vm = chain.get_vm()
