4a9e4bb97317dfcc4e34007e923d529a47e9300d
mhchia <kevin.mh.chia@gmail.com>
2018-01-18 23:19:10 +0800

    Add the mistakenly removed setting of shard_head

--- evm/vm/forks/sharding/contracts/validator_manager.v.py
+++ evm/vm/forks/sharding/contracts/validator_manager.v.py
@@ -360,6 +360,7 @@ def add_header(header: bytes <= 4096) -> bool:
     # Determine the head
     is_new_head = False
     if _score > self.collation_headers[shard_id][self.shard_head[shard_id]].score:
+        self.shard_head[shard_id] = entire_header_hash
         is_new_head = True
 
     # Emit log
