3397277ee47379074f9b37a49435f0426a7f4069
Jannik Luhn <jannik.luhn@brainbot.com>
2018-01-24 09:53:13 +0100

    Update BaseChainDB for new account state db

--- evm/db/chain.py
+++ evm/db/chain.py
@@ -26,8 +26,7 @@ from evm.db.journal import (
     JournalDB,
 )
 from evm.db.state import (
-    AccountStateDB,
-    NestedTrieBackend,
+    MainAccountStateDB,
 )
 from evm.rlp.headers import (
     BlockHeader,
@@ -59,9 +58,9 @@ class TransactionKey(rlp.Serializable):
 
 class BaseChainDB:
 
-    def __init__(self, db, state_backend_class=NestedTrieBackend):
+    def __init__(self, db, account_state_class=MainAccountStateDB):
         self.db = JournalDB(db)
-        self.state_backend_class = state_backend_class
+        self.account_state_class = account_state_class
 
     def exists(self, key):
         return self.db.exists(key)
@@ -376,10 +375,11 @@ class BaseChainDB:
         self.db.clear()
 
     def get_state_db(self, state_root, read_only, access_list=None):
-        return AccountStateDB(
-            db=self.db,
-            root_hash=state_root,
-            read_only=read_only,
-            access_list=access_list,
-            backend_class=self.state_backend_class,
-        )
+        kwargs = {
+            "db": self.db,
+            "root_hash": state_root,
+            "read_only": read_only,
+        }
+        if access_list is not None:
+            kwargs["access_list"] = access_list
+        return self.account_state_class(**kwargs)
