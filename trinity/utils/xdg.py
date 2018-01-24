d5d3ccd81ca658ff845ef171e7bcff92a7f079a3
Piper Merriam <pipermerriam@gmail.com>
2018-01-24 12:41:10 -0700

    dirty

--- trinity/utils/xdg.py
+++ trinity/utils/xdg.py
@@ -25,39 +25,3 @@ def get_xdg_trinity_home():
         'TRINITY_HOME',
         os.path.join(XDG_DATA_HOME, 'trinity'),
     )
-
-
-def get_chain_dir(chain_identifier):
-    """
-    Returns the base directory path where data for a given chain will be stored.
-    """
-    return os.environ.get(
-        'TRINITY_CHAIN_DIR',
-        os.path.join(get_xdg_trinity_home(), chain_identifier),
-    )
-
-
-DATA_DIR_NAME = 'chain'
-
-
-def get_data_dir(chain_identifier):
-    """
-    Returns the directory path where chain data will be stored.
-    """
-    return os.environ.get(
-        'TRINITY_DATA_DIR',
-        os.path.join(get_chain_dir(chain_identifier), DATA_DIR_NAME),
-    )
-
-
-NODEKEY_FILENAME = 'nodekey'
-
-
-def get_nodekey_path(chain_identifier):
-    """
-    Returns the path to the private key used for devp2p connections.
-    """
-    return os.environ.get(
-        'TRINITY_NODEKEY',
-        os.path.join(get_chain_dir(chain_identifier), NODEKEY_FILENAME),
-    )
