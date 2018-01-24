046d2b7cf9c58d6ae213b8f385f67c029883ae47
Jannik Luhn <jannik.luhn@brainbot.com>
2018-01-24 11:11:27 +0100

    Mark sharding vm tests as xfailing

--- tests/core/vm/test_sharding_vm.py
+++ tests/core/vm/test_sharding_vm.py
@@ -1,3 +1,5 @@
+import pytest
+
 from eth_utils import (
     int_to_big_endian,
     decode_hex,
@@ -24,6 +26,7 @@ from tests.core.vm.contract_fixture import (
 )
 
 
+@pytest.mark.xfail(reason="#281", strict=True)
 def test_sharding_apply_transaction(shard_chain_without_block_validation):  # noqa: F811
     chain = shard_chain_without_block_validation
     # First test: simple ether transfer contract
@@ -90,6 +93,7 @@ def test_sharding_apply_transaction(shard_chain_without_block_validation):  # no
         assert state_db.get_storage(CREATE2_contract_address, 0) == 1
 
 
+@pytest.mark.xfail(reason="#281", strict=True)
 def test_CREATE2_deploy_contract_edge_cases(shard_chain_without_block_validation):  # noqa: F811
     # First case: computed contract address not the same as provided in `transaction.to`
     chain = shard_chain_without_block_validation
