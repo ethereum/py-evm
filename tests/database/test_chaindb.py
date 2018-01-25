37229c863c907322df42dc9b022aec69d35e5dce
Jannik Luhn <jannik.luhn@brainbot.com>
2018-01-25 11:22:29 +0100

    Mark get_witness test as xfailing

--- tests/database/test_chaindb.py
+++ tests/database/test_chaindb.py
@@ -147,6 +147,7 @@ def test_lookup_block_hash(chaindb, block):
     assert block_hash == block.hash
 
 
+@pytest.mark.xfail(reason="#289 (switch to binary trie not complete yet)", strict=True)
 def test_get_witness_nodes(populated_chaindb_and_root_hash):
     chaindb, root_hash = populated_chaindb_and_root_hash
     header = CollationHeader(
