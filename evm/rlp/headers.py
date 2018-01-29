ba573849d02e6d13821c23967d4ae3a2c29d5fd3
Jannik Luhn <jannik.luhn@brainbot.com>
2018-01-29 10:17:32 +0100

    Add number field to collation header

--- evm/rlp/headers.py
+++ evm/rlp/headers.py
@@ -153,6 +153,7 @@ class CollationHeader(rlp.Serializable):
         ("coinbase", address),
         ("state_root", hash32),
         ("receipt_root", hash32),
+        ("number", big_endian_int),
         ("sig", binary)
     ]
 
@@ -161,6 +162,7 @@ class CollationHeader(rlp.Serializable):
                  expected_period_number,
                  period_start_prevhash,
                  parent_hash,
+                 number,
                  transaction_root=BLANK_ROOT_HASH,
                  coinbase=ZERO_ADDRESS,
                  state_root=BLANK_ROOT_HASH,
@@ -175,6 +177,7 @@ class CollationHeader(rlp.Serializable):
             coinbase=coinbase,
             state_root=state_root,
             receipt_root=receipt_root,
+            number=number,
             sig=sig
         )
 
@@ -206,6 +209,7 @@ class CollationHeader(rlp.Serializable):
             "period_start_prevhash": period_start_prevhash,
             "parent_hash": parent.hash,
             "state_root": parent.state_root,
+            "number": parent.number + 1,
             "sig": sig
         }
         header = cls(**header_kwargs)
