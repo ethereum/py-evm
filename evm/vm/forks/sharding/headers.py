33bca378b1c361070a20261faf25cfe974b342b9
Hsiao-Wei Wang <hwwang156@gmail.com>
2018-02-08 01:59:47 +0800

    PR feedback

--- evm/vm/forks/sharding/headers.py
+++ evm/vm/forks/sharding/headers.py
@@ -7,8 +7,6 @@ from evm.vm.forks.byzantium.headers import (
 
 
 def create_sharding_header_from_parent(parent_header, **header_params):
-    if 'transaction_root' not in header_params:
-        header_params['transaction_root'] = EMPTY_SHA3
-    if 'receipt_root' not in header_params:
-        header_params['receipt_root'] = EMPTY_SHA3
+    header_params.setdefault('transaction_root', EMPTY_SHA3)
+    header_params.setdefault('receipt_root', EMPTY_SHA3)
     return create_byzantium_header_from_parent(parent_header, **header_params)
