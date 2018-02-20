019497cad33f9c8acbd1d946f29f48255d283e24
Piper Merriam <piper@pipermerriam.com>
2018-02-19 19:45:38 -0700

    Backport changes from trinity branch to master (#377)

--- evm/db/chain.py
+++ evm/db/chain.py
@@ -136,20 +136,20 @@ class BaseChainDB:
         raise NotImplementedError("ChainDB classes must implement this method")
 
     def persist_block_to_db(self, block):
+        """
+        Chain must do follow-up work to persist transactions to db
+        """
         raise NotImplementedError("ChainDB classes must implement this method")
 
     #
     # Transaction and Receipt API
     #
-    @to_list
     def get_receipts(self, header, receipt_class):
         raise NotImplementedError("ChainDB classes must implement this method")
 
-    @to_list
     def get_block_transaction_hashes(self, block_header):
         raise NotImplementedError("ChainDB classes must implement this method")
 
-    @to_list
     def get_block_transactions(self, block_header, transaction_class):
         raise NotImplementedError("ChainDB classes must implement this method")
 
@@ -202,7 +202,7 @@ class BaseChainDB:
     # State Database API
     #
     def get_state_db(self, state_root, read_only):
-        return AccountStateDB(db=self.db, root_hash=state_root, read_only=read_only)
+        raise NotImplementedError("ChainDB classes must implement this method")
 
 
 class ChainDB(BaseChainDB):
--- tests/rpc/conftest.py
+++ tests/rpc/conftest.py
@@ -9,17 +9,24 @@ from evm.rpc.ipc import (
 )
 
 
-@pytest.yield_fixture(scope='session')
+@pytest.fixture(scope='session')
 def event_loop():
     loop = asyncio.new_event_loop()
-    yield loop
-    loop.close()
+    try:
+        yield loop
+    finally:
+        loop.close()
 
 
 @pytest.fixture(scope='session')
 def ipc_pipe_path():
-    tmpdir = tempfile.gettempdir()
-    return os.path.join(tmpdir, 'test-%s.ipc' % uuid.uuid4())
+    with tempfile.TemporaryDirectory() as temp_dir:
+        ipc_path = os.path.join(temp_dir, '{0}.ipc'.format(uuid.uuid4()))
+        try:
+            yield ipc_path
+        finally:
+            if os.path.exists(ipc_path):
+                os.remove(ipc_path)
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -31,8 +38,8 @@ def ipc_server(ipc_pipe_path, event_loop):
     '''
     server = start(ipc_pipe_path, loop=event_loop)
 
-    yield
-
-    server.close()
-    event_loop.run_until_complete(server.wait_closed())
-    os.remove(ipc_pipe_path)
+    try:
+        yield
+    finally:
+        server.close()
+        event_loop.run_until_complete(server.wait_closed())
