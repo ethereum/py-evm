40fb6612d8a3ea6844daded2f45f31ffb6487b6e
Jannik Luhn <jannik.luhn@brainbot.com>
2018-02-27 11:58:19 +0100

    Use eth_utils.to_hex instead of own implementation

--- evm/utils/test_builder/formatters.py
+++ evm/utils/test_builder/formatters.py
@@ -1,45 +1,40 @@
-from cytoolz import (
-    compose,
-)
 from eth_utils import (
     combine_argument_formatters,
     encode_hex,
-    int_to_big_endian,
     to_checksum_address,
+    to_hex,
 )
 import cytoolz.curried
 import eth_utils.curried
 
 
-int_to_hex = compose(encode_hex, int_to_big_endian)
-
 environment_formatter = eth_utils.curried.apply_formatters_to_dict({
     # shared
     "currentCoinbase": to_checksum_address,
     "previousHash": encode_hex,
-    "currentNumber": int_to_hex,
+    "currentNumber": to_hex,
 
     # only main environment
-    "currentDifficulty": int_to_hex,
-    "currentGasLimit": int_to_hex,
-    "currentTimestamp": int_to_hex,
+    "currentDifficulty": to_hex,
+    "currentGasLimit": to_hex,
+    "currentTimestamp": to_hex,
 
     # only sharding environment
-    "shardID": int_to_hex,
-    "expectedPeriodNumber": int_to_hex,
+    "shardID": to_hex,
+    "expectedPeriodNumber": to_hex,
     "periodStartHash": encode_hex,
     "currentCoinbase": to_checksum_address,
-    "currentNumber": int_to_hex,
+    "currentNumber": to_hex,
 })
 
 
-storage_item_formatter = combine_argument_formatters(int_to_hex, int_to_hex)
+storage_item_formatter = combine_argument_formatters(to_hex, to_hex)
 storage_formatter = cytoolz.curried.itemmap(storage_item_formatter)
 
 
 account_state_formatter = eth_utils.curried.apply_formatters_to_dict({
-    "balance": int_to_hex,
-    "nonce": int_to_hex,
+    "balance": to_hex,
+    "nonce": to_hex,
     "code": encode_hex,
     "storage": storage_formatter,
 })
@@ -57,17 +52,17 @@ transaction_group_formatter = eth_utils.curried.apply_formatters_to_dict({
     # all transaction types
     "to": to_checksum_address,
     "data": eth_utils.curried.apply_formatter_to_array(encode_hex),
-    "gasLimit": eth_utils.curried.apply_formatter_to_array(int_to_hex),
-    "gasPrice": int_to_hex,
+    "gasLimit": eth_utils.curried.apply_formatter_to_array(to_hex),
+    "gasPrice": to_hex,
 
     # only main chain transactions
-    "nonce": int_to_hex,
+    "nonce": to_hex,
     "secretKey": encode_hex,
-    "value": eth_utils.curried.apply_formatter_to_array(int_to_hex),
+    "value": eth_utils.curried.apply_formatter_to_array(to_hex),
 
     # only sharding transactions
-    "chainID": int_to_hex,
-    "shardID": int_to_hex,
+    "chainID": to_hex,
+    "shardID": to_hex,
     "code": encode_hex,
     "accessList": access_list_formatter,
 })
@@ -78,10 +73,10 @@ execution_formatter = eth_utils.curried.apply_formatters_to_dict({
     "origin": to_checksum_address,
     "caller": to_checksum_address,
     "code": encode_hex,
-    "value": int_to_hex,
+    "value": to_hex,
     "data": encode_hex,
-    "gasPrice": int_to_hex,
-    "gas": int_to_hex,
+    "gasPrice": to_hex,
+    "gas": to_hex,
 })
 
 
@@ -116,8 +111,8 @@ filled_state_test_formatter = cytoolz.curried.valmap(eth_utils.curried.apply_for
 call_create_item_formatter = eth_utils.curried.apply_formatters_to_dict({
     "data": encode_hex,
     "destination": to_checksum_address,
-    "gasLimit": int_to_hex,
-    "value": int_to_hex,
+    "gasLimit": to_hex,
+    "value": to_hex,
 })
 call_creates_formatter = eth_utils.curried.apply_formatter_to_array(call_create_item_formatter)
 
@@ -128,6 +123,6 @@ filled_vm_test_formatter = cytoolz.curried.valmap(eth_utils.curried.apply_format
     "post": state_formatter,
     "callcreates": call_creates_formatter,
     "logs": encode_hex,
-    "gas": int_to_hex,
+    "gas": to_hex,
     "output": encode_hex,
 }))
