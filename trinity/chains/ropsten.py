ad47b01d322c03910f14868f0ceb887c9e0855c2
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2018-03-02 00:05:32 +0100

    Add type hints for trinity/chains

--- trinity/chains/ropsten.py
+++ trinity/chains/ropsten.py
@@ -3,8 +3,9 @@ from evm.chains.ropsten import ROPSTEN_NETWORK_ID
 
 from p2p.lightchain import LightChain
 
+from typing import Type
 
-RopstenLightChain = LightChain.configure(
+RopstenLightChain: Type[LightChain] = LightChain.configure(
     __name__='RopstenLightChain',
     vm_configuration=MAINNET_VM_CONFIGURATION,  # TODO: use real ropsten configuration
     network_id=ROPSTEN_NETWORK_ID,
