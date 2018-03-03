ebe8e1ffca7b039baad415dd5b8e49fa8a4c82ca
NIC619 <twedusuck@gmail.com>
2018-03-03 17:47:02 +0800

    Update Sharding transaction validation logic

--- evm/vm/forks/sharding/validation.py
+++ evm/vm/forks/sharding/validation.py
@@ -7,15 +7,6 @@ from evm.validation import (
 
 
 def validate_sharding_transaction(vm_state, transaction):
-    gas_cost = transaction.gas * transaction.gas_price
-    with vm_state.state_db(read_only=True) as state_db:
-        txn_initiator_balance = state_db.get_balance(transaction.to)
-
-    if txn_initiator_balance < gas_cost:
-        raise ValidationError(
-            "Transaction initiator account balance cannot afford txn gas: `{0}`".format(transaction.to)  # noqa: E501
-        )
-
     if vm_state.gas_used + transaction.gas > vm_state.gas_limit:
         raise ValidationError("Transaction exceeds gas limit")
 
