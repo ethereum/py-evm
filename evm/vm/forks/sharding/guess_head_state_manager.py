7eda86c65017e7bfd02be6e202d90c04dfe224e0
mhchia <kevin.mh.chia@gmail.com>
2018-03-09 00:25:59 +0800

    Add logics but seems to be not working now
    Still need to clarify:
    - TODOs in guess_head_state_manager.py

--- evm/vm/forks/sharding/guess_head_state_manager.py
+++ evm/vm/forks/sharding/guess_head_state_manager.py
@@ -1,67 +1,59 @@
+from collections import (
+    OrderedDict,
+    defaultdict,
+)
+
+import logging
+
 from evm.vm.forks.sharding.vmc_handler import (
+    NoCandidateHead,
     fetch_and_verify_collation,
 )
 
 
 GENESIS_COLLATION_HASH = b'\x00' * 32
+NUM_RESERVED_BLOCKS = 5
+
+
+def threaded_execute(function, *args, **kwargs):
+    # TODO: fake thread, should be replaced by Threading or async
+    function(*args, **kwargs)
+
+
+def create_collation(data):
+    # return collation with data with head = candidate_head
+    # ablalalala
+    print("!@# collation={} created".format(data))
+    return True
 
 
 class GuessHeadStateManager:
 
+    logger = logging.getLogger("evm.chain.sharding.GuessHeadStateManager")
+
     collation_validity_cache = None
-    candidate_head_hash = None
-    checking_collation_hash = None
+    head_validity = None
+    last_period_fetching_candidate_head = None
+    is_verifying_collations = None
 
     def __init__(self, vmc, shard_id, my_address):
-        # a well-setup vmc handler for shard_trackers in shard `shard_id`
+        # a well-setup vmc handler with a shard_tracker in shard `shard_id`
         self.vmc = vmc
         self.shard_id = shard_id
         self.my_address = my_address
 
+        # map[collation] -> validity
         self.collation_validity_cache = {}
-
-    def verify_collation(self, collation_hash):
-        if collation_hash not in self.collation_validity_cache:
-            self.collation_validity_cache[collation_hash] = fetch_and_verify_collation(
-                collation_hash,
-            )
-        return self.collation_validity_cache[collation_hash]
-
-    def process_current_collation(self):
-        """
-        Verfiy collation and return the result.
-        If the verification fails(`self.verify_collation` returns False),
-        indicate the current chain which we are verifying is invalid,
-        should jump to another candidate chain
-        """
-        result = self.verify_collation(self.checking_collation_hash)
-        if result:
-            self.checking_collation_hash = self.vmc.self.get_parent_hash(
-                self.shard_id,
-                self.checking_collation_hash,
-            )
-        # this collation is invalid, so don't check the candidate head anymore
-        else:
-            self.candidate_head_hash = None
-        return result
-
-    def tick_guess_head(self):
-        # if there is no candidate head,
-        # find one invalid, jump to the next chain
-        if self.candidate_head_hash is None:
-            candidate_head_hash = self.vmc.fetch_candidate_head(self.shard_id)
-            # No candidate head now, should stop
-            if candidate_head_hash == GENESIS_COLLATION_HASH:
-                raise ValueError("No candidate head")
-            self.candidate_head_hash = candidate_head_hash
-            self.checking_collation_hash = candidate_head_hash
-        # there is candidate head, process the current collation
-        return self.process_current_collation()
-
-    def create_collation(self, data):
-        # return collation with data with head = candidate_head
-        collation = True # ablalalala
-        return collation
+        # map[chain_head] -> validity
+        # this should be able to be updated by the collations' validity
+        # TODO: need to check if it is thread-safe
+        self.head_validity = defaultdict(lambda: True)
+        # list of chain head, to indicate priority
+        # order: older -------> newer
+        self.last_period_fetching_candidate_head = 0
+        # map[collation] -> thread
+        self.collation_thread = []
+        self.is_verifying_collations = False
 
     def get_current_period(self):
         return self.vmc.web3.eth.blockNumber // self.vmc.config['PERIOD_LENGTH']
@@ -73,33 +65,103 @@ class GuessHeadStateManager:
         )
         return collator_address == self.my_address
 
-    def is_collator_in_lookahead_period(self):
-        lookahead_period = self.get_current_period() + self.vmc.config['LOOKAHEAD_PERIODS']
-        return self.is_collator_in_period(lookahead_period)
+    def is_collator_in_lookahead_periods(self):
+        """
+        See if we are going to be collator in the future periods
+        """
+        result = False
+        for future_periods in self.vmc.config['LOOKAHEAD_PERIODS']:
+            lookahead_period = self.get_current_period() + future_periods
+            result |= self.is_collator_in_period(lookahead_period)
+        return result
 
     def is_collator_in_current_period(self):
         current_period = self.get_current_period()
         return self.is_collator_in_period(current_period)
 
+    def is_time_to_collate(self):
+        current_period = self.get_current_period()
+        current_block_num  = self.vmc.web3.eth.blockNumber
+        last_block_num_in_current_period = current_period * self.vmc.config['PERIOD_LENGTH']
+        return (
+            self.is_collator_in_period(current_period) and
+            (current_block_num + NUM_RESERVED_BLOCKS >= last_block_num_in_current_period)
+        )
+
+    def verify_collation(self, collation_hash):
+        # TODO: thread-safe, not sure if GIL is countable
+        if collation_hash not in self.collation_validity_cache:
+            self.collation_validity_cache[collation_hash] = fetch_and_verify_collation(
+                collation_hash,
+            )
+        return self.collation_validity_cache[collation_hash]
+
+    def process_collation(self, head_collation_hash, checking_collation_hash):
+        """
+        Verfiy collation and return the result.
+        If the verification fails(`self.verify_collation` returns False),
+        indicate the current chain which we are verifying is invalid,
+        should jump to another candidate chain
+        """
+        result = self.verify_collation(checking_collation_hash)
+        if not result:
+            # TODO: thread-safe right?
+            self.head_validity[head_collation_hash] = False
+
+    def fetch_candidate_head_hash(self):
+        head_collation_hash = None
+        try:
+            head_collation_dict = self.vmc.fetch_candidate_head(self.shard_id)
+            head_collation_hash = head_collation_dict['header'].hash
+        except NoCandidateHead:
+            self.logger.debug("No candidate head available, `guess_head` stops")
+        self.last_period_fetching_candidate_head = self.get_current_period()
+        return head_collation_hash
+
     def main(self):
+        # At any time, there should be only one `head_collation`
+        # The timing where `head_collation_hash` change are:
+        #    1. `head_collation` is invalid, change to the new head
+        #    2. There are new logs, and `fetch_candidate_head` is called
+        # When to stop processing collation?
+        head_collation_hash = None
         while True:
-            if self.is_collator_in_lookahead_period():
-                # run verify chain for current candidate_head
-            if self.is_collator_in_current_period():
-                # peek if there are new logs
-                shard_tracker = self.vmc.get_shard_tracker[self.shard_id]
-                new_logs = shard_tracker.peek_new_logs()
-                # if there are new logs, should run `guess_head` again
-                if len(new_logs) != 0:
-                    # resetup candidate head hash
-                    self.candidate_head_hash = None
+            # Actual `tick_guess_head` body  ##################################
+            if self.is_collator_in_lookahead_periods():
+                self.is_verifying_collations = True
 
-                # setup candidate head hash
-                self.candidate_head_hash = None
+            # Check for head changing
+            current_period = self.get_current_period()
+            if ((current_period > self.last_period_fetching_candidate_head) or
+                    (head_collation_hash is None) or
+                    (not self.head_validity[head_collation_hash])):
+                if current_period > self.last_period_fetching_candidate_head:
+                    # Should flush old candidate heads first, since all those candidates are stale
+                    shard_tracker = self.vmc.get_shard_tracker(self.shard_id)
+                    shard_tracker.clean_logs()
+                head_collation_hash = self.fetch_candidate_head_hash()
+                current_collation_hash = head_collation_hash
 
-            # case3: need to stop guess_head and go create collations
-            else:
-                tx_data = []
-                self.create_collation(tx_data)
-                return
-            self.tick_guess_head()
+            # tick guess head: only process collations when the node is collating
+            if self.is_verifying_collations and (head_collation_hash is not None):
+                # process current collation
+                threaded_execute(
+                    self.process_collation,
+                    head_collation_hash,
+                    current_collation_hash,
+                )
+                current_collation_hash = self.vmc.get_parent_hash(
+                    self.shard_id,
+                    current_collation_hash,
+                )
+
+            # Check if it is time to collate  #################################
+            if self.is_time_to_collate():
+                # create collation
+                create_collation("123")
+            # Stop collating anyway, if we are still in periods where we should collate,
+            # `self.is_verifying_collations` will be set to True again
+            self.is_verifying_collations = False
+
+            # TODO: when to stop ?
+            # TODO: what to do when current_collation_hash == genesis?
