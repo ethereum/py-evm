5566c26e32bd64c85234ceacb616665d3cb46d8e
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2018-03-26 11:27:26 +0200

    Add more type hints
    Relates to #132

--- evm/db/backends/base.py
+++ evm/db/backends/base.py
@@ -7,7 +7,13 @@ from abc import (
 class BaseDB(metaclass=ABCMeta):
 
     @abstractmethod
-    def get(self, key):
+    def __init__(self) -> None:
+        raise NotImplementedError(
+            "The `init` method must be implemented by subclasses of BaseDB"
+        )
+
+    @abstractmethod
+    def get(self, key: bytes) -> bytes:
         """Return the value for the given key.
 
         Raises KeyError if key doesn't exist.
@@ -17,20 +23,20 @@ class BaseDB(metaclass=ABCMeta):
         )
 
     @abstractmethod
-    def set(self, key, value):
+    def set(self, key: bytes, value: bytes) -> None:
         raise NotImplementedError(
             "The `set` method must be implemented by subclasses of BaseDB"
         )
 
     @abstractmethod
-    def exists(self, key):
+    def exists(self, key: bytes) -> bool:
         """Return True if the key exists or False if it doesn't."""
         raise NotImplementedError(
             "The `exists` method must be implemented by subclasses of BaseDB"
         )
 
     @abstractmethod
-    def delete(self, key):
+    def delete(self, key: bytes) -> None:
         raise NotImplementedError(
             "The `delete` method must be implemented by subclasses of BaseDB"
         )
@@ -38,14 +44,14 @@ class BaseDB(metaclass=ABCMeta):
     #
     # Dictionary API
     #
-    def __getitem__(self, key):
+    def __getitem__(self, key: bytes) -> bytes:
         return self.get(key)
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: bytes, value: bytes) -> None:
         return self.set(key, value)
 
-    def __delitem__(self, key):
+    def __delitem__(self, key: bytes) -> None:
         return self.delete(key)
 
-    def __contains__(self, key):
+    def __contains__(self, key: bytes) -> bool:
         return self.exists(key)
--- evm/db/immutable.py
+++ evm/db/immutable.py
@@ -4,17 +4,17 @@ from evm.db.backends.base import BaseDB
 class ImmutableDB(BaseDB):
     _wrapped_db = None
 
-    def __init__(self, wrapped_db):
+    def __init__(self, wrapped_db: BaseDB) -> None:
         self._wrapped_db = wrapped_db
 
-    def get(self, key):
+    def get(self, key: bytes) -> bytes:
         return self._wrapped_db.get(key)
 
-    def set(self, key, value):
+    def set(self, key: bytes, value: bytes) -> None:
         raise TypeError("Current database is immutable and does not support setting of keys.")
 
-    def exists(self, key):
+    def exists(self, key: bytes) -> bool:
         return self._wrapped_db.exists(key)
 
-    def delete(self, key):
+    def delete(self, key: bytes) -> None:
         raise TypeError("Current database is immutable and does not support setting of keys.")
