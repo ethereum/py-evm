f3a6be0504f2697528aa735a17899e356d168f1f
mhchia <kevin.mh.chia@gmail.com>
2018-03-30 09:11:01 +0800

    Restructure and remove old code

--- evm/vm/forks/sharding/guess_head_state_manager.py
+++ evm/vm/forks/sharding/guess_head_state_manager.py
@@ -9,7 +9,6 @@ import logging
 from evm.vm.forks.sharding.constants import (
     GENESIS_COLLATION_HASH,
 )
-
 from evm.vm.forks.sharding.shard_tracker import (
     NoCandidateHead,
 )
@@ -51,16 +50,6 @@ async def fetch_and_verify_collation(collation_hash):
     return await verify_collation(collation, collation_hash)
 
 
-def create_collation(parent_hash, data):
-    # return collation with data with head = candidate_head
-    # ablalalala
-    print("!@# create_collation: parent_hash={}, data={}".format(
-        parent_hash,
-        data,
-    ))
-    return True
-
-
 def clean_done_task(tasks):
     return [task for task in tasks if not task.done()]
 
@@ -72,9 +61,7 @@ class GuessHeadStateManager:
     logger = logging.getLogger("evm.chain.sharding.GuessHeadStateManager")
 
     collation_validity_cache = None
-    head_validity = None
-    head_collation_hash = None
-    current_collation_hash = None
+    chain_validity = None
     last_period_fetching_candidate_head = None
     tasks = None
     collation_task = None
@@ -91,7 +78,7 @@ class GuessHeadStateManager:
         self.collation_validity_cache = {}
         # map[chain_head] -> validity
         # this should be able to be updated by the collations' validity
-        self.head_validity = defaultdict(lambda: True)
+        self.chain_validity = defaultdict(lambda: True)
         # list of chain head, to indicate priority
         # order: older -------> newer
         self.last_period_fetching_candidate_head = 0
@@ -139,7 +126,7 @@ class GuessHeadStateManager:
         collation_validity = await asyncio.wait_for(task, None)
         self.collation_validity_cache[collation_hash] = collation_validity
         if not collation_validity:
-            self.head_validity[head_collation_hash] = False
+            self.chain_validity[head_collation_hash] = False
 
     def fetch_candidate_head_hash(self):
         head_collation_hash = None
@@ -148,27 +135,8 @@ class GuessHeadStateManager:
             head_collation_hash = head_collation_dict['header'].hash
         except NoCandidateHead:
             self.logger.debug("No candidate head available, `guess_head` stops")
-            raise
         self.last_period_fetching_candidate_head = self.get_current_period()
         return head_collation_hash
-
-    def try_change_head(self):
-        # check for head changing
-        current_period = self.get_current_period()
-        # if head and current are None in the same time, it means it is just started up
-        # should try to get the head for it
-        if ((self.head_collation_hash is None and self.current_collation_hash is None) or
-                (current_period > self.last_period_fetching_candidate_head) or
-                (not self.head_validity[self.head_collation_hash])):
-            if current_period > self.last_period_fetching_candidate_head:
-                # TODO: should check if it is correct
-                # flush old candidate heads first, since all those candidates are stale
-                # self.shard_tracker.clean_logs()
-                pass
-            # perform head changing
-            self.head_collation_hash = self.fetch_candidate_head_hash()
-            if self.head_collation_hash is not None:
-                self.current_collation_hash = self.head_collation_hash
 
     def set_collation_task(self, head_collation_hash, current_collation_hash, task):
         self.tasks.append(task)
@@ -182,87 +150,75 @@ class GuessHeadStateManager:
             for collation_hash in self.chain_collations[head_collation_hash]
         ]
 
-    def process_current_collation(self):
-        # only process collations when the node is collating
-        if self.head_collation_hash is None:
-            return
-        if (self.current_collation_hash is None or
-                self.current_collation_hash == GENESIS_COLLATION_HASH):
-            return
-        if self.current_collation_hash not in self.collation_validity_cache:
-            # process current collation
-            coro = self.verify_collation(
-                self.head_collation_hash,
-                self.current_collation_hash,
-            )
-            task = asyncio.ensure_future(coro)
-            asyncio.wait(task)
-            self.set_collation_task(self.head_collation_hash, self.current_collation_hash, task)
-        if self.current_collation_hash != GENESIS_COLLATION_HASH:
-            self.current_collation_hash = self.vmc.get_parent_hash(
-                self.shard_id,
-                self.current_collation_hash,
-            )
+    async def guess_head(self):
+        '''
+        Perform windback process.
+        returns None if there is no candidate head available in this period
+        '''
+        start = time.time()
 
-    def try_create_collation(self, enable_running_out_of_time=False):
-        # Check if it is time to collate  #################################
-        if self.head_collation_hash is None:
-            return None
-        # if verified enough collations
-        # TODO: be replaced by the windback length checks
-        if self.current_collation_hash == GENESIS_COLLATION_HASH:
-            candidate_chain_tasks = self.get_chain_tasks(self.head_collation_hash)
-            if len(candidate_chain_tasks) != 0:
-                # check if all of the tasks in the chain are done
-                is_tasks_done = True
-                for task in candidate_chain_tasks:
-                    is_tasks_done &= task.done()
-                candidate_chain_tasks = clean_done_task(candidate_chain_tasks)
-                if not is_tasks_done:
-                    return None
-            # ensure all of the collations are done and valid
-            if not self.head_validity[self.head_collation_hash]:
-                return None
-        # or running out of time
-        elif enable_running_out_of_time and self.is_late_collator_period():
-            pass
-        # other cases, should not create collation
-        else:
-            return None
-        head_collation_hash = self.head_collation_hash
-        create_collation(head_collation_hash, data="123")
-        self.head_collation_hash = self.current_collation_hash = None
-        return head_collation_hash
-
-    async def async_loop_main(self, stop_after_create_collation=False):
-        # At any time, there should be only one `head_collation`
-        # The timing where `head_collation_hash` change are:
-        #    1. `head_collation` is invalid, change to the new head
-        #    2. There are new logs, and `fetch_candidate_head` is called
-        # When to stop processing collation?
-        self.head_collation_hash = self.current_collation_hash = None
+        head_collation_hash = current_collation_hash = None
         self.tasks = []
         while True:
-            if self.is_collator_in_lookahead_periods():
-                try:
-                    self.try_change_head()
-                except NoCandidateHead:
-                    return None
-                self.process_current_collation()
-                parent_hash = self.try_create_collation()
-                if parent_hash is not None:
-                    return parent_hash
+            # discard old logs if we're in the new period
+            if self.get_current_period() > self.last_period_fetching_candidate_head:
+                self.shard_tracker.clean_logs()
+            head_collation_hash = self.fetch_candidate_head_hash()
+            if head_collation_hash is None:
+                break
+            current_collation_hash = head_collation_hash
+            while self.chain_validity[head_collation_hash]:
+                # if self.is_late_collator_period():
+                #     return head_collation_hash
 
-            # clean up the finished tasks
-            if len(self.tasks) != 0:
-                await asyncio.wait(self.tasks, timeout=self.TIME_ONE_STEP)
-                not_finished_tasks = clean_done_task(self.tasks)
-                self.tasks = not_finished_tasks
+                # when `WINDBACK_LENGTH` is reached
+                # TODO: assume the whole chain for now
+                if current_collation_hash == GENESIS_COLLATION_HASH:
+                    while self.chain_validity[head_collation_hash]:
+                        # if self.is_late_collator_period():
+                        #     return head_collation_hash
+                        candidate_chain_tasks = self.get_chain_tasks(head_collation_hash)
+                        is_chain_tasks_done = all([task.done() for task in candidate_chain_tasks])
+                        if is_chain_tasks_done:
+                            break
+                        await asyncio.wait(candidate_chain_tasks, timeout=self.TIME_ONE_STEP)
+                    # if we break from `while`, either the chain is done or chain is invalid
+                    # Case 1:
+                    #   if the chain_validity is True, it means chain_tasks are done and
+                    #   chain_validity is still True, return
+                    if self.chain_validity[head_collation_hash]:
+                        print("!@# time elapsed={}".format(time.time() - start))
+                        return head_collation_hash
+                    # Case 2:
+                    #   chain_validity is False, change to other candidate head
+                    else:
+                        break
+                # process current collation
+                if current_collation_hash not in self.collation_validity_cache:
+                    coro = self.verify_collation(
+                        head_collation_hash,
+                        current_collation_hash,
+                    )
+                    task = asyncio.ensure_future(coro)
+                    asyncio.wait(task, timeout=self.TIME_ONE_STEP)
+                    self.set_collation_task(head_collation_hash, current_collation_hash, task)
 
-            # yield the CPU to other coroutine
-            await asyncio.sleep(self.TIME_ONE_STEP)
+                current_collation_hash = self.vmc.get_parent_hash(
+                    self.shard_id,
+                    current_collation_hash,
+                )
 
-    def async_daemon(self, stop_after_create_collation=False):
+                # clean up the finished tasks, yield CPU to tasks
+                if len(self.tasks) != 0:
+                    await asyncio.wait(self.tasks, timeout=self.TIME_ONE_STEP)
+                    not_finished_tasks = clean_done_task(self.tasks)
+                    self.tasks = not_finished_tasks
+                # yield the CPU to other coroutine
+                await asyncio.sleep(self.TIME_ONE_STEP)
+        print("!@# time elapsed={}".format(time.time() - start))
+        return head_collation_hash
+
+    def run_guess_head(self):
         loop = asyncio.get_event_loop()
-        result = loop.run_until_complete(self.async_loop_main(stop_after_create_collation))
+        result = loop.run_until_complete(self.guess_head())
         return result
--- tests/sharding/test_guess_head_state_manager.py
+++ tests/sharding/test_guess_head_state_manager.py
@@ -17,7 +17,7 @@ from tests.sharding.fixtures import (  # noqa: F401
 )
 
 
-# def test_guess_head_state_manager_async_loop_main(ghs_manager, vmc):
+# def test_guess_head_async(ghs_manager, vmc):
 #     async def colhdr_generator():
 #         head_header_hash = GENESIS_COLLATION_HASH
 #         for _ in range(5):
@@ -41,109 +41,46 @@ from tests.sharding.fixtures import (  # noqa: F401
 #     loop.run_until_complete(main())
 
 
-def test_no_new_candidate_head(ghs_manager, vmc):  # noqa: F811
-    assert ghs_manager.async_daemon(stop_after_create_collation=True) is None
+def test_guess_head_no_new_collations(ghs_manager, vmc):
+    assert ghs_manager.run_guess_head() is None
 
 
-def test_daemon_async(ghs_manager, vmc):  # noqa: F811
-    # without fork
-    head_header_hash = mk_colhdr_chain(vmc, default_shard_id, 6)
-    assert ghs_manager.async_daemon(stop_after_create_collation=True) == head_header_hash
-
-
-def test_daemon_without_fork(ghs_manager, vmc):  # noqa: F811
-    # without fork
+def test_guess_head_without_fork(ghs_manager, vmc):
     header2_hash = mk_colhdr_chain(vmc, default_shard_id, 2)
-    assert ghs_manager.async_daemon(stop_after_create_collation=True) == header2_hash
+    assert ghs_manager.run_guess_head() == header2_hash
     header3_hash = mk_colhdr_chain(vmc, default_shard_id, 1, header2_hash)
-    assert ghs_manager.async_daemon(stop_after_create_collation=True) == header3_hash
+    assert ghs_manager.run_guess_head() == header3_hash
 
 
-def test_daemon_with_fork(ghs_manager, vmc):  # noqa: F811
+def test_guess_head_with_fork(ghs_manager, vmc):  # noqa: F811
     # without fork
     mk_colhdr_chain(vmc, default_shard_id, 2)
-    header0_3_prime_hash = mk_colhdr_chain(vmc, default_shard_id, 3)
+    header3_prime_hash = mk_colhdr_chain(vmc, default_shard_id, 3)
     # head changes
-    assert ghs_manager.async_daemon(stop_after_create_collation=True) == header0_3_prime_hash
+    assert ghs_manager.run_guess_head() == header3_prime_hash
 
 
-def test_invalid_chain(ghs_manager, vmc):  # noqa: F811
+def test_guess_head_invalid_chain(ghs_manager, vmc):  # noqa: F811
     # setup two collation header chains, both having length=3.
     # originally, guess_head should return the hash of canonical chain head `header0_3_hash`
     header3_hash = mk_colhdr_chain(vmc, default_shard_id, 3)
     header3_prime_hash = mk_colhdr_chain(vmc, default_shard_id, 3)
     ghs_manager.collation_validity_cache[header3_hash] = False
-    ghs_manager.head_validity[header3_hash] = False
+    ghs_manager.chain_validity[header3_hash] = False
     # the candidates is  [`header3`, `header3_prime`, `header2`, ...]
     # since the 1st candidate is invalid, `guess_head` should returns `header3_prime` instead
-    assert ghs_manager.async_daemon(stop_after_create_collation=True) == header3_prime_hash
+    assert ghs_manager.run_guess_head() == header3_prime_hash
 
 
-def wait_for_tasks_complete(ghs_manager):
-    '''
-    Used to complete the unfinished verification works
-    '''
-    event_loop = asyncio.get_event_loop()
-    for task in ghs_manager.tasks:
-        event_loop.run_until_complete(task)
+def test_guess_head_new_only_candidate_is_invalid(ghs_manager, vmc):  # noqa: F811
+    head_header_hash = mk_colhdr_chain(vmc, default_shard_id, 1)
+    ghs_manager.collation_validity_cache[head_header_hash] = False
+    ghs_manager.chain_validity[head_header_hash] = False
+    assert ghs_manager.run_guess_head() is None
 
 
-def test_step_by_step(ghs_manager, vmc):  # noqa: F811
-    # assume collations "A B C D E F" are added
-    head_collation_hash = mk_colhdr_chain(
-        vmc,
-        default_shard_id,
-        6,
-    )
-    current_collation_hash = head_collation_hash
-
-    # assume ghs_manager just found itself to be a collator in the lookahead periods,
-    # so it keep doing `guess_head`
-
-    # fetch one candidate and set it as the head
-    ghs_manager.try_change_head()
-    assert ghs_manager.head_collation_hash == head_collation_hash
-    assert ghs_manager.current_collation_hash == current_collation_hash
-    # process one collation of the current chain
-    ghs_manager.process_current_collation()
-    wait_for_tasks_complete(ghs_manager)
-    assert (
-        (current_collation_hash in ghs_manager.collation_validity_cache) and
-        ghs_manager.collation_validity_cache[current_collation_hash]
-    )
-
-    ghs_manager.try_change_head()
-    # `head_collation_hash` should remain the same since the chain is still valid
-    assert ghs_manager.head_collation_hash == head_collation_hash
-    current_collation_hash = vmc.get_parent_hash(default_shard_id, current_collation_hash)
-    assert ghs_manager.current_collation_hash == current_collation_hash
-    assert ghs_manager.head_collation_hash != ghs_manager.current_collation_hash
-    # if current_collation is verified
-    ghs_manager.process_current_collation()
-    wait_for_tasks_complete(ghs_manager)
-    assert (
-        (current_collation_hash in ghs_manager.collation_validity_cache) and
-        ghs_manager.collation_validity_cache[current_collation_hash]
-    )
-
-    # one period elapses and a new collation created
-    new_head_hash = mk_colhdr_chain(
-        vmc,
-        default_shard_id,
-        3,
-        top_collation_hash=head_collation_hash,
-    )
-    # since new collation head is added, head should be changed
-    ghs_manager.try_change_head()
-    assert ghs_manager.head_collation_hash == new_head_hash
-    # and because head collation changed, we start verifying the chain from the head
-    assert ghs_manager.current_collation_hash == new_head_hash
-    ghs_manager.process_current_collation()
-    wait_for_tasks_complete(ghs_manager)
-
-    # although we don't have enough time to verify all collation in this chain, we still guess that
-    # the chain head is `head_collation`
-    ghs_manager.try_change_head()
-    ghs_manager.process_current_collation()
-    wait_for_tasks_complete(ghs_manager)
-    assert ghs_manager.try_create_collation(enable_running_out_of_time=True) == new_head_hash
+def test_guess_head_new_only_candidate_is_not_longest(ghs_manager, vmc):
+    mk_colhdr_chain(vmc, default_shard_id, 3)
+    ghs_manager.run_guess_head()
+    mk_colhdr_chain(vmc, default_shard_id, 1)
+    assert ghs_manager.run_guess_head() is None
