8e744568ffc312abc4c9e15ce9bb6e84eb9c1602
mhchia <kevin.mh.chia@gmail.com>
2018-04-07 18:39:43 +0800

    Fix ShardTracker, add test for logs in batches

--- evm/vm/forks/sharding/shard_tracker.py
+++ evm/vm/forks/sharding/shard_tracker.py
@@ -1,13 +1,5 @@
 import copy
 
-from cytoolz import (
-    pipe,
-)
-
-from eth_utils import (
-    to_tuple,
-)
-
 from evm.rlp.headers import (
     CollationHeader,
 )
@@ -25,27 +17,19 @@ from evm.utils.numeric import (
 COLLATION_ADDED_EVENT_NAME = "CollationAdded"
 
 
-class NextLogUnavailable(Exception):
-    pass
-
-
-class NoCandidateHead(Exception):
-    pass
-
-
 def parse_collation_added_log(log):
     # here assume `shard_id` is the first indexed , which is the second element in topics
     shard_id_bytes32 = log['topics'][1]
     data_hex = log['data']
     data_bytes = decode_hex(data_hex)
-    score = big_endian_to_int(data_bytes[-32:])
+    height = big_endian_to_int(data_bytes[-32:])
     is_new_head = bool(big_endian_to_int(data_bytes[-64:-32]))
     header_bytes = shard_id_bytes32 + data_bytes[:-64]
     collation_header = CollationHeader.from_bytes(header_bytes)
     return {
         'header': collation_header,
         'is_new_head': is_new_head,
-        'score': score,
+        'height': height,
     }
 
 
@@ -59,11 +43,12 @@ class ShardTracker:
     '''Track logs `CollationAdded` in mainchain
     '''
 
-    current_score = None
-    new_logs = None
-    unchecked_logs = None
-    headers_by_height = None
+    shard_id = None
+    log_handler = None
+    smc_address = None
     collation_added_topic = None
+
+    headers_by_height = None
 
     def __init__(self, shard_id, log_handler, smc_address, collation_added_topic):
         # TODO: currently set one log_handler for each shard. Should see if there is a better way
@@ -72,9 +57,6 @@ class ShardTracker:
         self.log_handler = log_handler
         self.smc_address = smc_address
         self.collation_added_topic = collation_added_topic
-        self.current_score = None
-        self.new_logs = []
-        self.unchecked_logs = []
 
         # for the alternative `fetch_candidate_head`
         # TODO: might need to be a sliding window, discarding the headers which are no longer
@@ -84,77 +66,29 @@ class ShardTracker:
         #       should still be reachable.
         self.headers_by_height = []
 
-    @to_tuple
-    def _get_new_logs(self):
+    def new_logs(self):
         shard_id_topic_hex = encode_hex(self.shard_id.to_bytes(32, byteorder='big'))
-        new_logs = self.log_handler.get_new_logs(
+        logs = self.log_handler.get_new_logs(
             address=self.smc_address,
             topics=[
                 encode_hex(self.collation_added_topic),
                 shard_id_topic_hex,
             ],
         )
-        for log in new_logs:
+        for log in logs:
             yield parse_collation_added_log(log)
 
-    def get_next_log(self):
-        new_logs = self._get_new_logs()
-        self.new_logs.extend(new_logs)
-        if len(self.new_logs) == 0:
-            raise NextLogUnavailable("No more next logs")
-        return self.new_logs.pop()
-
-    # TODO: this method may return wrong result when new logs arrive before the logs inside
-    #       `self.new_logs` are consumed entirely. This issue can be resolved by saving the
-    #       status of `new_logs`, `unchecked_logs`, and `current_score`, when it start to run
-    #       `GUESS_HEAD`. If there is a new block arriving, just restore them to the saved status,
-    #       append new logs to `new_logs`, and re-run `GUESS_HEAD`
-    def fetch_candidate_head(self):
-        # Try to return a log that has the score that we are checking for,
-        # checking in order of oldest to most recent.
-        unchecked_logs = pipe(
-            self.unchecked_logs,
-            enumerate,
-            tuple,
-            reversed,
-            tuple,
-        )
-        for idx, log_entry in unchecked_logs:
-            if log_entry['score'] == self.current_score:
-                return self.unchecked_logs.pop(idx)
-        # If no further recorded but unchecked logs exist, go to the next
-        # is_new_head = true log
-        while True:
-            try:
-                log_entry = self.get_next_log()
-            # TODO: currently just raise when there is no log anymore
-            except NextLogUnavailable:
-                # TODO: should returns the genesis collation instead or just leave it?
-                raise NoCandidateHead("No candidate head available")
-            if log_entry['is_new_head']:
-                break
-            self.unchecked_logs.append(log_entry)
-        self.current_score = log_entry['score']
-        return log_entry
-
-    def clean_logs(self):
-        self.new_logs = []
-        self.unchecked_logs = []
-
     def add_log(self, log_entry):
-        collation_height = log_entry['score']
+        collation_height = log_entry['height']
         while len(self.headers_by_height) <= collation_height:
             self.headers_by_height.append([])
         self.headers_by_height[collation_height].append(log_entry['header'])
 
-    def fetch_candidate_heads_generator(self, windback_length):
-        new_logs = self._get_new_logs()
-        for log_entry in new_logs:
+    def fetch_candidate_heads_generator(self):
+        for log_entry in self.new_logs():
             # TODO: we can use another coroutine to sync the logs concurrently, instead of doing
             #       that in this function
             self.add_log(log_entry)
         # TODO: deepcopy seems to be costly
         headers_by_height = copy.deepcopy(self.headers_by_height)
-        max_height = len(headers_by_height) - 1
-        windback_index = max(0, max_height + 1 - windback_length)
-        return candidate_heads(headers_by_height, windback_index, max_height)
+        return candidate_heads(headers_by_height, 0, len(headers_by_height) - 1)
--- tests/sharding/test_shard_tracker.py
+++ tests/sharding/test_shard_tracker.py
@@ -5,8 +5,6 @@ from evm.rlp.headers import (
 )
 
 from evm.vm.forks.sharding.shard_tracker import (
-    NextLogUnavailable,
-    NoCandidateHead,
     parse_collation_added_log,
 )
 
@@ -19,7 +17,7 @@ from tests.sharding.fixtures import (  # noqa: F401
 
 
 @pytest.mark.parametrize(
-    'log, expected_header_dict, expected_is_new_head, expected_score',
+    'log, expected_header_dict, expected_is_new_head, expected_height',
     (
         (
             {'type': 'mined', 'logIndex': 0, 'transactionIndex': 0, 'transactionHash': b'\xda\xb8:\xe5\x86\xe9Q\xf2\x9c\xc6<g\x9bl\x84\x85\xf4\x1dh\xce\x8d\xe6\xc0D\xa0*E\xd8m\xd4\x01\xcf', 'blockHash': b'\x13\xa97d\r\x90t\xe5;\x84\xf9\xe0\xb8\xf2c\x1c}\x88\xbf\x84DN\xa0\x16Q\xd9|\xa1\x00\x91\xc0\xbd', 'blockNumber': 25, 'address': '0xf4F1600B0a65995833854738764b50A4DA8d6BE1', 'data': '0x000000000000000000000000000000000000000000000000000000000000000534c998a5b8325a1276f385558aae7f5c3f8a40023d289f39649d2fcdd7d49100000000000000000000000000000000000000000000000000000000000000000074785f6c6973742074785f6c6973742074785f6c6973742074785f6c697374200000000000000000000000007e5f4552091a69125d5dfcb7b8c2659029395bdf706f73745f737461706f73745f737461706f73745f737461706f73745f7374617265636569707420726563656970742072656365697074207265636569707420000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000001', 'topics': [b'\x95\x86g\xed\xf5J\xea\x9d\xfa[\xee!\xb2\xb4\x9f|\x11D\xe4[\xa0h"\xa3\xa5\x8fc\x90\xa9\xa1\xc5C', b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00']},  # noqa: E501
@@ -38,80 +36,142 @@ from tests.sharding.fixtures import (  # noqa: F401
 def test_parse_collation_added_log(log,
                                    expected_header_dict,
                                    expected_is_new_head,
-                                   expected_score):
+                                   expected_height):
     parsed_data = parse_collation_added_log(log)
     assert parsed_data['header'] == CollationHeader(**expected_header_dict)
     assert parsed_data['is_new_head'] == expected_is_new_head
-    assert parsed_data['score'] == expected_score
+    assert parsed_data['height'] == expected_height
 
 
-def test_shard_tracker_get_next_log(smc_handler):  # noqa: F811
+def test_shard_tracker_new_logs(smc_handler):  # noqa: F811
     shard_tracker0 = shard_tracker(smc_handler, 0)
-    header2_hash = make_collation_header_chain(smc_handler, 0, 2)
+    make_collation_header_chain(smc_handler, 0, 2)
     # confirm the logs are correct
-    assert shard_tracker0.get_next_log()['score'] == 2
-    assert shard_tracker0.get_next_log()['score'] == 1
-    with pytest.raises(NextLogUnavailable):
-        shard_tracker0.get_next_log()
-
-    # ensure that `get_next_log(0)` does not affect `get_next_log(1)`
-    shard_tracker1 = shard_tracker(smc_handler, 1)
-    make_collation_header_chain(smc_handler, 0, 1, header2_hash)
-    make_collation_header_chain(smc_handler, 1, 1)
-    assert shard_tracker0.get_next_log()['score'] == 3
-    assert shard_tracker1.get_next_log()['score'] == 1
+    new_logs_generator = shard_tracker0.new_logs()
+    assert next(new_logs_generator)['height'] == 1
+    assert next(new_logs_generator)['height'] == 2
 
 
 @pytest.mark.parametrize(  # noqa: F811
-    'mock_score,mock_is_new_head,expected_score,expected_is_new_head',
+    'mock_height,mock_period,expected_height,expected_period',
     (
         # test case in doc.md
         (
             (10, 11, 12, 11, 13, 14, 15, 11, 12, 13, 14, 12, 13, 14, 15, 16, 17, 18, 19, 16),
-            (True, True, True, False, True, True, True, False, False, False, False, False, False, False, False, True, True, True, True, False),  # noqa: E501
+            (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19),
             (19, 18, 17, 16, 16, 15, 15, 14, 14, 14, 13, 13, 13, 12, 12, 12, 11, 11, 11, 10),
-            (True, True, True, True, False, True, False, True, False, False, True, False, False, True, False, False, True, False, False, True),  # noqa: E501
+            (18, 17, 16, 15, 19, 6, 14, 5, 10, 13, 4, 9, 12, 2, 8, 11, 1, 3, 7, 0),
         ),
         (
             (1, 2, 3, 2, 2, 2),
-            (True, True, True, False, False, False),
+            (0, 1, 2, 3, 4, 5),
             (3, 2, 2, 2, 2, 1),
-            (True, True, False, False, False, True),
+            (2, 1, 3, 4, 5, 0),
         ),
     )
 )
-def test_shard_tracker_fetch_candidate_head(smc_handler,
-                                            mock_score,
-                                            mock_is_new_head,
-                                            expected_score,
-                                            expected_is_new_head):
+def test_fetch_candidate_heads_generator(smc_handler,
+                                         mock_height,
+                                         mock_period,
+                                         expected_height,
+                                         expected_period,
+                                         monkeypatch):
     shard_tracker0 = shard_tracker(smc_handler, 0)
+
     mock_collation_added_logs = [
         {
-            'header': [None] * 10,
-            'score': mock_score[i],
-            'is_new_head': mock_is_new_head[i],
-        } for i in range(len(mock_score))
+            'header': CollationHeader(
+                shard_id=0,
+                expected_period_number=mock_period[i],
+                period_start_prevhash=b'\x00' * 32,
+                parent_hash=b'\x00' * 32,
+                number=mock_height[i],
+            ),
+            'height': mock_height[i],
+            'is_new_head': None,
+        } for i in range(len(mock_height))
     ]
-    # mock collation_added_logs
-    shard_tracker0.new_logs = mock_collation_added_logs
-    for i in range(len(mock_score)):
-        log = shard_tracker0.fetch_candidate_head()
-        assert log['score'] == expected_score[i]
-        assert log['is_new_head'] == expected_is_new_head[i]
-    with pytest.raises(NoCandidateHead):
-        log = shard_tracker0.fetch_candidate_head()
 
+    def mock_shard_tracker_new_logs(self):
+        for log in mock_collation_added_logs:
+            yield log
 
-def test_fetch_candidate_heads_generator(smc_handler):  # noqa: F811
-    shard_tracker0 = shard_tracker(smc_handler, 0)
-    make_collation_header_chain(smc_handler, 0, 2)
-    make_collation_header_chain(smc_handler, 0, 1)
-    candidate_heads = shard_tracker0.fetch_candidate_heads_generator(
-        smc_handler.config['WINDBACK_LENGTH']
+    monkeypatch.setattr(
+        'evm.vm.forks.sharding.shard_tracker.ShardTracker.new_logs',
+        mock_shard_tracker_new_logs,
     )
-    assert next(candidate_heads).number == 2
-    assert next(candidate_heads).number == 1
-    assert next(candidate_heads).number == 1
+    candidate_heads = shard_tracker0.fetch_candidate_heads_generator()
+    for idx in range(len(expected_height)):
+        candidate_header = next(candidate_heads)
+        assert candidate_header.number == expected_height[idx]
+        assert candidate_header.expected_period_number == expected_period[idx]
+
+    with pytest.raises(StopIteration):
+        next(candidate_heads)
+
+
+def test_fetch_candidate_heads_generator_batches_logs(smc_handler, monkeypatch):  # noqa: F811
+    shard_tracker0 = shard_tracker(smc_handler, 0)
+
+    mock_height_batch1 = (10, 11, 12, 11, 13, 14, 15, 11, 12, 13)
+    mock_period_batch1 = tuple(range(10))
+    expected_height_after_batch1 = (15, 14, 13, 13, 12, 12, 11, 11, 11, 10)
+    expected_period_after_batch1 = (6, 5, 4, 9, 2, 8, 1, 3, 7, 0)
+
+    mock_height_batch2 = (14, 12, 13, 14, 15, 16, 17, 18, 19, 16)
+    mock_period_batch2 = tuple(range(10, 20))
+    expected_height_after_batch2 = (19, 18, 17, 16, 16, 15, 15, 14, 14, 14, 13, 13, 13, 12, 12, 12, 11, 11, 11, 10)  # noqa: E501
+    expected_period_after_batch2 = (18, 17, 16, 15, 19, 6, 14, 5, 10, 13, 4, 9, 12, 2, 8, 11, 1, 3, 7, 0)  # noqa: E501
+
+    mock_collation_added_logs = []
+
+    mock_collation_added_logs.extend((
+        {
+            'header': CollationHeader(
+                shard_id=0,
+                expected_period_number=mock_period_batch1[i],
+                period_start_prevhash=b'\x00' * 32,
+                parent_hash=b'\x00' * 32,
+                number=mock_height_batch1[i],
+            ),
+            'height': mock_height_batch1[i],
+            'is_new_head': None,
+        } for i in range(len(mock_height_batch1))
+    ))
+
+    def mock_shard_tracker_new_logs(self):
+        for _ in range(len(mock_collation_added_logs)):
+            yield mock_collation_added_logs.pop(0)
+
+    monkeypatch.setattr(
+        'evm.vm.forks.sharding.shard_tracker.ShardTracker.new_logs',
+        mock_shard_tracker_new_logs,
+    )
+    candidate_heads = shard_tracker0.fetch_candidate_heads_generator()
+    for idx in range(len(expected_height_after_batch1)):
+        candidate_header = next(candidate_heads)
+        assert candidate_header.number == expected_height_after_batch1[idx]
+        assert candidate_header.expected_period_number == expected_period_after_batch1[idx]
+
+    assert len(mock_collation_added_logs) == 0
+
+    mock_collation_added_logs.extend((
+        {
+            'header': CollationHeader(
+                shard_id=0,
+                expected_period_number=mock_period_batch2[i],
+                period_start_prevhash=b'\x00' * 32,
+                parent_hash=b'\x00' * 32,
+                number=mock_height_batch2[i],
+            ),
+            'height': mock_height_batch2[i],
+            'is_new_head': None,
+        } for i in range(len(mock_height_batch2))
+    ))
+    candidate_heads = shard_tracker0.fetch_candidate_heads_generator()
+    for idx in range(len(expected_height_after_batch2)):
+        candidate_header = next(candidate_heads)
+        assert candidate_header.number == expected_height_after_batch2[idx]
+        assert candidate_header.expected_period_number == expected_period_after_batch2[idx]
     with pytest.raises(StopIteration):
         next(candidate_heads)
