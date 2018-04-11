21468dac1e0ba6ca8d236ca1ee4f42641ab52081
Jannik Luhn <jannik.luhn@brainbot.com>
2018-04-11 12:13:13 +0800

    Update Collation according to minimal sharding spec

--- evm/rlp/collations.py
+++ evm/rlp/collations.py
@@ -1,9 +1,5 @@
 import rlp
 
-from eth_utils import (
-    encode_hex,
-)
-
 from evm.utils.datatypes import (
     Configurable,
 )
@@ -20,16 +16,8 @@ class Collation(rlp.Serializable, Configurable):
     ]
 
     @property
-    def hash(self) -> bytes:
-        return self.header.hash
-
-    @property
     def shard_id(self) -> int:
         return self.header.shard_id
-
-    @property
-    def parent_hash(self) -> bytes:
-        return self.header.parent_hash
 
     @property
     def chunk_root(self) -> bytes:
@@ -40,33 +28,11 @@ class Collation(rlp.Serializable, Configurable):
         return self.header.period
 
     @property
-    def height(self) -> int:
-        return self.header.height
-
-    @property
     def proposer_address(self) -> bytes:
         return self.header.proposer_address
 
-    @property
-    def proposer_bid(self) -> int:
-        return self.header.proposer_bid
-
-    @property
-    def proposer_signature(self) -> bytes:
-        return self.header.proposer_signature
-
     def __repr__(self) -> str:
-        return "<Collation {} shard={} height={}>".format(
-            self.__class__.__name__,
-            encode_hex(self.hash)[2:10],
+        return "<Collation shard={} period={}>".format(
             self.shard_id,
-            self.height,
-        )
-
-    def __str__(self) -> str:
-        return "Collation {} shard={} height={} period={}".format(
-            encode_hex(self.hash)[2:10],
-            self.shard_id,
-            self.height,
             self.period,
         )
