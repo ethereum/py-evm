be0c1fe84618cdd82fb512c1f7ca63dbb2b438c8
Hsiao-Wei Wang <hwwang156@gmail.com>
2018-04-23 16:37:16 +0800

    Test p2p handshake
    1. Added `wait_with_token()` for Reader.read()
    2. Moved `decode_authentication` to p2p/auth.py
    3. Added a testing daemon in server.py for simple testing
    4. Added a `test_two_servers`: testing two nodes
    P2P handshake
    5. Modified `test_server_authenticates_incoming_connections`: adding `await reader.read()` to replace `await asyncio.sleep(0.2)`

--- p2p/constants.py
+++ p2p/constants.py
@@ -60,3 +60,7 @@ ROPSTEN_BOOTNODES = [
     # 'enode://6332792c4a00e3e4ee0926ed89e0d27ef985424d97b6a45bf0f23e51f0dcb5e66b875777506458aea7af6f9e4ffb69f43f3778ee73c81ed9d34c51c4b16b0b0f@52.232.243.152:30303',  # noqa: E501
     # 'enode://94c15d1b9e2fe7ce56e458b9a3b672ef11894ddedd0c6f247e0f1d3487f52b66208fb4aeb8179fce6e3a749ea93ed147c37976d67af557508d199d9594c35f09@192.81.208.223:30303',  # noqa: E501
 ]
+
+# Default configuration
+# Minimum peers number, we'll try to keep open connections to at least this number of peers
+DEFAULT_MIN_PEERS = 5
