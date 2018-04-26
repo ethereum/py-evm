761bcce8776f40ee68c1b42d2bf5dc6fd34e3d01
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2018-04-26 14:05:09 +0200

    Add common base exception to all p2p exceptions
    Closes #591

--- p2p/exceptions.py
+++ p2p/exceptions.py
@@ -1,54 +1,96 @@
-class AuthenticationError(Exception):
+class BaseP2PError(Exception):
+    """
+    The base class for all p2p errors.
+    """
     pass
 
 
-class DecryptionError(Exception):
+class AuthenticationError(BaseP2PError):
+    """
+    Raised when a message could not be decrypted.
+    """
     pass
 
 
-class PeerConnectionLost(Exception):
+class DecryptionError(BaseP2PError):
+    """
+    Raised when a message could not be decrypted.
+    """
     pass
 
 
-class HandshakeFailure(Exception):
+class PeerConnectionLost(BaseP2PError):
+    """
+    Raised when the connection to a peer was lost.
+    """
     pass
 
 
-class UnknownProtocolCommand(Exception):
+class HandshakeFailure(BaseP2PError):
+    """
+    Raised when the protocol handshake was unsuccessful.
+    """
     pass
 
 
-class UnexpectedMessage(Exception):
+class UnknownProtocolCommand(BaseP2PError):
+    """
+    Raised when the received protocal command isn't known.
+    """
     pass
 
 
-class UnreachablePeer(Exception):
+class UnexpectedMessage(BaseP2PError):
+    """
+    Raised when the received message was unexpected.
+    """
     pass
 
 
-class EmptyGetBlockHeadersReply(Exception):
+class UnreachablePeer(BaseP2PError):
+    """
+    Raised when a peer was unreachable.
+    """
     pass
 
 
-class LESAnnouncementProcessingError(Exception):
+class EmptyGetBlockHeadersReply(BaseP2PError):
+    """
+    Raised when the received block headers were empty.
+    """
     pass
 
 
-class TooManyTimeouts(Exception):
+class LESAnnouncementProcessingError(BaseP2PError):
+    """
+    Raised when an LES announcement could not be processed.
+    """
     pass
 
 
-class PeerFinished(Exception):
+class TooManyTimeouts(BaseP2PError):
+    """
+    Raised when too many timeouts occurred.
+    """
     pass
 
 
-class OperationCancelled(Exception):
+class OperationCancelled(BaseP2PError):
+    """
+    Raised when an operation was cancelled.
+    """
     pass
 
 
-class NoMatchingPeerCapabilities(Exception):
+class NoMatchingPeerCapabilities(BaseP2PError):
+    """
+    Raised when no matching protocol between peers was found.
+    """
     pass
 
 
-class RemoteDisconnected(Exception):
+class RemoteDisconnected(BaseP2PError):
+    """
+    Raised when a remote disconnected.
+    """
     pass
