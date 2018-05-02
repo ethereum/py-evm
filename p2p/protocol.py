1d663896bd3e654542c16944d229c50c1e56e166
Guilherme Salgado <gsalgado@gmail.com>
2018-05-02 15:56:14 +0100

    p2p: Remove some leftover code added when debugging

--- p2p/protocol.py
+++ p2p/protocol.py
@@ -60,14 +60,7 @@ class Command:
             encoder = self.structure
         else:
             encoder = sedes.List([type_ for _, type_ in self.structure])
-        try:
-            return rlp.encode(data, sedes=encoder)
-        except Exception as arst:
-            import logging
-            logger = logging.getLogger('p2p')
-            logger.error('DATA WAS %s', data)
-            logger.error('Encoder WAS %s', encoder)
-            raise
+        return rlp.encode(data, sedes=encoder)
 
     def decode_payload(self, rlp_data: bytes) -> _DecodedMsgType:
         if isinstance(self.structure, sedes.CountableList):
