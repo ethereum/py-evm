558f3b7b7f693742dbe182404336bb9cc677d47a
Jannik Luhn <jannik.luhn@brainbot.com>
2018-05-09 19:25:24 +0200

    Add more sharding network tests

--- tests/p2p/test_sharding.py
+++ tests/p2p/test_sharding.py
@@ -30,7 +30,6 @@ from p2p.sharding import (
     ShardingPeer,
     Collations,
     ShardSyncer,
-    COLLATION_PERIOD,
 )
 
 from p2p.exceptions import (
@@ -135,33 +134,35 @@ async def test_sending_collations(request, event_loop):
         None,
     )
 
-    incoming_collation_queue = asyncio.Queue()
-    receiver.set_incoming_collation_queue(incoming_collation_queue)
-
     c1 = Collation(CollationHeader(0, b"\x11" * 32, 2, b"\x33" * 20), b"\x44" * COLLATION_SIZE)
     c2 = Collation(CollationHeader(1, b"\x11" * 32, 2, b"\x33" * 20), b"\x44" * COLLATION_SIZE)
     c3 = Collation(CollationHeader(2, b"\x11" * 32, 2, b"\x33" * 20), b"\x44" * COLLATION_SIZE)
 
     sender.sub_proto.send_collations([c1])
-    received_c1 = await asyncio.wait_for(incoming_collation_queue.get(), timeout=1)
+    received_c1 = await asyncio.wait_for(receiver.incoming_collation_queue.get(), timeout=1)
     assert received_c1 == c1
     assert receiver.known_collation_hashes == set([c1.hash])
-    assert incoming_collation_queue.qsize() == 0
 
     sender.sub_proto.send_collations([c2, c3])
-    received_c2 = await asyncio.wait_for(incoming_collation_queue.get(), timeout=1)
-    received_c3 = await asyncio.wait_for(incoming_collation_queue.get(), timeout=1)
+    received_c2 = await asyncio.wait_for(receiver.incoming_collation_queue.get(), timeout=1)
+    received_c3 = await asyncio.wait_for(receiver.incoming_collation_queue.get(), timeout=1)
     assert set([received_c2, received_c3]) == set([c2, c3])
     assert receiver.known_collation_hashes == set([c1.hash, c2.hash, c3.hash])
-    assert incoming_collation_queue.qsize() == 0
 
 
 @pytest.mark.asyncio
-async def test_shard_syncer():
+@pytest.mark.parametrize("n_peers,connections", [
+    (2, [(0, 1)]),  # two peers connected directly with each other
+    (3, [(0, 1), (0, 2), (1, 2)]),  # three fully connected peers
+    (3, [(0, 1), (1, 2)]),  # three peers in a row
+    # 10 nodes randomly connected to ~4 peers each
+    # TODO: do something against time out because of long chunk root calculation time
+    # (10, set(tuple(sorted(random.sample(range(10), 2))) for _ in range(10 * 4))),
+])
+async def test_shard_syncer(n_peers, connections):
     cancel_token = CancelToken("canceltoken")
 
     PeerTuple = collections.namedtuple("PeerTuple", ["node", "server", "syncer", "syncer_task"])
-    n_peers = 2
     peer_tuples = []
 
     for i in range(n_peers):
@@ -184,7 +185,7 @@ async def test_shard_syncer():
 
         shard_db = ShardDB(MemoryDB())
         shard = Shard(shard_db, 0)
-        syncer = ShardSyncer(shard, COLLATION_PERIOD * 2, server.peer_pool, cancel_token)
+        syncer = ShardSyncer(shard, server.peer_pool, cancel_token)
         syncer_task = asyncio.ensure_future(syncer.run())
 
         peer_tuples.append(PeerTuple(
@@ -195,18 +196,31 @@ async def test_shard_syncer():
         ))
 
     # connect peers to each other
-    await peer_tuples[0].server.peer_pool._connect_to_nodes([peer_tuples[1].node])
-    assert len(peer_tuples[0].server.peer_pool.peers) == 1
+    await asyncio.gather(*[
+        peer_tuples[i].server.peer_pool._connect_to_nodes([peer_tuples[j].node])
+        for i, j in connections
+    ])
+    for i, j in connections:
+        peer_remotes = [peer.remote for peer in peer_tuples[i].server.peer_pool.peers]
+        assert peer_tuples[j].node in peer_remotes
 
-    # both syncers should propose and receive collations
-    await asyncio.wait_for(peer_tuples[0].syncer.collations_proposed_event.wait(), 10)
-    await asyncio.wait_for(peer_tuples[0].syncer.collations_received_event.wait(), 10)
-    await asyncio.wait_for(peer_tuples[1].syncer.collations_proposed_event.wait(), 10)
-    await asyncio.wait_for(peer_tuples[1].syncer.collations_received_event.wait(), 10)
+    # let each node propose and check that collation appears at all other nodes
+    for proposer in peer_tuples:
+        collation = proposer.syncer.propose()
+        await asyncio.wait_for(asyncio.gather(*[
+            peer_tuple.syncer.collations_received_event.wait()
+            for peer_tuple in peer_tuples
+            if peer_tuple != proposer
+        ]), timeout=10)
+        for peer_tuple in peer_tuples:
+            assert peer_tuple.syncer.shard.get_collation_by_hash(collation.hash) == collation
 
     # stop everything
-    await peer_tuples[0].server.stop()
-    await peer_tuples[1].server.stop()
     cancel_token.trigger()
-    with pytest.raises(OperationCancelled):
-        await asyncio.gather(*[peer_tuple.syncer_task for peer_tuple in peer_tuples])
+    await asyncio.gather(*[peer_tuple.server.stop() for peer_tuple in peer_tuples])
+    exceptions = await asyncio.gather(
+        *[peer_tuple.syncer_task for peer_tuple in peer_tuples],
+        return_exceptions=True,
+    )
+    for exception in exceptions:
+        assert isinstance(exception, OperationCancelled)
