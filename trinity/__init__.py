8d8abcafdebd4b7207dc665b46a607c36d1453cc
Piper Merriam <pipermerriam@gmail.com>
2018-05-17 16:59:32 -0600

    mypy cleanup

--- trinity/__init__.py
+++ trinity/__init__.py
@@ -1,7 +1,7 @@
 import pkg_resources
 
 # TODO: update this to use the `trinity` version once extracted from py-evm
-__version__ = pkg_resources.get_distribution("py-evm").version
+__version__: str = pkg_resources.get_distribution("py-evm").version
 
 from .main import (  # noqa: F401
     main,
