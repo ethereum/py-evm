6b73789563194826e2d9511df0fac01302d5623b
Piper Merriam <pipermerriam@gmail.com>
2018-05-22 15:37:36 -0600

    Try separating trinity and evm packages

---
+++ README-trinity.md
@@ -0,0 +1,8 @@
+# Trinity
+
+[Documentation hosted by ReadTheDocs](http://py-evm.readthedocs.io/en/latest/)
+
+
+## Introducing Py-EVM
+
+Trinity is a client for the Ethereum blockchain written in Python.
--- trinity/__init__.py
+++ trinity/__init__.py
@@ -1,7 +1,10 @@
 import pkg_resources
 
 # TODO: update this to use the `trinity` version once extracted from py-evm
-__version__: str = pkg_resources.get_distribution("py-evm").version
+try:
+    __version__: str = pkg_resources.get_distribution("trinity").version
+except pkg_resources.DistributionNotFound:
+    __version__: str = pkg_resources.get_distribution("py-evm").version
 
 from .main import (  # noqa: F401
     main,
