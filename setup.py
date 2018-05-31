2634023b2fe9dba87b8fdf49068cbbd1b5378fd8
Piper Merriam <pipermerriam@gmail.com>
2018-05-31 15:59:25 -0600

    Bump version: 0.2.0-alpha.20 → 0.2.0-alpha.21

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ install_requires = deps['evm'] + deps['p2p'] + deps['trinity']
 setup(
     name='py-evm',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='0.2.0-alpha.20',
+    version='0.2.0-alpha.21',
     description='Python implementation of the Ethereum Virtual Machine',
     long_description_markdown_filename='README.md',
     author='Piper Merriam',
