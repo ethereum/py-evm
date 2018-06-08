31ca2c309523b5d7c2f388b91e02cc1ff9699049
Jason Carver <ut96caarrs@snkmail.com>
2018-06-08 13:40:46 -0700

    Add TheDAO fork header.extra_data validation

--- evm/chains/mainnet/constants.py
+++ evm/chains/mainnet/constants.py
@@ -1,13 +1,17 @@
+# Blocks listed in ascending order
+
 #
-# Byzantium Block
+# Homestead Block
 #
-BYZANTIUM_MAINNET_BLOCK = 4370000
+HOMESTEAD_MAINNET_BLOCK = 1150000
 
 
 #
 # DAO Block
 #
 DAO_FORK_MAINNET_BLOCK = 1920000
+
+DAO_FORK_MAINNET_EXTRA_DATA = b'dao-hard-fork'
 
 
 #
@@ -17,12 +21,12 @@ TANGERINE_WHISTLE_MAINNET_BLOCK = 2463000
 
 
 #
-# Homestead Block
-#
-HOMESTEAD_MAINNET_BLOCK = 1150000
-
-
-#
 # Spurious Dragon Block
 #
 SPURIOUS_DRAGON_MAINNET_BLOCK = 2675000
+
+
+#
+# Byzantium Block
+#
+BYZANTIUM_MAINNET_BLOCK = 4370000
