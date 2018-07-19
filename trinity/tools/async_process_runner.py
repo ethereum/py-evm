9424a9f9642403a8530b75ad7b4a141c9882a3f7
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2018-07-19 15:51:03 +0200

    Create AsyncProcessRunner to support integration tests

---
+++ trinity/tools/async_process_runner.py
@@ -0,0 +1,65 @@
+import asyncio
+import os
+import signal
+from typing import (
+    AsyncIterable,
+    Awaitable,
+    Callable,
+    Iterable,
+)
+
+
+class AsyncProcessRunner():
+
+    def __init__(self, debug_fn: Callable[[bytes], None] = None) -> None:
+        self.debug_fn = debug_fn
+
+    @classmethod
+    async def create_and_run(cls, cmds: Iterable[str], timeout_sec: int=10) -> 'AsyncProcessRunner':
+        runner = cls()
+        await runner.run(cmds, timeout_sec)
+        return runner
+
+    async def run(self, cmds: Iterable[str], timeout_sec: int=10) -> None:
+        proc = await asyncio.create_subprocess_exec(
+            *cmds,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+            # We need this because Trinity spawns multiple processes and we need to take down
+            # the entire group of processes.
+            preexec_fn=os.setsid
+        )
+        self.proc = proc
+        asyncio.ensure_future(self.kill_after_timeout(timeout_sec))
+
+    @property
+    async def stdout(self) -> AsyncIterable[str]:
+        async for line in self._iterate_until_empty(self.proc.stdout.readline):
+            yield line
+
+    @property
+    async def stderr(self) -> AsyncIterable[str]:
+        async for line in self._iterate_until_empty(self.proc.stderr.readline):
+            yield line
+
+    async def _iterate_until_empty(
+            self,
+            awaitable_bytes_fn: Callable[[], Awaitable[bytes]]) -> AsyncIterable[str]:
+
+        while True:
+            line = await awaitable_bytes_fn()
+            if self.debug_fn:
+                self.debug_fn(line)
+            if line == b'':
+                return
+            else:
+                yield line.decode('utf-8')
+
+    async def kill_after_timeout(self, timeout_sec: int) -> None:
+        await asyncio.sleep(timeout_sec)
+        self.kill()
+        raise TimeoutError('Killed process after {} seconds'.format(timeout_sec))
+
+    def kill(self) -> None:
+        # TODO: investigate if we could do this more gracefully
+        os.killpg(os.getpgid(self.proc.pid), signal.SIGKILL)
