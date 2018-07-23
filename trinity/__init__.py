45d723d1d08d256e253e510d1a59a854dd24b37d
Piper Merriam <pipermerriam@gmail.com>
2018-07-23 10:38:55 -0600

    Prefix trinity version with eth if trinity not installed

--- trinity/__init__.py
+++ trinity/__init__.py
@@ -5,7 +5,9 @@ __version__: str
 try:
     __version__ = pkg_resources.get_distribution("trinity").version
 except pkg_resources.DistributionNotFound:
-    __version__ = pkg_resources.get_distribution("py-evm").version
+    __version__ = "eth-{0}".format(
+        pkg_resources.get_distribution("py-evm").version,
+    )
 
 from .main import (  # noqa: F401
     main,
