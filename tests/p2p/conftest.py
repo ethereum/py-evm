b61329788a064ed6076a63ca6b788cbb70bc65f0
Piper Merriam <pipermerriam@gmail.com>
2018-07-27 12:01:07 -0600

    autouse the network sim fixture

--- tests/p2p/conftest.py
+++ tests/p2p/conftest.py
@@ -35,7 +35,7 @@ def p2p_logger():
 """
 
 
-@pytest.fixture
+@pytest.fixture(autouse=True)
 def _network_sim(router):
     network = router.get_network(name='simulated')
     with network.patch_asyncio():
