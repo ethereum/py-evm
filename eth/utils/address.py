be43653e35dd6af775efc69e13a37af9b8b396f0
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2018-08-16 08:49:40 +0200

    Use 'Address' NewType in utils.address

--- eth/utils/address.py
+++ eth/utils/address.py
@@ -1,13 +1,13 @@
 import rlp
 
 from eth_hash.auto import keccak
+from eth_typing import Address
 
-
-def force_bytes_to_address(value: bytes) -> bytes:
+def force_bytes_to_address(value: bytes) -> Address:
     trimmed_value = value[-20:]
     padded_value = trimmed_value.rjust(20, b'\x00')
-    return padded_value
+    return Address(padded_value)
 
 
-def generate_contract_address(address: bytes, nonce: bytes) -> bytes:
-    return keccak(rlp.encode([address, nonce]))[-20:]
+def generate_contract_address(address: Address, nonce: bytes) -> Address:
+    return Address(keccak(rlp.encode([address, nonce]))[-20:])
