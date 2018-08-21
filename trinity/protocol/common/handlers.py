e45c7222332acaadaba5dec28001c0a97d44c753
Jason Carver <ut96caarrs@snkmail.com>
2018-08-21 16:22:33 -0700

    exchange handler stats fix

--- trinity/protocol/common/handlers.py
+++ trinity/protocol/common/handlers.py
@@ -3,6 +3,7 @@ from typing import (
     Any,
     Dict,
     List,
+    Set,
     Type,
 )
 
@@ -17,6 +18,8 @@ from trinity.protocol.common.managers import (
 
 
 class BaseExchangeHandler:
+    _exchange_managers: Set[ExchangeManager[Any, Any, Any]]
+
     @property
     @abstractmethod
     def _exchanges(self) -> Dict[str, Type[BaseExchange[Any, Any, Any]]]:
@@ -24,6 +27,7 @@ class BaseExchangeHandler:
 
     def __init__(self, peer: BasePeer) -> None:
         self._peer = peer
+        self._exchange_managers = set()
 
         for attr, exchange_cls in self._exchanges.items():
             if hasattr(self, attr):
@@ -32,9 +36,9 @@ class BaseExchangeHandler:
                     "present on the class: {1}".format(attr, getattr(self, attr))
                 )
             manager: ExchangeManager[Any, Any, Any] = ExchangeManager(self._peer, peer.cancel_token)
+            self._exchange_managers.add(manager)
             exchange = exchange_cls(manager)
             setattr(self, attr, exchange)
 
     def get_stats(self) -> List[str]:
-        manager_attrs = self._exchanges.keys()
-        return [getattr(self, attr).get_stats() for attr in manager_attrs]
+        return [exchange_manager.get_stats() for exchange_manager in self._exchange_managers]
