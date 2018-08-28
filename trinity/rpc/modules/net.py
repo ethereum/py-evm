a904708988ad8e438b5aee8d07a5168470801ca6
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2018-08-28 10:22:57 +0200

    Enforce RPC methods to be async

--- trinity/rpc/modules/net.py
+++ trinity/rpc/modules/net.py
@@ -4,19 +4,19 @@ from trinity.rpc.modules import (
 
 
 class Net(RPCModule):
-    def version(self) -> str:
+    async def version(self) -> str:
         """
         Returns the current network ID.
         """
         return str(self._chain.network_id)
 
-    def peerCount(self) -> str:
+    async def peerCount(self) -> str:
         """
         Return the number of peers that are currently connected to the node
         """
         return hex(len(self._peer_pool))  # type: ignore
 
-    def listening(self) -> bool:
+    async def listening(self) -> bool:
         """
         Return `True` if the client is actively listening for network connections
         """
