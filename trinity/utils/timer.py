00c7c0b64fdb5e04faeab38ca9074d1ef7ade244
Jason Carver <ut96caarrs@snkmail.com>
2018-08-29 10:09:11 -0700

    Add Timer.pop_elapsed()

--- trinity/utils/timer.py
+++ trinity/utils/timer.py
@@ -11,6 +11,13 @@ class Timer:
     def start(self) -> None:
         self._start = time.perf_counter()
 
+    def pop_elapsed(self) -> float:
+        """Return time elapsed since last start, and start the timer over"""
+        now = time.perf_counter()
+        elapsed = now - self._start
+        self._start = now
+        return elapsed
+
     @property
     def elapsed(self) -> float:
         return time.perf_counter() - self._start
