f9a5ab02598702761d4bdf83c49aaa9f10ebb6c3
Piper Merriam <pipermerriam@gmail.com>
2018-09-04 11:42:23 -0600

    Bump version: 0.2.0-alpha.31 → 0.2.0-alpha.32

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ install_requires = deps['eth']
 setup(
     name='py-evm',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='0.2.0-alpha.31',
+    version='0.2.0-alpha.32',
     description='Python implementation of the Ethereum Virtual Machine',
     long_description_markdown_filename='README.md',
     author='Ethereum Foundation',
