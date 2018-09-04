3294433ace0b9190ba50e4cee1896559882243f4
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2018-09-04 20:11:49 +0200

    Ensure plugins don't hang the process on exit

--- trinity/main.py
+++ trinity/main.py
@@ -77,6 +77,9 @@ from trinity.utils.mp import (
 from trinity.utils.profiling import (
     setup_cprofiler,
 )
+from trinity.utils.shutdown import (
+    exit_on_signal
+)
 from trinity.utils.version import (
     construct_trinity_client_identifier,
 )
@@ -249,6 +252,7 @@ def trinity_boot(args: Namespace,
             database_server_process,
             networking_process,
             plugin_manager,
+            main_endpoint,
             event_bus
         )
     )
@@ -268,6 +272,7 @@ def trinity_boot(args: Namespace,
             database_server_process,
             networking_process,
             plugin_manager,
+            main_endpoint,
             event_bus
         )
 
@@ -276,6 +281,7 @@ def kill_trinity_gracefully(logger: logging.Logger,
                             database_server_process: Any,
                             networking_process: Any,
                             plugin_manager: PluginManager,
+                            main_endpoint: Endpoint,
                             event_bus: EventBus) -> None:
     # When a user hits Ctrl+C in the terminal, the SIGINT is sent to all processes in the
     # foreground *process group*, so both our networking and database processes will terminate
@@ -289,7 +295,8 @@ def kill_trinity_gracefully(logger: logging.Logger,
     # perform a non-gracefull shutdown if the process takes too long to terminate.
     logger.info('Keyboard Interrupt: Stopping')
     plugin_manager.shutdown()
-    event_bus.shutdown()
+    main_endpoint.stop()
+    event_bus.stop()
     kill_process_gracefully(database_server_process, logger)
     logger.info('DB server process (pid=%d) terminated', database_server_process.pid)
     # XXX: This short sleep here seems to avoid us hitting a deadlock when attempting to
@@ -324,21 +331,6 @@ def run_database_process(chain_config: ChainConfig, db_class: Type[BaseDB]) -> N
 def exit_because_ambigious_filesystem(logger: logging.Logger) -> None:
     logger.error(TRINITY_AMBIGIOUS_FILESYSTEM_INFO)
     sys.exit(1)
-
-
-async def exit_on_signal(service_to_exit: BaseService) -> None:
-    loop = service_to_exit.get_event_loop()
-    sigint_received = asyncio.Event()
-    for sig in [signal.SIGINT, signal.SIGTERM]:
-        # TODO also support Windows
-        loop.add_signal_handler(sig, sigint_received.set)
-
-    await sigint_received.wait()
-    try:
-        await service_to_exit.cancel()
-        service_to_exit._executor.shutdown(wait=True)
-    finally:
-        loop.stop()
 
 
 @setup_cprofiler('launch_node')
