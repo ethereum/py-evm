971bc606f60a639a8b488f356db158887102a132
Piper Merriam <piper@pipermerriam.com>
2018-09-10 11:06:05 -0600

    Fix so trinity respects --trinity-root-dir setting (#1268)
    * Fix so trinity respects --trinity-root-dir setting
    
    * cleanup path utils
    
    * docstring cleanup

--- trinity/config.py
+++ trinity/config.py
@@ -39,6 +39,10 @@ from trinity.utils.chains import (
 from trinity.utils.filesystem import (
     PidFile,
 )
+from trinity.utils.xdg import (
+    get_xdg_trinity_root,
+)
+
 
 if TYPE_CHECKING:
     # avoid circular import
@@ -48,6 +52,7 @@ DATABASE_DIR_NAME = 'chain'
 
 
 class ChainConfig:
+    _trinity_root_dir: Path = None
     _data_dir: Path = None
     _nodekey_path: Path = None
     _logfile_path: Path = None
@@ -61,7 +66,8 @@ class ChainConfig:
 
     def __init__(self,
                  network_id: int,
-                 max_peers: int,
+                 max_peers: int=25,
+                 trinity_root_dir: str=None,
                  data_dir: str=None,
                  nodekey_path: str=None,
                  logfile_path: str=None,
@@ -74,6 +80,9 @@ class ChainConfig:
         self.max_peers = max_peers
         self.sync_mode = sync_mode
         self.port = port
+
+        if trinity_root_dir is not None:
+            self.trinity_root_dir = trinity_root_dir
 
         if not preferred_nodes and network_id in DEFAULT_PREFERRED_NODES:
             self.preferred_nodes = DEFAULT_PREFERRED_NODES[self.network_id]
@@ -92,32 +101,28 @@ class ChainConfig:
         else:
             self.bootstrap_nodes = bootstrap_nodes
 
-        # validation
-        if nodekey is not None and nodekey_path is not None:
-            raise ValueError("It is invalid to provide both a `nodekey` and a `nodekey_path`")
-
-        # set values
         if data_dir is not None:
             self.data_dir = data_dir
-        else:
-            self.data_dir = get_data_dir_for_network_id(self.network_id)
 
-        if nodekey_path is not None:
+        if nodekey is not None and nodekey_path is not None:
+            raise ValueError("It is invalid to provide both a `nodekey` and a `nodekey_path`")
+        elif nodekey_path is not None:
             self.nodekey_path = nodekey_path
         elif nodekey is not None:
             self.nodekey = nodekey
 
         if logfile_path is not None:
             self.logfile_path = logfile_path
-        else:
-            self.logfile_path = get_logfile_path(self.data_dir)
 
     @property
     def logfile_path(self) -> Path:
         """
         Return the path to the log file.
         """
-        return self._logfile_path
+        if self._logfile_path is not None:
+            return self._logfile_path
+        else:
+            return get_logfile_path(self.data_dir)
 
     @logfile_path.setter
     def logfile_path(self, value: Path) -> None:
@@ -131,13 +136,36 @@ class ChainConfig:
         return self.logfile_path.parent
 
     @property
+    def trinity_root_dir(self) -> Path:
+        """
+        The trinity_root_dir is the base directory that all trinity data is
+        stored under.
+
+        The default ``data_dir`` path will be resolved relative to this
+        directory.
+        """
+        if self._trinity_root_dir is not None:
+            return self._trinity_root_dir
+        else:
+            return get_xdg_trinity_root()
+
+    @trinity_root_dir.setter
+    def trinity_root_dir(self, value: str) -> None:
+        self._trinity_root_dir = Path(value).resolve()
+
+    @property
     def data_dir(self) -> Path:
         """
         The data_dir is the base directory that all chain specific information
-        for a given chain is stored.  All other chain directories are by
-        default relative to this directory.
+        for a given chain is stored.
+
+        All defaults for chain directories are resolved relative to this
+        directory.
         """
-        return self._data_dir
+        if self._data_dir is not None:
+            return self._data_dir
+        else:
+            return get_data_dir_for_network_id(self.network_id, self.trinity_root_dir)
 
     @data_dir.setter
     def data_dir(self, value: str) -> None:
@@ -145,6 +173,11 @@ class ChainConfig:
 
     @property
     def database_dir(self) -> Path:
+        """
+        Path where the chain database will be stored.
+
+        This is resolved relative to the ``data_dir``
+        """
         if self.sync_mode == SYNC_FULL:
             return self.data_dir / DATABASE_DIR_NAME / "full"
         elif self.sync_mode == SYNC_LIGHT:
@@ -154,14 +187,23 @@ class ChainConfig:
 
     @property
     def database_ipc_path(self) -> Path:
+        """
+        Path for the database IPC socket connection.
+        """
         return get_database_socket_path(self.data_dir)
 
     @property
     def jsonrpc_ipc_path(self) -> Path:
+        """
+        Path for the JSON-RPC server IPC socket.
+        """
         return get_jsonrpc_socket_path(self.data_dir)
 
     @property
     def nodekey_path(self) -> Path:
+        """
+        Path where the nodekey is stored
+        """
         if self._nodekey_path is None:
             if self._nodekey is not None:
                 return None
@@ -203,11 +245,18 @@ class ChainConfig:
 
     @classmethod
     def from_parser_args(cls, parser_args: argparse.Namespace) -> 'ChainConfig':
+        """
+        Helper function for initializing from the namespace object produced by
+        an ``argparse.ArgumentParser``
+        """
         constructor_kwargs = construct_chain_config_params(parser_args)
         return cls(**constructor_kwargs)
 
     @property
     def node_class(self) -> Type['Node']:
+        """
+        The ``Node`` class that trinity will use.
+        """
         from trinity.nodes.mainnet import (
             MainnetFullNode,
             MainnetLightNode,
