ed324233c66d5722e27f9fd92266661af2562934
Hsiao-Wei Wang <hwwang156@gmail.com>
2018-10-05 03:38:30 +0800

    Add BeaconChainDB (#1339)

---
+++ eth/beacon/db/schema.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+
+from eth_typing import (
+    Hash32,
+)
+
+
+class BaseSchema(ABC):
+    @staticmethod
+    @abstractmethod
+    def make_canonical_head_hash_lookup_key() -> bytes:
+        raise NotImplementedError('Must be implemented by subclasses')
+
+    @staticmethod
+    @abstractmethod
+    def make_block_slot_to_hash_lookup_key(slot: int) -> bytes:
+        raise NotImplementedError('Must be implemented by subclasses')
+
+    @staticmethod
+    @abstractmethod
+    def make_block_hash_to_score_lookup_key(block_hash: Hash32) -> bytes:
+        raise NotImplementedError('Must be implemented by subclasses')
+
+
+class SchemaV1(BaseSchema):
+    @staticmethod
+    def make_canonical_head_hash_lookup_key() -> bytes:
+        return b'v1:canonical_head_hash'
+
+    @staticmethod
+    def make_block_slot_to_hash_lookup_key(slot: int) -> bytes:
+        slot_to_hash_key = b'block-slot-to-hash:%d' % slot
+        return slot_to_hash_key
+
+    @staticmethod
+    def make_block_hash_to_score_lookup_key(block_hash: Hash32) -> bytes:
+        return b'block-hash-to-score:%s' % block_hash
