339f407c80aaab9e0b5aa4ee3a3928a3238160c2
Hsiao-Wei Wang <hwwang156@gmail.com>
2018-10-05 03:37:18 +0800

    Rename Block -> BaseBeaconBlock or versioning (#1339)

--- tests/beacon/types/test_block.py
+++ tests/beacon/types/test_block.py
@@ -3,7 +3,7 @@ import pytest
 import rlp
 
 from eth.beacon.types.block import (
-    Block,
+    BaseBeaconBlock,
 )
 from eth.beacon.types.attestation_record import (
     AttestationRecord,
@@ -25,12 +25,12 @@ from eth.utils.blake import (
 )
 def test_defaults(param, default_value, sample_block_params):
     del sample_block_params[param]
-    block = Block(**sample_block_params)
+    block = BaseBeaconBlock(**sample_block_params)
     assert getattr(block, param) == default_value
 
 
 def test_update_attestations(sample_attestation_record_params, sample_block_params):
-    block = Block(**sample_block_params)
+    block = BaseBeaconBlock(**sample_block_params)
     attestations = block.attestations
     attestations = list(attestations)
     attestations.append(AttestationRecord(**sample_attestation_record_params))
@@ -41,5 +41,5 @@ def test_update_attestations(sample_attestation_record_params, sample_block_para
 
 
 def test_hash(sample_block_params):
-    block = Block(**sample_block_params)
+    block = BaseBeaconBlock(**sample_block_params)
     assert block.hash == blake(rlp.encode(block))
