249b72c5fea427e006287e7e6c123f4e6e92cf91
Evgeniy Filatov <evgeniyfilatov@gmail.com>
2018-10-04 19:11:34 +0300

    Improve handling of server url for eth stats plugin
    Fixes #1337

--- trinity/plugins/builtin/ethstats/plugin.py
+++ trinity/plugins/builtin/ethstats/plugin.py
@@ -7,6 +7,8 @@ from argparse import (
     _SubParsersAction,
 )
 
+from eth.chains.mainnet import MAINNET_NETWORK_ID
+from eth.chains.ropsten import ROPSTEN_NETWORK_ID
 from trinity.extensibility import (
     BaseIsolatedPlugin,
 )
@@ -18,12 +20,24 @@ from trinity.plugins.builtin.ethstats.ethstats_service import (
     EthstatsService,
 )
 
+DEFAULT_SERVERS_URLS = {
+    MAINNET_NETWORK_ID: 'wss://ethstats.net/api',
+    ROPSTEN_NETWORK_ID: 'wss://ropsten-stats.parity.io/api',
+}
+
 
 class EthstatsPlugin(BaseIsolatedPlugin):
+    server_url: str
+    server_secret: str
+    node_id: str
+    node_contact: str
 
     @property
     def name(self) -> str:
         return 'Ethstats'
+
+    def get_default_server_url(self) -> str:
+        return DEFAULT_SERVERS_URLS.get(self.context.trinity_config.network_id, '')
 
     def configure_parser(self, arg_parser: ArgumentParser, subparser: _SubParsersAction) -> None:
         ethstats_parser = arg_parser.add_argument_group('ethstats (experimental)')
@@ -36,7 +50,7 @@ class EthstatsPlugin(BaseIsolatedPlugin):
 
         ethstats_parser.add_argument(
             '--ethstats-server-url',
-            help='Node stats server URL',
+            help='Node stats server URL (e. g. wss://example.com/api)',
             default=os.environ.get('ETHSTATS_SERVER_URL'),
         )
         ethstats_parser.add_argument(
@@ -56,17 +70,34 @@ class EthstatsPlugin(BaseIsolatedPlugin):
         )
 
     def should_start(self) -> bool:
-        if not self.context.args.ethstats:
+        args = self.context.args
+
+        if not args.ethstats:
             return False
 
-        configuration_provided: bool = all((
-            self.context.args.ethstats_server_url,
-            self.context.args.ethstats_server_secret,
-        ))
-
-        if not configuration_provided:
-            self.logger.warning('Ethstats configuration not provided, skipping')
+        if not (args.ethstats_server_url or self.get_default_server_url()):
+            self.logger.error(
+                'You must provide ethstats server url using the `--ethstats-server-url`'
+            )
+            self.context.shutdown_host()
             return False
+
+        if not args.ethstats_server_secret:
+            self.logger.error(
+                'You must provide ethstats server secret using `--ethstats-server-secret`'
+            )
+            self.context.shutdown_host()
+            return False
+
+        if (args.ethstats_server_url):
+            self.server_url = args.ethstats_server_url
+        else:
+            self.server_url = self.get_default_server_url()
+
+        self.server_secret = args.ethstats_server_secret
+
+        self.node_id = args.ethstats_node_id
+        self.node_contact = args.ethstats_node_contact
 
         return True
 
@@ -76,10 +107,10 @@ class EthstatsPlugin(BaseIsolatedPlugin):
 
         service = EthstatsService(
             self.context,
-            self.context.args.ethstats_server_url,
-            self.context.args.ethstats_server_secret,
-            self.context.args.ethstats_node_id,
-            self.context.args.ethstats_node_contact,
+            self.server_url,
+            self.server_secret,
+            self.node_id,
+            self.node_contact,
         )
 
         loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
