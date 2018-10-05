0e70efc560b791704c65ed08d5d72f1c72d6583c
Hsiao-Wei Wang <hwwang156@gmail.com>
2018-10-05 23:29:35 +0800

    Fix py35-linter CI setting and fix some type hints

--- eth/utils/blake.py
+++ eth/utils/blake.py
@@ -1,4 +1,5 @@
-from hashlib import blake2b
+# This module only supports for Python3.6+, ignore the type hints test for now.
+from hashlib import blake2b  # type: ignore
 
 
 def blake(data: bytes) -> bytes:
