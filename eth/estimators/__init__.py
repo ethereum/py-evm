453265f633379f880a8cc4edb0a3188ce73564ca
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2018-10-19 17:03:09 +0200

    Tighten type of gas estimator

--- eth/estimators/__init__.py
+++ eth/estimators/__init__.py
@@ -1,14 +1,20 @@
 import os
 from typing import (
-    Any,
+    Callable,
 )
 
+from eth.rlp.transactions import (
+    BaseTransaction,
+)
 from eth.utils.module_loading import (
     import_string,
 )
+from eth.vm.state import (
+    BaseState,
+)
 
 
-def get_gas_estimator() -> Any:
+def get_gas_estimator() -> Callable[[BaseState, BaseTransaction], int]:
     import_path = os.environ.get(
         'GAS_ESTIMATOR_BACKEND_FUNC',
         'eth.estimators.gas.binary_gas_search_intrinsic_tolerance',
