0b1a67b135b8411d429fd38e0e5cbb0e4c36fc4d
Piper Merriam <pipermerriam@gmail.com>
2018-10-23 10:57:40 -0600

    Cache rocksdb installation

--- .circleci/install_rocksdb.sh
+++ .circleci/install_rocksdb.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
 
+set -e
+set -u
+
 sudo apt-get install -y liblz4-dev libsnappy-dev libgflags-dev zlib1g-dev libbz2-dev libzstd-dev
-if [ ! -f "/root/project/rocksdb" ]; then
-  git clone https://github.com/facebook/rocksdb
+
+
+if [ ! -d "/home/circleci/rocksdb" ]; then
+  git clone https://github.com/facebook/rocksdb /home/circleci/rocksdb
 fi
-if [ ! -f "/root/project/rocksdb/librocksdb.a" ]; then
-  cd rocksdb/ && git checkout v5.8.8 && sudo make install-shared INSTALL_PATH=/usr
+if [ ! -f "/home/circleci/rocksdb/librocksdb.so.5.8.8" ]; then
+  cd /home/circleci/rocksdb/ && git checkout v5.8.8 && sudo make install-shared INSTALL_PATH=/usr
+fi
+if [ ! -f "/usr/lib/librocksdb.so.5.8" ]; then
+  ln -fs /home/circleci/rocksdb/librocksdb.so.5.8.8 /usr/lib/librocksdb.so.5.8
+fi
+if [ ! -f "/usr/lib/librocksdb.so.5" ]; then
+  ln -fs /home/circleci/rocksdb/librocksdb.so.5.8.8 /usr/lib/librocksdb.so.5
+fi
+if [ ! -f "/usr/lib/librocksdb.so" ]; then
+  ln -fs /home/circleci/rocksdb/librocksdb.so.5.8.8 /usr/lib/librocksdb.so
 fi  
