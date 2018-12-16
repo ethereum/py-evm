076fae90f804829187f293b69ad2eda583ac5d68
Chih Cheng Liang <chihchengliang@gmail.com>
2018-12-16 15:50:14 +0800

    test hash_is_keccak and fix formatting

--- tests/beacon/utils/test_hash.py
+++ tests/beacon/utils/test_hash.py
@@ -1,6 +1,11 @@
 from eth.beacon.utils.hash import hash_
+from eth_hash.auto import keccak
 
 
 def test_hash():
     output = hash_(b'helloworld')
     assert len(output) == 32
+
+
+def test_hash_is_keccak256():
+    assert hash_(b'foo') == keccak(b'foo')
