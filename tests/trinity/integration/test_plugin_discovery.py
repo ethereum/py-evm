97b000440486147919824340d4b601ec88be0f78
Jason Carver <ut96caarrs@snkmail.com>
2018-12-18 11:11:33 -0800

    Test release packaging in tox

--- tests/trinity/integration/test_plugin_discovery.py
+++ tests/trinity/integration/test_plugin_discovery.py
@@ -4,7 +4,11 @@ from trinity.plugins.registry import (
 # This plugin is external to this code base and installed by tox
 # In order to install it locally run:
 # pip install -e trinity-external-plugins/examples/peer_count_reporter
-from peer_count_reporter_plugin import PeerCountReporterPlugin
+try:
+    from peer_count_reporter_plugin import PeerCountReporterPlugin
+except ImportError:
+    # test will fail at runtime, should not fail at import/collection time
+    pass
 
 
 def test_plugin_discovery():
