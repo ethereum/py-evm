5a308cd3f28a93b58bc714bc2e791aebb7d8a16d
Hsiao-Wei Wang <hwwang156@gmail.com>
2018-12-18 11:08:26 +0800

    Add docstring for hash_eth2
    Co-Authored-By: ChihChengLiang <chihchengliang@gmail.com>

--- eth/beacon/utils/hash.py
+++ eth/beacon/utils/hash.py
@@ -3,4 +3,9 @@ from eth_hash.auto import keccak
 
 
 def hash_eth2(data: bytes) -> Hash32:
+    """
+    Return Keccak-256 hashed result.
+    Note: it's a placeholder and we aim to migrate to a S[T/N]ARK-friendly hash function in
+    a future Ethereum 2.0 deployment phase.
+    """
     return Hash32(keccak(data))
