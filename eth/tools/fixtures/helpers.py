40bf0e3543a53601c1ac252f9f37cf061cfab963
Brian Cloutier <github@briancloutier.com>
2018-12-18 17:33:20 -0800

    Check for DAO extradata while running blockchain tests

--- eth/tools/fixtures/helpers.py
+++ eth/tools/fixtures/helpers.py
@@ -24,6 +24,9 @@ from eth.rlp.blocks import (
 from eth.chains.base import (
     BaseChain,
 )
+from eth.chains.mainnet import (
+    MainnetDAOValidatorVM,
+)
 from eth.db.account import (
     BaseAccountDB,
 )
@@ -141,7 +144,7 @@ def chain_vm_configuration(fixture: Dict[str, Any]) -> Iterable[Tuple[int, Type[
             (5, TangerineWhistleVM),
         )
     elif network == 'HomesteadToDaoAt5':
-        HomesteadVM = BaseHomesteadVM.configure(
+        HomesteadVM = MainnetDAOValidatorVM.configure(
             support_dao_fork=True,
             _dao_fork_block_number=5,
         )
