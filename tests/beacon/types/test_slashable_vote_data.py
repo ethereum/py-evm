91e620430c44ecf0047fd566c1dccab979662fde
Alex Stokes <r.alex.stokes@gmail.com>
2018-12-21 14:51:44 -0600

    Add tests performing sanity checks on SlashableVoteData properties

--- tests/beacon/types/test_slashable_vote_data.py
+++ tests/beacon/types/test_slashable_vote_data.py
@@ -12,3 +12,48 @@ def test_defaults(sample_slashable_vote_data_params):
             sample_slashable_vote_data_params['aggregate_signature_poc_1_indices'])
     assert votes.data == sample_slashable_vote_data_params['data']
     assert votes.aggregate_signature == sample_slashable_vote_data_params['aggregate_signature']
+
+
+def test_hash(sample_slashable_vote_data_params):
+    votes = SlashableVoteData(**sample_slashable_vote_data_params)
+
+    # NOTE: this hash was simply copied from the existing implementation
+    # which should be the keccak-256 of the rlp serialization of `votes`.
+    # Given that this value will change soon (cf. ssz tree hash), we just
+    # do this to get the test passing for now and will need to update later
+    # if we expect the hash computation is not working correctly
+    hash_hex = "463911fa7e7f0935c5647384c8bbf32e96f42f4c6c45c6ab001db009a67cbec0"
+
+    assert votes.hash == bytes.fromhex(hash_hex)
+
+
+def test_root(sample_slashable_vote_data_params):
+    votes = SlashableVoteData(**sample_slashable_vote_data_params)
+
+    # NOTE: see note in `test_hash`, this test will need to be updated
+    # once ssz tree hash lands...
+
+    assert votes.root == votes.hash
+
+
+def test_vote_count(sample_slashable_vote_data_params):
+    votes = SlashableVoteData(**sample_slashable_vote_data_params)
+
+    key = "aggregate_signature_poc_0_indices"
+    proof_of_custody_0_indices = sample_slashable_vote_data_params[key]
+    key = "aggregate_signature_poc_1_indices"
+    proof_of_custody_1_indices = sample_slashable_vote_data_params[key]
+
+    assert votes.vote_count == len(proof_of_custody_0_indices) + len(proof_of_custody_1_indices)
+
+
+def test_messages(sample_slashable_vote_data_params):
+    votes = SlashableVoteData(**sample_slashable_vote_data_params)
+
+    zero_discriminator = (0).to_bytes(1, 'big')
+    one_discriminator = (1).to_bytes(1, 'big')
+
+    assert votes.messages == (
+        votes.root + zero_discriminator,
+        votes.root + one_discriminator,
+    )
