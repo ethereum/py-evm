0efcf2d9da7680825b7b85bd697b788c78a1c0f8
Hsiao-Wei Wang <hwwang156@gmail.com>
2018-12-25 18:22:51 +0800

    Beacon chain Merkle tree APIs
    1. Refactor `eth._utils.merkle` APIs
    2. Add `get_merkle_root` and `get_updated_merkle_accumulator`

--- eth/beacon/_utils/hash.py
+++ eth/beacon/_utils/hash.py
@@ -1,8 +1,12 @@
+from typing import (
+    Hashable,
+)
+
 from eth_typing import Hash32
 from eth_hash.auto import keccak
 
 
-def hash_eth2(data: bytes) -> Hash32:
+def hash_eth2(data: Hashable) -> Hash32:
     """
     Return Keccak-256 hashed result.
     Note: it's a placeholder and we aim to migrate to a S[T/N]ARK-friendly hash function in
--- tests/core/merkle-utils/test_merkle_trees.py
+++ tests/core/merkle-utils/test_merkle_trees.py
@@ -4,8 +4,11 @@ from eth_utils import (
     ValidationError,
 )
 
-from eth_hash.auto import (
-    keccak,
+from eth.constants import (
+    ZERO_HASH32,
+)
+from eth.beacon._utils.hash import (
+    hash_eth2,
 )
 
 from eth._utils.merkle import (
@@ -13,6 +16,8 @@ from eth._utils.merkle import (
     calc_merkle_tree,
     get_root,
     get_merkle_proof,
+    get_merkle_root,
+    get_updated_merkle_accumulator,
     verify_merkle_proof,
 )
 
@@ -21,41 +26,41 @@ from eth._utils.merkle import (
     (
         (b"single leaf",),
         (
-            (keccak(b"single leaf"),),
+            (hash_eth2(b"single leaf"),),
         ),
     ),
     (
         (b"left", b"right"),
         (
-            (keccak(keccak(b"left") + keccak(b"right")),),
-            (keccak(b"left"), keccak(b"right")),
+            (hash_eth2(hash_eth2(b"left") + hash_eth2(b"right")),),
+            (hash_eth2(b"left"), hash_eth2(b"right")),
         ),
     ),
     (
         (b"1", b"2", b"3", b"4"),
         (
             (
-                keccak(
-                    keccak(
-                        keccak(b"1") + keccak(b"2")
-                    ) + keccak(
-                        keccak(b"3") + keccak(b"4")
+                hash_eth2(
+                    hash_eth2(
+                        hash_eth2(b"1") + hash_eth2(b"2")
+                    ) + hash_eth2(
+                        hash_eth2(b"3") + hash_eth2(b"4")
                     )
                 ),
             ),
             (
-                keccak(
-                    keccak(b"1") + keccak(b"2")
+                hash_eth2(
+                    hash_eth2(b"1") + hash_eth2(b"2")
                 ),
-                keccak(
-                    keccak(b"3") + keccak(b"4")
+                hash_eth2(
+                    hash_eth2(b"3") + hash_eth2(b"4")
                 ),
             ),
             (
-                keccak(b"1"),
-                keccak(b"2"),
-                keccak(b"3"),
-                keccak(b"4"),
+                hash_eth2(b"1"),
+                hash_eth2(b"2"),
+                hash_eth2(b"3"),
+                hash_eth2(b"4"),
             ),
         ),
     ),
@@ -77,32 +82,32 @@ def test_invalid_merkle_root_calculation(leave_number):
     (
         (b"1", b"2"),
         0,
-        (keccak(b"2"),),
+        (hash_eth2(b"2"),),
     ),
     (
         (b"1", b"2"),
         1,
-        (keccak(b"1"),),
+        (hash_eth2(b"1"),),
     ),
     (
         (b"1", b"2", b"3", b"4"),
         0,
-        (keccak(b"2"), keccak(keccak(b"3") + keccak(b"4"))),
+        (hash_eth2(b"2"), hash_eth2(hash_eth2(b"3") + hash_eth2(b"4"))),
     ),
     (
         (b"1", b"2", b"3", b"4"),
         1,
-        (keccak(b"1"), keccak(keccak(b"3") + keccak(b"4"))),
+        (hash_eth2(b"1"), hash_eth2(hash_eth2(b"3") + hash_eth2(b"4"))),
     ),
     (
         (b"1", b"2", b"3", b"4"),
         2,
-        (keccak(b"4"), keccak(keccak(b"1") + keccak(b"2"))),
+        (hash_eth2(b"4"), hash_eth2(hash_eth2(b"1") + hash_eth2(b"2"))),
     ),
     (
         (b"1", b"2", b"3", b"4"),
         3,
-        (keccak(b"3"), keccak(keccak(b"1") + keccak(b"2"))),
+        (hash_eth2(b"3"), hash_eth2(hash_eth2(b"1") + hash_eth2(b"2"))),
     ),
 ])
 def test_merkle_proofs(leaves, index, proof):
@@ -142,3 +147,82 @@ def test_proof_generation_index_validation(leaves):
     for invalid_index in [-1, len(leaves)]:
         with pytest.raises(ValidationError):
             get_merkle_proof(tree, invalid_index)
+
+
+def test_get_merkle_root():
+    hash_0 = b"0" * 32
+    leaves = (hash_0,)
+    root = get_merkle_root(leaves)
+    assert root == hash_0
+
+    hash_1 = b"1" * 32
+    leaves = (hash_0, hash_1)
+    root = get_merkle_root(leaves)
+    assert root == hash_eth2(hash_0 + hash_1)
+
+
+def test_get_updated_merkle_accumulator_three_elements():
+    # Initialize the accumulator
+    accumulator = tuple(
+        ZERO_HASH32
+        for _ in range(8)
+    )
+
+    # Add element 1
+    position = 0
+    value_1 = b"a" * 32
+    accumulator = get_updated_merkle_accumulator(
+        accumulator,
+        position,
+        value_1,
+    )
+    assert accumulator[0] == value_1
+
+    # Add element 2
+    position = 1
+    value_2 = b"b" * 32
+    accumulator = get_updated_merkle_accumulator(
+        accumulator,
+        position,
+        value_2,
+    )
+    assert accumulator[1] == hash_eth2(accumulator[0] + value_2)
+
+    # Add element 3
+    position = 2
+    value_3 = b"c" * 32
+    accumulator = get_updated_merkle_accumulator(
+        accumulator,
+        position,
+        value_3,
+    )
+    assert accumulator[0] == value_3
+
+    leaves = (value_1, value_2, value_3)
+    assert accumulator[1] == get_merkle_root(leaves[0:2])
+    assert accumulator[0] == get_merkle_root(leaves[2:3])
+
+
+def test_get_updated_merkle_accumulator_spec_example():
+    accumulator = tuple(
+        ZERO_HASH32
+        for _ in range(64)
+    )
+
+    leaves = tuple(
+        i.to_bytes(32, 'big')
+        for i in range(329)
+    )
+
+    for i in range(329):
+        accumulator = get_updated_merkle_accumulator(
+            accumulator,
+            position=i,
+            value=leaves[i],
+        )
+
+    # `len(leaves) = 329` (note: 333 = 256 + 64 + 8 + 1)
+    assert accumulator[8] == get_merkle_root(leaves[0:256])
+    assert accumulator[6] == get_merkle_root(leaves[256:320])
+    assert accumulator[3] == get_merkle_root(leaves[320:328])
+    assert accumulator[0] == get_merkle_root(leaves[328:329])
