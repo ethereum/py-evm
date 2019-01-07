38c97c8569d21d92fa25fa6c197cf5adc6db1c68
Brian Cloutier <lithp@users.noreply.github.com>
2019-01-07 08:05:07 -0800

    Don't run plugin discovery tests by default (#111)
    * Don't run plugin discovery tests by default
    
    py-evm's 713de38d solved py-evm#1646 but it was dropped during the
    migration.
    
    Originally-by: cburgdorf

--- tests/integration/test_plugin_discovery.py
+++ tests/integration/test_plugin_discovery.py
@@ -1,12 +1,17 @@
+import pytest
 from trinity.plugins.registry import (
     discover_plugins
 )
-# This plugin is external to this code base and installed by tox
-# In order to install it locally run:
-# pip install -e trinity-external-plugins/examples/peer_count_reporter
-from peer_count_reporter_plugin import PeerCountReporterPlugin
 
 
 def test_plugin_discovery():
+    if not pytest.config.getoption("--integration"):
+        pytest.skip("Not asked to run integration tests")
+
+    # This plugin is external to this code base and installed by tox
+    # In order to install it locally run:
+    # pip install -e trinity-external-plugins/examples/peer_count_reporter
+    from peer_count_reporter_plugin import PeerCountReporterPlugin
+
     plugins = [type(plugin) for plugin in discover_plugins()]
     assert PeerCountReporterPlugin in plugins
