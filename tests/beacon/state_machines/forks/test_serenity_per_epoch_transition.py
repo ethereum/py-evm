399c1cc8b15c0e50f1784e822e2cddfedcc65924
Chih Cheng Liang <chihchengliang@gmail.com>
2019-01-08 17:15:58 +0800

    add more epoch of tests

--- tests/beacon/state_machines/forks/test_serenity_per_epoch_transition.py
+++ tests/beacon/state_machines/forks/test_serenity_per_epoch_transition.py
@@ -33,18 +33,20 @@ def test_justification_without_validators(
     "total_balance,"
     "slot,"
 
+    # Key state variables before process_justification
     "previous_justified_slot_before,"
     "justified_slot_before,"
     "justification_bitfield_before,"
     "finalized_slot_before,"
 
+    # Key state variables after process_justification
     "previous_justified_slot_after,"
     "justified_slot_after,"
     "justification_bitfield_after,"
     "finalized_slot_after,",
     (
         (
-            # The first epoch transition happens in slot 64
+            # The 1st epoch transition happens in slot 64
             # Before the transition, the finalized slot, justified slot, and previous justified slot
             # should be the genesis.
             0, 10, 10, 64,
@@ -52,14 +54,50 @@ def test_justification_without_validators(
             0, 0, 0b1, 0,
         ),
         (
+            # The 2nd epoch: enough previous and current attestations justify slot 64
             10, 10, 15, 128,
             0, 0, 0b01, 0,
             0, 64, 0b11, 0,
         ),
         (
+            # The 3rd epoch: enough previous and current attestations justify slot 128
+            # 64 is finalized. (check_finalization `should_finalize_B3`)
             10, 10, 15, 192,
             0, 64, 0b11, 0,
             64, 128, 0b111, 64,
+        ),
+        (
+            # The 4th epoch:
+            # due to network delay
+            # insufficient current attestations to justify slot 192
+            10, 5, 15, 256,
+            64, 128, 0b111, 64,
+            128, 128, 0b1110, 64,
+        ),
+        (
+            # The 5th epoch:
+            # some attestations with justified_slot 128 arrived, and
+            # no delay in current attestations.
+            # Resulting 128 and 192 are justified, and 128 finalized.
+            # (check_finalization `should_finalize_B2`)
+            10, 10, 15, 320,
+            128, 128, 0b1110, 64,
+            128, 256, 0b11111, 128,
+        ),
+        (
+            # The 6th epoch:
+            # more validator joining, total_balance increased
+            # but suffering from network delay, no attestation arrives.
+            10, 0, 20, 384,
+            128, 256, 0b11111, 128,
+            256, 256, 0b111110, 128,
+        ),
+        (
+            # The 7th epoch:
+            # still suffering from network delay
+            0, 0, 20, 448,
+            256, 256, 0b111110, 128,
+            256, 256, 0b1111100, 128,
         ),
     ),
 )
