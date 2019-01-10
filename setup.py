04723f56da04168d6caebb76864c688affc90b6c
Piper Merriam <pipermerriam@gmail.com>
2019-01-10 09:35:00 -0700

    update eth-bloom

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 deps = {
     'eth': [
         "cryptography>=2.0.3,<3.0.0",
-        "eth-bloom>=1.0.2,<2.0.0",
+        "eth-bloom>=1.0.3,<2.0.0",
         "eth-keys>=0.2.1,<1.0.0",
         "eth-typing>=2.0.0,<3.0.0",
         "eth-utils>=1.3.0,<2.0.0",
