952acfa261e16e96c4ad4fe3c2524af186d52b10
Noel Maersk <veox@users.noreply.github.com>
2019-01-25 14:41:57 +0200

    tests/eth2/beacon: move test to beacon-chain-specific file.
    `test_validation.py` conflicts with another file by same name, so
    move test to `test_beacon_validation.py` that was made just for that,
    in PR https://github.com/ethereum/py-evm/pull/1660

--- tests/eth2/beacon/test_beacon_validation.py
+++ tests/eth2/beacon/test_beacon_validation.py
@@ -6,6 +6,7 @@ from eth_utils import (
 
 from eth2.beacon.validation import (
     validate_slot,
+    validate_slot_for_state_slot,
 )
 
 
@@ -30,3 +31,41 @@ def test_validate_slot(slot, is_valid):
     else:
         with pytest.raises(ValidationError):
             validate_slot(slot)
+
+
+@pytest.mark.parametrize(
+    (
+        'state_slot, slot, epoch_length, success'
+    ),
+    [
+        (
+            0, 0, 64, True,
+        ),
+        (
+            64 * 2, 64, 64, True,
+        ),
+        (
+            64 * 2, 64 - 1, 64, False,  # slot is too small
+        ),
+        (
+            64 * 2, 64 * 3 - 1, 64, True,
+        ),
+        (
+            64 * 2, 64 * 3, 64, False,  # slot is too large
+        ),
+    ]
+)
+def test_validate_slot_for_state_slot(state_slot, slot, epoch_length, success):
+    if success:
+        validate_slot_for_state_slot(
+            state_slot=state_slot,
+            slot=slot,
+            epoch_length=epoch_length
+        )
+    else:
+        with pytest.raises(ValidationError):
+            validate_slot_for_state_slot(
+                state_slot=state_slot,
+                slot=slot,
+                epoch_length=epoch_length
+            )
