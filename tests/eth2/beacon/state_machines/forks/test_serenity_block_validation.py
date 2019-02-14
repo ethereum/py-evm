412ae4629941148fce5779cdc2861f742b5a2f29
Alex Stokes <r.alex.stokes@gmail.com>
2019-02-14 15:16:49 -0800

    Refactor tests to cover refactor of aggregate signature validation

--- tests/eth2/beacon/state_machines/forks/test_serenity_block_validation.py
+++ tests/eth2/beacon/state_machines/forks/test_serenity_block_validation.py
@@ -1,21 +1,42 @@
+import copy
+import random
+
 import pytest
+
+from hypothesis import (
+    given,
+    strategies as st,
+)
 
 from eth_utils import (
     ValidationError,
 )
 
-from eth2._utils import bls
+from eth_utils.toolz import (
+    assoc,
+)
 
+from eth2._utils import bls
+from eth2._utils.bitfield import (
+    get_empty_bitfield,
+    set_voted,
+)
 from eth2.beacon.configs import (
     CommitteeConfig,
 )
 from eth2.beacon.enums import (
     SignatureDomain,
 )
+from eth2.beacon.helpers import (
+    get_domain,
+    slot_to_epoch,
+)
 from eth2.beacon.types.blocks import BeaconBlock
 from eth2.beacon.types.proposal_signed_data import (
     ProposalSignedData,
 )
+from eth2.beacon.types.forks import Fork
+from eth2.beacon.types.slashable_attestations import SlashableAttestation
 from eth2.beacon.types.states import BeaconState
 from eth2.beacon.types.forks import Fork
 
@@ -24,9 +45,13 @@ from eth2.beacon.helpers import (
 )
 
 from eth2.beacon.state_machines.forks.serenity.block_validation import (
+    generate_aggregate_pubkeys_from_indices,
+    get_pubkey_for_indices,
     validate_block_slot,
     validate_proposer_signature,
     validate_randao_reveal,
+    validate_slashable_attestation,
+    verify_slashable_attestation_signature,
 )
 
 from tests.eth2.beacon.helpers import mock_validator_record
@@ -169,3 +194,367 @@ def test_randao_reveal_validation(is_valid,
     else:
         if not is_valid:
             pytest.fail("Did not raise")
+
+
+def _generate_some_indices(data, max_value_for_list):
+    """
+    Hypothesis helper that generates a list of some integers [0, `max_value_for_list`].
+    The usage is to randomly sample some elements from a sequence of some element.
+    """
+    return data.draw(
+        st.lists(
+            st.integers(
+                min_value=0,
+                max_value=max_value_for_list,
+            ),
+        )
+    )
+
+
+@given(st.data())
+def test_get_pubkey_for_indices(activated_genesis_validators, data):
+    max_value_for_list = len(activated_genesis_validators) - 1
+    indices = _generate_some_indices(data, max_value_for_list)
+    pubkeys = get_pubkey_for_indices(activated_genesis_validators, indices)
+
+    assert len(indices) == len(pubkeys)
+
+    for index, pubkey in enumerate(pubkeys):
+        validator_index = indices[index]
+        assert activated_genesis_validators[validator_index].pubkey == pubkey
+
+
+def _list_and_index(data, max_size=None, elements=st.integers()):
+    """
+    Hypothesis helper function cribbed from their docs on @composite
+    """
+    xs = data.draw(st.lists(elements, max_size=max_size, unique=True))
+    i = data.draw(st.integers(min_value=0, max_value=max(len(xs) - 1, 0)))
+    return (xs, i)
+
+
+@given(st.data())
+def test_generate_aggregate_pubkeys(activated_genesis_validators,
+                                    sample_slashable_attestation_params,
+                                    data):
+    max_value_for_list = len(activated_genesis_validators) - 1
+    (validator_indices, some_index) = _list_and_index(
+        data,
+        elements=st.integers(
+            min_value=0,
+            max_value=max_value_for_list,
+        )
+    )
+
+    key = "validator_indices"
+    sample_slashable_attestation_params[key] = validator_indices
+
+    custody_bitfield = get_empty_bitfield(len(validator_indices))
+    for index in range(some_index):
+        custody_bitfield = set_voted(custody_bitfield, index)
+
+    key = "custody_bitfield"
+    sample_slashable_attestation_params[key] = custody_bitfield
+
+    slashable_attestation = SlashableAttestation(**sample_slashable_attestation_params)
+    custody_bit_0_indices, custody_bit_1_indices = slashable_attestation.custody_bit_indices
+    assert len(
+        set(custody_bit_0_indices).intersection(set(custody_bit_1_indices))
+    ) == 0
+
+    keys = generate_aggregate_pubkeys_from_indices(
+        activated_genesis_validators,
+        *slashable_attestation.custody_bit_indices,
+    )
+    assert len(keys) == 2
+
+    (poc_0_key, poc_1_key) = keys
+
+    poc_0_keys = get_pubkey_for_indices(activated_genesis_validators, custody_bit_0_indices)
+    poc_1_keys = get_pubkey_for_indices(activated_genesis_validators, custody_bit_1_indices)
+
+    assert bls.aggregate_pubkeys(poc_0_keys) == poc_0_key
+    assert bls.aggregate_pubkeys(poc_1_keys) == poc_1_key
+
+
+def _get_indices_and_signatures(num_validators, message, privkeys, fork, epoch):
+    num_indices = 5
+    assert num_validators >= num_indices
+    indices = random.sample(range(num_validators), num_indices)
+    indices.sort()
+
+    privkeys = [privkeys[i] for i in indices]
+    domain_type = SignatureDomain.DOMAIN_ATTESTATION
+    domain = get_domain(
+        fork=fork,
+        epoch=epoch,
+        domain_type=domain_type,
+    )
+    signatures = tuple(
+        map(lambda key: bls.sign(message, key, domain), privkeys)
+    )
+    return (indices, signatures)
+
+
+def _correct_slashable_attestation_params(
+        epoch_length,
+        num_validators,
+        params,
+        messages,
+        privkeys,
+        fork):
+    valid_params = copy.deepcopy(params)
+
+    (validator_indices, signatures) = _get_indices_and_signatures(
+        num_validators,
+        messages[1],
+        privkeys,
+        fork,
+        slot_to_epoch(params["data"].slot, epoch_length),
+    )
+
+    valid_params["validator_indices"] = validator_indices
+    valid_params["custody_bitfield"] = get_empty_bitfield(len(validator_indices))
+
+    aggregate_signature = bls.aggregate_signatures(signatures)
+
+    valid_params["aggregate_signature"] = aggregate_signature
+
+    return valid_params
+
+
+def _corrupt_custody_bitfield_not_empty(params):
+    validator_indices_length = len(params["validator_indices"])
+    corrupt_custody_bitfield = get_empty_bitfield(validator_indices_length)
+    corrupt_custody_bitfield = set_voted(corrupt_custody_bitfield, 0)
+    return assoc(params, "custody_bitfield", corrupt_custody_bitfield)
+
+
+def _corrupt_validator_indices(params):
+    corrupt_validator_indices = (
+        params["validator_indices"][1],
+        params["validator_indices"][0],
+    ) + tuple(params["validator_indices"][2:])
+
+    return assoc(params, "validator_indices", corrupt_validator_indices)
+
+
+def _corrupt_custody_bitfield_invalid(params):
+    validator_indices_length = len(params["validator_indices"])
+    corrupt_custody_bitfield = get_empty_bitfield(validator_indices_length + 8)
+    return assoc(params, "custody_bitfield", corrupt_custody_bitfield)
+
+
+def _corrupt_validator_indices_max(max_indices_per_slashable_vote, params):
+    corrupt_validator_indices = [
+        i
+        for i in range(max_indices_per_slashable_vote + 1)
+    ]
+    return assoc(params, "validator_indices", corrupt_validator_indices)
+
+
+def _corrupt_signature(epoch_length, params, fork):
+    message = bytes.fromhex("deadbeefcafe")
+    privkey = 42
+    domain_type = SignatureDomain.DOMAIN_ATTESTATION
+    domain = get_domain(
+        fork=fork,
+        epoch=slot_to_epoch(params["data"].slot, epoch_length),
+        domain_type=domain_type,
+    )
+    corrupt_signature = bls.sign(message, privkey, domain)
+
+    return assoc(params, "aggregate_signature", corrupt_signature)
+
+
+def _create_slashable_attestation_messages(params):
+    # TODO update when we move to `ssz` tree hash
+    votes = SlashableAttestation(**params)
+    return votes.messages
+
+
+@pytest.mark.parametrize(
+    (
+        'num_validators',
+    ),
+    [
+        (40,),
+    ]
+)
+def test_verify_slashable_attestation_signature(
+        epoch_length,
+        num_validators,
+        privkeys,
+        sample_beacon_state_params,
+        activated_genesis_validators,
+        genesis_balances,
+        sample_slashable_attestation_params,
+        sample_fork_params):
+    state = BeaconState(**sample_beacon_state_params).copy(
+        validator_registry=activated_genesis_validators,
+        validator_balances=genesis_balances,
+        fork=Fork(**sample_fork_params),
+    )
+
+    # NOTE: we can do this before "correcting" the params as they
+    # touch disjoint subsets of the provided params
+    messages = _create_slashable_attestation_messages(sample_slashable_attestation_params)
+
+    valid_params = _correct_slashable_attestation_params(
+        epoch_length,
+        num_validators,
+        sample_slashable_attestation_params,
+        messages,
+        privkeys,
+        state.fork,
+    )
+    valid_votes = SlashableAttestation(**valid_params)
+    assert verify_slashable_attestation_signature(state, valid_votes, epoch_length)
+
+    invalid_params = _corrupt_signature(epoch_length, valid_params, state.fork)
+    invalid_votes = SlashableAttestation(**invalid_params)
+    assert not verify_slashable_attestation_signature(state, invalid_votes, epoch_length)
+
+
+def _run_verify_slashable_vote(
+        epoch_length,
+        params,
+        state,
+        max_indices_per_slashable_vote,
+        should_succeed):
+    votes = SlashableAttestation(**params)
+    if should_succeed:
+        validate_slashable_attestation(state, votes, max_indices_per_slashable_vote, epoch_length)
+    else:
+        with pytest.raises(ValidationError):
+            validate_slashable_attestation(
+                state,
+                votes,
+                max_indices_per_slashable_vote,
+                epoch_length,
+            )
+
+
+@pytest.mark.parametrize(
+    (
+        'num_validators',
+    ),
+    [
+        (40,),
+    ]
+)
+@pytest.mark.parametrize(
+    (
+        'param_mapper',
+        'should_succeed',
+        'needs_fork',
+        'is_testing_max_length',
+    ),
+    [
+        (lambda params: params, True, False, False),
+        (_corrupt_custody_bitfield_not_empty, False, False, False),
+        (_corrupt_validator_indices, False, False, False),
+        (_corrupt_custody_bitfield_invalid, False, False, False),
+        (_corrupt_validator_indices_max, False, False, True),
+        (_corrupt_signature, False, True, False),
+    ],
+)
+def test_validate_slashable_attestation(
+        epoch_length,
+        num_validators,
+        param_mapper,
+        should_succeed,
+        needs_fork,
+        is_testing_max_length,
+        privkeys,
+        sample_beacon_state_params,
+        activated_genesis_validators,
+        genesis_balances,
+        sample_slashable_attestation_params,
+        sample_fork_params,
+        max_indices_per_slashable_vote):
+    state = BeaconState(**sample_beacon_state_params).copy(
+        validator_registry=activated_genesis_validators,
+        validator_balances=genesis_balances,
+        fork=Fork(**sample_fork_params),
+    )
+
+    # NOTE: we can do this before "correcting" the params as they
+    # touch disjoint subsets of the provided params
+    messages = _create_slashable_attestation_messages(sample_slashable_attestation_params)
+
+    params = _correct_slashable_attestation_params(
+        epoch_length,
+        num_validators,
+        sample_slashable_attestation_params,
+        messages,
+        privkeys,
+        state.fork,
+    )
+    if needs_fork:
+        params = param_mapper(epoch_length, params, state.fork)
+    elif is_testing_max_length:
+        params = param_mapper(max_indices_per_slashable_vote, params)
+
+    else:
+        params = param_mapper(params)
+    _run_verify_slashable_vote(
+        epoch_length,
+        params,
+        state,
+        max_indices_per_slashable_vote,
+        should_succeed,
+    )
+
+
+@pytest.mark.parametrize(
+    (
+        'num_validators',
+    ),
+    [
+        (40,),
+    ]
+)
+def test_verify_slashable_attestation_after_fork(
+        epoch_length,
+        num_validators,
+        privkeys,
+        sample_beacon_state_params,
+        activated_genesis_validators,
+        genesis_balances,
+        sample_slashable_attestation_params,
+        sample_fork_params,
+        max_indices_per_slashable_vote):
+    # Test that slashable data is still valid after fork
+    # Slashable data slot = 10, fork slot = 15, current slot = 20
+    past_fork_params = {
+        'previous_version': 0,
+        'current_version': 1,
+        'epoch': 15,
+    }
+
+    state = BeaconState(**sample_beacon_state_params).copy(
+        validator_registry=activated_genesis_validators,
+        validator_balances=genesis_balances,
+        fork=Fork(**past_fork_params),
+        slot=20,
+    )
+
+    messages = _create_slashable_attestation_messages(sample_slashable_attestation_params)
+
+    valid_params = _correct_slashable_attestation_params(
+        epoch_length,
+        num_validators,
+        sample_slashable_attestation_params,
+        messages,
+        privkeys,
+        state.fork,
+    )
+    _run_verify_slashable_vote(
+        epoch_length,
+        valid_params,
+        state,
+        max_indices_per_slashable_vote,
+        True,
+    )
+>>>>>>> Refactor tests to cover refactor of aggregate signature validation
