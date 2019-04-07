ethereum/trinity

Change `parent_root` to `previous_block_root`

trinity/protocol/bcc/servers.py
@@ -135,7 +135,7 @@ class BCCRequestServer(BaseRequestServer):
             # ensure only a connected chain is returned (breaks might occur if the start block is
             # not part of the canonical chain or if the canonical chain changes during execution)
             for parent, child in sliding_window(2, max_blocks_generator):
-                if child.parent_root == parent.hash:
+                if child.previous_block_root == parent.hash:
                     yield child
                 else:
                     break

ethereum/py-evm

trinity/protocol/bcc/servers.py
@@ -126,7 +126,7 @@ class BCCRequestServer(BaseRequestServer):
                 # TODO: pass accurate `block_class: Type[BaseBeaconBlock]` under
                 # per BeaconStateMachine fork
                 block = await self.db.coro_get_canonical_block_by_slot(slot, BeaconBlock)
-                if block.parent_root == parent.hash:
+                if block.previous_block_root == parent.hash:
                     yield block
                 else:
                     break