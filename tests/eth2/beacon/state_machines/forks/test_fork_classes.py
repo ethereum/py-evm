ae03f0ccfd2d6bb4c283f342c596e561e2e14cd8
Chih Cheng Liang <chihchengliang@gmail.com>
2019-04-08 10:51:59 +0800

    test new state machine class and new chain class are well defined

---
+++ tests/eth2/beacon/state_machines/forks/test_fork_classes.py
@@ -0,0 +1,13 @@
+import pytest
+from eth2.beacon.state_machines.forks.xiao_long_bao import XiaoLongBaoStateMachine
+
+
+@pytest.mark.parametrize(
+    "sm_klass",
+    (
+        XiaoLongBaoStateMachine,
+    )
+)
+def test_sm_class_well_defined(sm_klass):
+    state_machine = sm_klass(chaindb=None, block=None)
+    assert state_machine.get_block_class()
