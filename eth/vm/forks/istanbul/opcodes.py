225c119fc19b921fe1d20bafa967cc83d333ac24
Bryant Eisenbach <fubuloubu@gmail.com>
2019-04-16 20:36:18 -0400

    lint: Removed unused imports

--- eth/vm/forks/istanbul/opcodes.py
+++ eth/vm/forks/istanbul/opcodes.py
@@ -3,23 +3,6 @@ from eth_utils.toolz import (
     merge
 )
 
-from eth import (
-    constants
-)
-from eth.vm import (
-    mnemonics,
-    opcode_values,
-)
-from eth.vm.forks.constantinople.opcodes import (
-    CONSTANTINOPLE_OPCODES,
-)
-from eth.vm.logic import (
-    context,
-)
-from eth.vm.opcode import (
-    as_opcode
-)
-
 
 UPDATED_OPCODES = {
 }
