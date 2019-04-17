d53a14dbb44732375ab762e3cfc0ea3509792ee8
Bryant Eisenbach <fubuloubu@gmail.com>
2019-04-16 20:48:31 -0400

    bug: Flake8 really doesn't like empty dicts

--- eth/vm/forks/istanbul/opcodes.py
+++ eth/vm/forks/istanbul/opcodes.py
@@ -1,17 +1,8 @@
 import copy
-from eth_utils.toolz import (
-    merge
-)
 
 from eth.vm.forks.constantinople.opcodes import (
     CONSTANTINOPLE_OPCODES,
 )
 
 
-UPDATED_OPCODES = {
-}
-
-ISTANBUL_OPCODES = merge(
-    copy.deepcopy(CONSTANTINOPLE_OPCODES),
-    UPDATED_OPCODES,
-)
+ISTANBUL_OPCODES = copy.deepcopy(CONSTANTINOPLE_OPCODES)
