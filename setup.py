609cbd72745c4faefc62953108a6d8d4019dfa72
Piper Merriam <pipermerriam@gmail.com>
2019-04-18 16:08:31 -0600

    PR feedback

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ deps = {
     'p2p': [
         "asyncio-cancel-token==0.1.0a2",
         "async_lru>=0.1.0,<1.0.0",
-        "eth-hash>=0.1.4,<1",
         # cryptography does not use semver and allows breaking changes within `0.3` version bumps.
-        "cryptography>=2.6,<2.9",
+        "cryptography>=2.5,<2.7",
+        "eth-hash>=0.1.4,<1",
         "netifaces>=0.10.7<1",
         "pysha3>=1.0.0,<2.0.0",
         "upnpclient>=0.0.8,<1",
