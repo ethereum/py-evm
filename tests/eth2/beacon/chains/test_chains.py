af18ca989e36ca940387754fff8b8419d7abe4eb
Hsiao-Wei Wang <hwwang156@gmail.com>
2019-04-18 18:38:21 +0800

    Update tests/eth2/beacon/chains/test_chains.py
    Co-Authored-By: ChihChengLiang <chihchengliang@gmail.com>

--- tests/eth2/beacon/chains/test_chains.py
+++ tests/eth2/beacon/chains/test_chains.py
@@ -10,4 +10,4 @@ from eth2.beacon.chains.testnet import TestnetChain
 )
 def test_chain_class_well_defined(chain_klass):
     chain = chain_klass(None)
-    assert chain.sm_configuration is not ()
+    assert chain.sm_configuration is not () and chain.sm_configuration is not None
