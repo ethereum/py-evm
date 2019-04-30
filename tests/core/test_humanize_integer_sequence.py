9b111551cb56948eb00ed491a2103b8cd1551059
Piper Merriam <pipermerriam@gmail.com>
2019-04-30 16:12:42 -0600

    Better messaging on header validation failure

---
+++ tests/core/test_humanize_integer_sequence.py
@@ -0,0 +1,23 @@
+import pytest
+
+from trinity._utils.humanize import humanize_integer_sequence
+
+
+@pytest.mark.parametrize(
+    'seq,expected',
+    (
+        ((), '(empty)'),
+        ((1,), '1'),
+        ((2,), '2'),
+        ((10,), '10'),
+        ((1, 2, 3), '1-3'),
+        (range(6), '0-5'),
+        ((1, 2, 3, 7, 8, 9), '1-3|7-9'),
+        ((1, 2, 3, 5, 7, 8, 9), '1-3|5|7-9'),
+        ((1, 3, 4, 5, 7, 8, 9), '1|3-5|7-9'),
+        ((1, 3, 4, 5, 9), '1|3-5|9'),
+    ),
+)
+def test_humanize_integer_sequence(seq, expected):
+    actual = humanize_integer_sequence(seq)
+    assert actual == expected
