4b2f5a5c7d577bdb84e5356cef0bf14e8755bd8a
NIC619 <twedusuck@gmail.com>
2019-05-08 22:34:31 +0800

    Add second half tick to slot ticker

--- trinity/plugins/eth2/beacon/slot_ticker.py
+++ trinity/plugins/eth2/beacon/slot_ticker.py
@@ -23,13 +23,14 @@ from trinity.endpoint import (
     TrinityEventBusEndpoint,
 )
 
-DEFAULT_CHECK_FREQUENCY = 5
+DEFAULT_CHECK_FREQUENCY = 6
 
 
 class NewSlotEvent(BaseEvent):
-    def __init__(self, slot: Slot, elapsed_time: Second):
+    def __init__(self, slot: Slot, elapsed_time: Second, is_second_signal: bool):
         self.slot = slot
         self.elapsed_time = elapsed_time
+        self.is_second_signal = is_second_signal
 
 
 class SlotTicker(BaseService):
@@ -60,20 +61,40 @@ class SlotTicker(BaseService):
         await self.cancellation()
 
     async def _keep_ticking(self) -> None:
+        is_second_signal_sent = False
         while self.is_operational:
             elapsed_time = Second(int(time.time()) - self.genesis_time)
             if elapsed_time >= self.seconds_per_slot:
                 slot = Slot(elapsed_time // self.seconds_per_slot + self.genesis_slot)
+                is_second_half = (elapsed_time % self.seconds_per_slot) >= self.seconds_per_slot / 2
                 if slot > self.latest_slot:
                     self.logger.debug(
                         bold_green(f"New slot: {slot}\tElapsed time: {elapsed_time}")
                     )
                     self.latest_slot = slot
+                    is_second_signal = False
                     await self.event_bus.broadcast(
                         NewSlotEvent(
                             slot=slot,
                             elapsed_time=elapsed_time,
+                            is_second_signal=is_second_signal,
                         ),
                         BroadcastConfig(internal=True),
                     )
+                    is_second_signal_sent = False
+                elif slot == self.latest_slot and is_second_half and not is_second_signal_sent:
+                    self.logger.debug(
+                        bold_green(f"Second half of slot: {slot}")
+                    )
+                    is_second_signal = True
+                    await self.event_bus.broadcast(
+                        NewSlotEvent(
+                            slot=slot,
+                            elapsed_time=elapsed_time,
+                            is_second_signal=is_second_signal,
+                        ),
+                        BroadcastConfig(internal=True),
+                    )
+                    is_second_signal_sent = True
+
             await asyncio.sleep(self.seconds_per_slot // DEFAULT_CHECK_FREQUENCY)
