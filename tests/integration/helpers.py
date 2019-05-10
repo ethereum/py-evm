c602b33a6bdad8dcc91a932f5f98abcd915d22dd
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2019-05-10 15:40:38 +0200

    Make CLI tests more strict in respect to error scanning

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -12,12 +12,15 @@ async def scan_for_errors(async_iterable):
     Consume the output produced by the async iterable and throw if it contains hints of an
     uncaught exception.
     """
+
+    error_trigger = ("exception was never retrieved", "Traceback (most recent call last)")
+
     lines_since_error = 0
     async for line in async_iterable:
 
         # We detect errors by some string at the beginning of the Traceback and keep
         # counting lines from there to be able to read and report more valuable info
-        if "Traceback (most recent call last)" in line and lines_since_error == 0:
+        if any(trigger in line for trigger in error_trigger) and lines_since_error == 0:
             lines_since_error = 1
         elif lines_since_error > 0:
             lines_since_error += 1
