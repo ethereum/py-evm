00086a9dd4b04959cd656cc9eab45391355baf38
mhchia <kevin.mh.chia@gmail.com>
2019-05-23 16:17:10 +0800

    Use the latest multiaddr==0.0.8

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ deps = {
     ],
     'libp2p': [
         "base58>=1.0.3",
-        "multiaddr>=0.0.7,<0.1.0",
+        "multiaddr>=0.0.8,<0.1.0",
         "protobuf>=3.6.1",
         "pymultihash>=0.8.2",
     ],
