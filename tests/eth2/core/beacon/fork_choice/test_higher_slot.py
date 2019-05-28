2e6adc56dd8b5299f767824f99f34c0a356f7494
Alex Stokes <r.alex.stokes@gmail.com>
2019-05-28 14:37:16 -0700

    Add test for higher_slot fork choice scoring

---
+++ tests/eth2/core/beacon/fork_choice/test_higher_slot.py
@@ -0,0 +1,16 @@
+import pytest
+
+from eth2.beacon.fork_choice import higher_slot_scoring
+from eth2.beacon.types.blocks import BeaconBlock
+
+
+@pytest.mark.parametrize(
+    "slot",
+    (i for i in range(10)),
+)
+def test_fork_choice_scoring(sample_beacon_block_params, slot):
+    block = BeaconBlock(**sample_beacon_block_params).copy(
+        slot=slot,
+    )
+
+    assert higher_slot_scoring(block) == slot
