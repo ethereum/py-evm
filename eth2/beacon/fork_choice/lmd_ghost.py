fa717883d0fa0df06bc5457aec676774ea27d07e
Alex Stokes <r.alex.stokes@gmail.com>
2019-05-31 17:07:11 -0700

    Finish Store implementation for LMD GHOST

--- eth2/beacon/fork_choice/lmd_ghost.py
+++ eth2/beacon/fork_choice/lmd_ghost.py
@@ -1,19 +1,35 @@
 from typing import Dict, Iterable, Optional, Sequence, Tuple, Type, Union
 
+from eth_typing import (
+    Hash32,
+)
 from eth_utils import (
     to_tuple,
 )
 from eth_utils.toolz import (
+    concat,
     curry,
+    first,
+    identity,
+    mapcat,
+    merge_with,
+    second,
+    valmap,
 )
 
+from eth2.beacon.epoch_processing_helpers import (
+    get_attesting_indices,
+)
 from eth2.beacon.helpers import (
     get_active_validator_indices,
     slot_to_epoch,
 )
 from eth2.beacon.db.chain import BeaconChainDB
+from eth2.beacon.operations.attestation_pool import AttestationPool
+from eth2.beacon.types.attestations import Attestation
 from eth2.beacon.types.attestation_data import AttestationData
 from eth2.beacon.types.blocks import BaseBeaconBlock
+from eth2.beacon.types.pending_attestations import PendingAttestation
 from eth2.beacon.types.states import BeaconState
 from eth2.beacon.typing import (
     Slot,
@@ -23,37 +39,75 @@ from eth2.configs import Eth2Config
 
 
 # TODO(ralexstokes) integrate `AttestationPool` once it has been merged
-class AttestationPool:
-    def get_latest_attestation_by_index(index: ValidatorIndex) -> AttestationData:
-        return AttestationData(
-            slot=1,
-            beacon_block_root=0xdeadbeefcafe,
-            source_epoch=0,
-            source_root=0xdeadbeefcafe,
-            target_root=0xdeadbeefcafe,
-            shard=1,
-            previous_crosslink=0xdeadbeeefcafe,
-            crosslink_data_root=0xdeadbeefcafe,
-        )
-
-
 AttestationIndex = Dict[ValidatorIndex, AttestationData]
+PreIndex = Dict[ValidatorIndex, Tuple[Slot, AttestationData]]
+AttestationLike = Union[Attestation, PendingAttestation]
+
+
+def _slot_from_attestation_data(attestation_data: AttestationData) -> Slot:
+    return attestation_data.slot
+
+
+def _take_latest_attestation_by_slot(
+        candidates: Sequence[Tuple[Slot, AttestationData]]) -> Tuple[Slot, AttestationData]:
+    return max(candidates, key=first)
 
 
 class Store:
     """
     A private class meant to encapsulate data access for the functionality in this module.
     """
-    def __init__(self, db: BeaconChainDB, state: BeaconState, attestation_pool: AttestationPool):
-        self.db = db
+    def __init__(self,
+                 chain_db: BeaconChainDB,
+                 state: BeaconState,
+                 attestation_pool: AttestationPool,
+                 block_class: Type[BaseBeaconBlock],
+                 config: Eth2Config):
+        self._db = chain_db
+        self._block_class = block_class
+        self._config = config
         self._attestation_index = self._build_attestation_index(state, attestation_pool)
+
+    @curry
+    def _mk_pre_index_from_attestation(self,
+                                       state: BeaconState,
+                                       attestation: AttestationLike) -> Iterable[PreIndex]:
+        attestation_data = attestation.data
+        slot = _slot_from_attestation_data(attestation_data)
+
+        # TODO(ralexstokes) the signature of get_attesting_indices
+        # changes in future versions of spec
+        return (
+            {index: (slot, attestation_data)}
+            for index in get_attesting_indices(state, (attestation,), self._config)
+        )
+
+    def _mk_pre_index_from_attestations(self,
+                                        state: BeaconState,
+                                        attestations: Sequence[AttestationLike]) -> PreIndex:
+        """
+        A 'pre-index' is a Dict[ValidatorIndex, Tuple[Slot, AttestationData]].
+        """
+        return merge_with(
+            identity,  # just return a sequence of all collisions by validator index
+            *mapcat(
+                self._mk_pre_index_from_attestation(state),
+                attestations,
+            )
+        )
 
     def _build_attestation_index(self,
                                  state: BeaconState,
                                  attestation_pool: AttestationPool) -> AttestationIndex:
         """
         Assembles a dictionary of latest attestations keyed by validator index.
-        Any attestation made by a validator in the ``attestation_pool`` that occur after the last known attestation according to the state take precedence.
+        Any attestation made by a validator in the ``attestation_pool`` that occur after the
+        last known attestation according to the state take precedence.
+
+        We start by building a 'pre-index' from all known attestations which map validator
+        indices to a pair of slot and attestation data. A final index is built from all
+        pre-indices by keeping the entry with the highest slot across the set of all
+        duplicates in the pre-indices keyed by validator index.
         """
         previous_epoch_index = self._mk_pre_index_from_attestations(
             state,
@@ -102,12 +156,20 @@ class Store:
         target_block = self._get_block_by_root(attestation.beacon_block_root)
         return target_block
 
-    def get_ancestor(self, index):
+    def _get_previous_block(self, block: BaseBeaconBlock) -> BaseBeaconBlock:
+        return self._db.get_block_by_root(block.previous_block_root, self._block_class)
+
+    def get_ancestor(self, block: BaseBeaconBlock, slot: Slot) -> BaseBeaconBlock:
         """
         Return the block in the chain that is a
         predecessor of ``block`` at the requested ``slot``.
         """
-        pass
+        if block.slot == slot:
+            return block
+        elif block.slot < slot:
+            return None
+        else:
+            return self.get_ancestor(self._get_previous_block(block), slot)
 
 
 AttestationTarget = Tuple[ValidatorIndex, Optional[BaseBeaconBlock]]
@@ -168,12 +230,13 @@ def lmd_ghost_scoring(chain_db: BeaconChainDB,
                       attestation_pool: AttestationPool,
                       state: BeaconState,
                       config: Eth2Config,
+                      block_class: Type[BaseBeaconBlock],
                       block: BaseBeaconBlock) -> int:
     """
     Return the score of the ``target_block`` according to the LMD GHOST algorithm,
     using the lexicographic ordering of the block root to break ties.
     """
-    store = Store(chain_db, state, attestation_pool)
+    store = Store(chain_db, state, attestation_pool, block_class, config)
 
     attestation_targets = _find_latest_attestation_targets(state, store, config)
 
