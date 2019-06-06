f205ed099c5534892c3afbbd1b14a2fa7f597673
Jason Carver <ut96caarrs@snkmail.com>
2019-06-05 20:47:03 -0700

    Bump version: 0.2.0-alpha.43 → 0.3.0-alpha.1

--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ with open('README.md') as readme_file:
 setup(
     name='py-evm',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='0.2.0-alpha.43',
+    version='0.3.0-alpha.1',
     description='Python implementation of the Ethereum Virtual Machine',
     long_description=long_description,
     long_description_content_type='text/markdown',
