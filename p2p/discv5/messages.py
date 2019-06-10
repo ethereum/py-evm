697d6b0cc6c30fadcefa03d9c1c52dbb038c9f34
Jannik Luhn <jannik@brainbot.com>
2019-06-10 11:57:32 +0200

    Implement discv5 message types

---
+++ p2p/discv5/messages.py
@@ -0,0 +1,183 @@
+from typing import (
+    Dict,
+    Type,
+    TYPE_CHECKING,
+)
+from collections import (
+    UserDict,
+)
+
+import rlp
+from rlp.sedes import (
+    big_endian_int,
+    binary,
+    boolean,
+    Binary,
+    CountableList,
+)
+
+from p2p.discv5.enr import (
+    ENR,
+)
+from p2p.discv5.constants import (
+    TOPIC_HASH_SIZE,
+    IP_V4_SIZE,
+    IP_V6_SIZE,
+)
+
+
+# https://github.com/python/mypy/issues/5264#issuecomment-399407428
+if TYPE_CHECKING:
+    MessageTypeRegistryBaseType = UserDict[bytes, Type["MessageData"]]
+else:
+    MessageTypeRegistryBaseType = UserDict
+
+
+#
+# Custom sedes objects
+#
+topic_sedes = Binary.fixed_length(TOPIC_HASH_SIZE)
+
+
+class IPAddressSedes(Binary):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def is_valid_length(self, length: int) -> bool:
+        return length in (IP_V4_SIZE, IP_V6_SIZE)
+
+
+ip_address_sedes = IPAddressSedes()
+
+
+class MessageTypeRegistry(MessageTypeRegistryBaseType):
+
+    def register(self,
+                 message_data_class: Type["MessageData"]
+                 ) -> Type["MessageData"]:
+        """Class Decorator to register MessageData classes."""
+        message_type = message_data_class.message_type
+        if message_type is None:
+            raise ValueError("Message type must be defined")
+
+        if message_type in self:
+            raise ValueError(f"Message with type {message_type} is already defined")
+
+        if not self:
+            expected_message_type = 1
+        else:
+            expected_message_type = max(self.keys()) + 1
+
+        if not message_type == expected_message_type:
+            raise ValueError(
+                f"Expected message type {expected_message_type}, but got {message_type}",
+            )
+
+        self[message_type] = message_data_class
+
+        return message_data_class
+
+
+default_message_type_registry = MessageTypeRegistry()
+
+
+#
+# Message types
+#
+class MessageData(rlp.Serializable):
+    message_type: int
+
+
+@default_message_type_registry.register
+class PingData(MessageData):
+    message_type = 1
+
+    fields = (
+        ("request_id", big_endian_int),
+        ("enr_seq", big_endian_int),
+    )
+
+
+@default_message_type_registry.register
+class PongData(MessageData):
+    message_type = 2
+
+    fields = (
+        ("request_id", big_endian_int),
+        ("enr_seq", big_endian_int),
+        ("packet_ip", ip_address_sedes),
+        ("packet_port", big_endian_int),
+    )
+
+
+@default_message_type_registry.register
+class FindNodeData(MessageData):
+    message_type = 3
+
+    fields = (
+        ("request_id", big_endian_int),
+        ("distance", big_endian_int),
+    )
+
+
+@default_message_type_registry.register
+class NodesData(MessageData):
+    message_type = 4
+
+    fields = (
+        ("request_id", big_endian_int),
+        ("total", big_endian_int),
+        ("enrs", CountableList(ENR)),
+    )
+
+
+@default_message_type_registry.register
+class ReqTicketData(MessageData):
+    message_type = 5
+
+    fields = (
+        ("request_id", big_endian_int),
+        ("topic", topic_sedes),
+    )
+
+
+@default_message_type_registry.register
+class TicketData(MessageData):
+    message_type = 6
+
+    fields = (
+        ("request_id", big_endian_int),
+        ("ticket", binary),
+        ("wait_time", big_endian_int),
+    )
+
+
+@default_message_type_registry.register
+class RegTopicData(MessageData):
+    message_type = 7
+
+    fields = (
+        ("request_id", big_endian_int),
+        ("ticket", binary),
+    )
+
+
+@default_message_type_registry.register
+class RegConfirmationData(MessageData):
+    message_type = 8
+
+    fields = (
+        ("request_id", big_endian_int),
+        ("registered", boolean),
+    )
+
+
+@default_message_type_registry.register
+class TopicQueryData(MessageData):
+    message_type = 9
+
+    fields = (
+        ("request_id", big_endian_int),
+        ("topic", topic_sedes),
+    )
