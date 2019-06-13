3e24159371cb9ec55226548a3115788da6e61642
Alex Stokes <r.alex.stokes@gmail.com>
2019-06-13 08:29:02 +0100

    Update AttestationData

--- eth2/beacon/types/attestation_data.py
+++ eth2/beacon/types/attestation_data.py
@@ -1,3 +1,7 @@
+from eth.constants import (
+    ZERO_HASH32,
+)
+
 from eth_typing import (
     Hash32,
 )
@@ -10,8 +14,6 @@ from ssz.sedes import (
 
 from eth2.beacon.typing import (
     Epoch,
-    Slot,
-    Shard,
 )
 from eth2.beacon.types.crosslinks import Crosslink
 from eth_utils import (
@@ -23,38 +25,32 @@ class AttestationData(ssz.Serializable):
 
     fields = [
         # LMD GHOST vote
-        ('slot', uint64),
         ('beacon_block_root', bytes32),
 
         # FFG vote
         ('source_epoch', uint64),
         ('source_root', bytes32),
+        ('target_epoch', uint64),
         ('target_root', bytes32),
 
         # Crosslink vote
-        ('shard', uint64),
-        ('previous_crosslink', Crosslink),
-        ('crosslink_data_root', bytes32),
+        ('crosslink', Crosslink),
     ]
 
     def __init__(self,
-                 slot: Slot,
-                 beacon_block_root: Hash32,
-                 source_epoch: Epoch,
-                 source_root: Hash32,
-                 target_root: Hash32,
-                 shard: Shard,
-                 previous_crosslink: Crosslink,
-                 crosslink_data_root: Hash32) -> None:
+                 beacon_block_root: Hash32=ZERO_HASH32,
+                 source_epoch: Epoch=0,
+                 source_root: Hash32=ZERO_HASH32,
+                 target_epoch: Epoch=0,
+                 target_root: Hash32=ZERO_HASH32,
+                 crosslink: Crosslink=Crosslink()) -> None:
         super().__init__(
-            slot=slot,
             beacon_block_root=beacon_block_root,
             source_epoch=source_epoch,
             source_root=source_root,
+            target_epoch=target_epoch,
             target_root=target_root,
-            shard=shard,
-            previous_crosslink=previous_crosslink,
-            crosslink_data_root=crosslink_data_root,
+            crosslink=crosslink,
         )
 
     def __str__(self) -> str:
