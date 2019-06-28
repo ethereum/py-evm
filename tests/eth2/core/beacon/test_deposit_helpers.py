9409a8ff762c8c66849c058a35eb21df67756569
Alex Stokes <r.alex.stokes@gmail.com>
2019-06-27 18:07:47 -0700

    Fix tests for deposit helpers

--- tests/eth2/core/beacon/test_deposit_helpers.py
+++ tests/eth2/core/beacon/test_deposit_helpers.py
@@ -4,141 +4,104 @@ from eth_utils import (
     ValidationError,
 )
 
-import ssz
-
-from eth2._utils.hash import (
-    hash_eth2,
-)
-from eth2._utils.merkle.common import (
-    get_merkle_proof,
-)
-from eth2._utils.merkle.sparse import (
-    calc_merkle_tree_from_leaves,
-    get_merkle_root,
-)
-
 from eth2.beacon.deposit_helpers import (
     process_deposit,
     validate_deposit_proof,
 )
-from eth2.beacon.types.forks import Fork
-from eth2.beacon.types.states import BeaconState
-from eth2.beacon.types.deposits import Deposit
 
-from eth2.beacon.tools.builder.validator import (
-    create_mock_deposit_data,
+from eth2.beacon.tools.builder.initializer import (
+    create_mock_deposit,
 )
-
-
-def create_mock_deposit(config,
-                        sample_beacon_state_params,
-                        keymap,
-                        pubkeys,
-                        withdrawal_credentials,
-                        validator_index):
-    state = BeaconState(**sample_beacon_state_params).copy(
-        slot=1,
-        validators=(),
-    )
-    fork = Fork(
-        previous_version=config.GENESIS_FORK_VERSION.to_bytes(4, 'little'),
-        current_version=config.GENESIS_FORK_VERSION.to_bytes(4, 'little'),
-        epoch=config.GENESIS_EPOCH,
-    )
-    deposit_data = create_mock_deposit_data(
-        config=config,
-        pubkeys=pubkeys,
-        keymap=keymap,
-        validator_index=validator_index,
-        withdrawal_credentials=withdrawal_credentials,
-        fork=fork,
-    )
-
-    item = hash_eth2(ssz.encode(deposit_data))
-    test_deposit_data_leaves = (item,)
-    tree = calc_merkle_tree_from_leaves(test_deposit_data_leaves)
-    root = get_merkle_root(test_deposit_data_leaves)
-    proof = list(get_merkle_proof(tree, item_index=validator_index))
-
-    state = state.copy(
-        eth1_data=state.eth1_data.copy(
-            deposit_root=root,
-        ),
-    )
-
-    deposit = Deposit(
-        proof=proof,
-        index=validator_index,
-        deposit_data=deposit_data,
-    )
-
-    return state, deposit
 
 
 @pytest.mark.parametrize(
     (
-        'deposit_index',
         'success',
     ),
     [
-        (0, True),
-        (1, False),
+        (True,),
+        (False,),
     ]
 )
 def test_validate_deposit_proof(config,
-                                sample_beacon_state_params,
                                 keymap,
                                 pubkeys,
                                 deposit_contract_tree_depth,
-                                deposit_index,
+                                genesis_state,
                                 success):
-    validator_index = 0
+    state = genesis_state
     withdrawal_credentials = b'\x34' * 32
     state, deposit = create_mock_deposit(
-        config,
-        sample_beacon_state_params,
+        state,
+        pubkeys[0],
         keymap,
-        pubkeys,
         withdrawal_credentials,
-        validator_index,
-    )
-    deposit = deposit.copy(
-        index=deposit_index,
+        config,
     )
 
     if success:
         validate_deposit_proof(state, deposit, deposit_contract_tree_depth)
     else:
+        deposit = deposit.copy(
+            data=deposit.data.copy(
+                withdrawal_credentials=b'\x23' * 32,
+            )
+        )
         with pytest.raises(ValidationError):
             validate_deposit_proof(state, deposit, deposit_contract_tree_depth)
 
 
+@pytest.mark.parametrize(
+    (
+        'is_new_validator',
+    ),
+    [
+        (True,),
+        (False,),
+    ]
+)
 def test_process_deposit(config,
                          sample_beacon_state_params,
                          keymap,
+                         genesis_state,
+                         validator_count,
+                         is_new_validator,
                          pubkeys):
-    validator_index = 0
+    state = genesis_state
     withdrawal_credentials = b'\x34' * 32
+    if is_new_validator:
+        validator_index = validator_count
+    else:
+        validator_index = validator_count - 1
+
+    pubkey = pubkeys[validator_index]
+
     state, deposit = create_mock_deposit(
-        config,
-        sample_beacon_state_params,
+        state,
+        pubkey,
         keymap,
-        pubkeys,
         withdrawal_credentials,
-        validator_index,
+        config,
     )
 
-    # Add the first validator
+    validator_count_before_deposit = state.validator_count
+
     result_state = process_deposit(
         state=state,
         deposit=deposit,
         config=config,
     )
 
-    assert len(result_state.validators) == 1
+    # test immutability
+    assert len(state.validators) == validator_count_before_deposit
+
     validator = result_state.validators[validator_index]
-    assert validator.pubkey == pubkeys[validator_index]
-    assert validator.withdrawal_credentials == withdrawal_credentials
-    assert result_state.balances[validator_index] == config.MAX_EFFECTIVE_BALANCE
-    # test immutable
-    assert len(state.validators) == 0
+    if is_new_validator:
+        assert len(result_state.validators) == len(state.validators) + 1
+        assert validator.pubkey == pubkeys[validator_index]
+        assert validator.withdrawal_credentials == withdrawal_credentials
+        assert result_state.balances[validator_index] == config.MAX_EFFECTIVE_BALANCE
+    else:
+        assert len(result_state.validators) == len(state.validators)
+        assert validator.pubkey == pubkeys[validator_index]
+        assert result_state.balances[validator_index] == 2 * config.MAX_EFFECTIVE_BALANCE
