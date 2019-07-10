4fff52ced9bdb5ddc503e0e83c7124dbcd57ad94
Alex Stokes <r.alex.stokes@gmail.com>
2019-07-10 12:54:38 -0700

    Add `CompactCommittee` type

---
+++ eth2/beacon/types/compact_committees.py
@@ -0,0 +1,37 @@
+from typing import (
+    Sequence,
+)
+
+from eth_typing import (
+    BLSPubkey,
+)
+
+import ssz
+from ssz.sedes import (
+    bytes48,
+    List,
+    uint64,
+)
+
+from .defaults import (
+    default_tuple,
+)
+
+
+class CompactCommittee(ssz.Serializable):
+
+    fields = [
+        ('pubkeys', List(bytes48)),
+        ('compact_validators', List(uint64)),
+    ]
+
+    def __init__(self,
+                 pubkeys: Sequence[BLSPubkey]=default_tuple,
+                 compact_validators: Sequence[int]=default_tuple) -> None:
+        super().__init__(
+            pubkeys=pubkeys,
+            compact_validators=compact_validators,
+        )
+
+
+default_compact_committee = CompactCommittee()
