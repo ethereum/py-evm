112b9bd6d5fe7691390794ba89acacdd75f8462e
Alex Stokes <r.alex.stokes@gmail.com>
2019-07-30 17:41:05 -0700

    Add test for block header

--- eth2/beacon/tools/fixtures/test_types/operations.py
+++ eth2/beacon/tools/fixtures/test_types/operations.py
@@ -1,5 +1,81 @@
+from pathlib import Path
+from typing import (
+    Any,
+    Dict,
+    Tuple,
+)
+
+from eth_utils import (
+    ValidationError,
+)
+
+from ssz.tools import (
+    from_formatted_dict,
+)
+
+
+from eth2.beacon.tools.fixtures.config_types import ConfigType
+from eth2.beacon.tools.fixtures.conditions import verify_state
+from eth2.beacon.tools.fixtures.test_handler import TestHandler
+from eth2.beacon.state_machines.forks.serenity.block_processing import process_block_header
+from eth2.beacon.types.blocks import BeaconBlock
+from eth2.beacon.types.states import BeaconState
+from eth2.configs import Eth2Config
+
 from . import TestType
 
 
-class Operations(TestType):
-    pass
+class BlockHeaderHandler(TestHandler):
+    name = "block_header"
+
+    def parse_inputs(self,
+                     test_case_data: Dict[str, Any]) -> Tuple[BeaconState, BeaconBlock]:
+        return (
+            from_formatted_dict(
+                test_case_data["pre"],
+                BeaconState,
+            ),
+            from_formatted_dict(
+                test_case_data["block"],
+                BeaconBlock,
+            ),
+        )
+
+    def parse_outputs(self, test_case_data: Dict[str, Any]) -> BeaconState:
+        return from_formatted_dict(
+            test_case_data["post"],
+            BeaconState,
+        )
+
+    def valid(self, test_case_data: Dict[str, Any]) -> bool:
+        return bool(test_case_data["post"])
+
+    def run_with(self,
+                 inputs: BeaconState,
+                 config: Eth2Config) -> BeaconState:
+        state, block = inputs
+        check_proposer_signature = True
+        try:
+            return process_block_header(state, block, config, check_proposer_signature)
+        # catch ValueError for bad signature
+        except ValueError as e:
+            raise ValidationError(e)
+
+    def condition(self, output: BeaconState, expected_output: BeaconState) -> None:
+        verify_state(output, expected_output)
+
+
+class OperationsTestType(TestType):
+    name = "operations"
+
+    handlers = (
+        BlockHeaderHandler,
+    )
+
+    @classmethod
+    def build_path(cls,
+                   tests_root_path: Path,
+                   test_handler: TestHandler,
+                   config_type: ConfigType) -> Path:
+        file_name = f"{test_handler.name}_{config_type.name}.yaml"
+        return tests_root_path / Path(cls.name) / Path(test_handler.name) / Path(file_name)
---
+++ tests/eth2/fixtures/test_operations.py
@@ -0,0 +1,36 @@
+import pytest
+
+from eth_utils import (
+    ValidationError,
+)
+
+from eth2.beacon.tools.fixtures.test_gen import (
+    pytest_from_eth2_fixture,
+    generate_pytests_from_eth2_fixture,
+)
+from eth2.beacon.tools.fixtures.config_types import (
+    Minimal,
+)
+from eth2.beacon.tools.fixtures.test_types.operations import (
+    OperationsTestType,
+)
+
+
+def pytest_generate_tests(metafunc):
+    generate_pytests_from_eth2_fixture(metafunc)
+
+
+@pytest_from_eth2_fixture({
+    "config_types": (
+        Minimal,
+    ),
+    "test_types": {
+        OperationsTestType: lambda handler: handler.name == "block_header",
+    }
+})
+def test_block_header(test_case):
+    if test_case.valid():
+        test_case.execute()
+    else:
+        with pytest.raises(ValidationError):
+            test_case.execute()
