8392a1b644cc54f066c142971392fb6c5d5aefaf
Alex Stokes <r.alex.stokes@gmail.com>
2019-08-28 10:20:13 -0700

    Remove conflicting configuration type

--- tests/eth2/fixtures/test_shuffling.py
+++ tests/eth2/fixtures/test_shuffling.py
@@ -1,4 +1,4 @@
-from eth2.beacon.tools.fixtures.config_types import Full, Minimal
+from eth2.beacon.tools.fixtures.config_types import Minimal
 from eth2.beacon.tools.fixtures.test_gen import (
     generate_pytests_from_eth2_fixture,
     pytest_from_eth2_fixture,
@@ -17,6 +17,6 @@ def test_minimal(test_case):
     test_case.execute()
 
 
-@pytest_from_eth2_fixture({"config_types": (Full,), "test_types": (ShufflingTestType,)})
-def test_full(test_case):
-    test_case.execute()
+# @pytest_from_eth2_fixture({"config_types": (Full,), "test_types": (ShufflingTestType,)})
+# def test_full(test_case):
+#     test_case.execute()
