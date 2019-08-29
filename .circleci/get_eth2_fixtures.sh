54789aa1e95aa5fe04b0fcd21f840ab6789373e3
Alex Stokes <r.alex.stokes@gmail.com>
2019-08-29 14:22:36 -0700

    Fix duplicated path in unzip

--- .circleci/get_eth2_fixtures.sh
+++ .circleci/get_eth2_fixtures.sh
@@ -4,8 +4,8 @@ if [ ! -d "./eth2-fixtures/tests" ]; then
   wget -c https://github.com/ethereum/eth2.0-spec-tests/releases/download/v0.8.3/general.tar.gz
   wget -c https://github.com/ethereum/eth2.0-spec-tests/releases/download/v0.8.3/mainnet.tar.gz
   wget -c https://github.com/ethereum/eth2.0-spec-tests/releases/download/v0.8.3/minimal.tar.gz
-  mkdir -p eth2-fixtures/tests
-  tar zxvf general.tar.gz -C ./eth2-fixtures/tests
-  tar zxvf mainnet.tar.gz -C ./eth2-fixtures/tests
-  tar zxvf minimal.tar.gz -C ./eth2-fixtures/tests
+  mkdir eth2-fixtures
+  tar zxvf general.tar.gz -C ./eth2-fixtures
+  tar zxvf mainnet.tar.gz -C ./eth2-fixtures
+  tar zxvf minimal.tar.gz -C ./eth2-fixtures
 fi
