6a91e17e66d78449b744db98a673384fdd53fc58
Alex Stokes <r.alex.stokes@gmail.com>
2019-08-29 11:02:36 -0700

    Update eth2 linting deps for flake8 version

--- setup.py
+++ setup.py
@@ -115,6 +115,10 @@ deps = {
     'eth2-lint': [
         "black==19.3b0",
         "isort==4.3.21",
+        "flake8==3.7.8",
+        "flake8-bugbear==19.8.0",
+        "mypy==0.720",
+        "sqlalchemy-stubs==0.2",
     ],
 }
 
