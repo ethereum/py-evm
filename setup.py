d8f4571ff8e0f523154da34857293c89d0ef517d
Jason Carver <ut96caarrs@snkmail.com>
2019-09-11 12:58:48 -0700

    Upgrade libp2p to use pypi release

--- setup.py
+++ setup.py
@@ -47,8 +47,7 @@ deps = {
         "argcomplete>=1.10.0,<2",
         "multiaddr>=0.0.8,<0.1.0",
         "pymultihash>=0.8.2",
-        # FIXME: Change to PyPI when the commit is released.
-        "libp2p @ git+https://git@github.com/libp2p/py-libp2p@f38899e",
+        "libp2p>=0.1.1,<0.2",
     ],
     'test': [
         "async-timeout>=3.0.1,<4",
