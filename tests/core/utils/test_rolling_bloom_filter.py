25623fbc80858ae375b54733b6f44c2acd69f5d2
Piper Merriam <pipermerriam@gmail.com>
2019-09-18 09:41:31 -0600

    PR feedback

--- tests/core/utils/test_rolling_bloom_filter.py
+++ tests/core/utils/test_rolling_bloom_filter.py
@@ -5,27 +5,28 @@ def test_rolling_bloom_maintains_history():
     # only one historical filter
     bloom = RollingBloom(generation_size=10, max_generations=2)
 
+    bloom_values = tuple(bytes((i,)) for i in range(20))
+
     # fill up the main filter and the history
-    for i in range(20):
-        value = bytes((i,))
+    for value in bloom_values:
         bloom.add(value)
         assert value in bloom
 
-    for i in range(20):
-        value = bytes((i,))
+    # since the filter discards old history, we loop back over the values
+    for value in bloom_values:
         assert value in bloom
 
     # this should eject all of the 0-9 values
+    assert b'\xff' not in bloom_values
     bloom.add(b'\xff')
 
     # this must be done probabalistically since bloom filters have false
     # positives.  At least one of the 0-9 values should be gone.
     assert any(
-        bytes((value,)) not in bloom
-        for value in range(10)
+        value not in bloom
+        for value in bloom_values[:10]
     )
 
-    for i in range(10, 20):
-        value = bytes((i,))
+    for value in bloom_values[10:]:
         assert value in bloom
     assert b'\xff' in bloom
--- trinity/_utils/bloom.py
+++ trinity/_utils/bloom.py
@@ -13,14 +13,14 @@ class RollingBloom:
         if max_generations < 2:
             raise ValueError(f"max_generations must be 2 or more: got {max_generations}")
         self._max_bloom_elements = generation_size
-        self._max_generations = max_generations
+        self._max_history = max_generations - 1
         self._history: Deque[BloomFilter] = collections.deque()
         self._active = BloomFilter(max_elements=self._max_bloom_elements)
         self._items_in_active = 0
 
     def add(self, key: bytes) -> None:
-        # before adding check to see if we need to cycle the push the active
-        # bloom filter into history.
+        # before adding the value check if the active bloom filter is full.  If
+        # so, push it into the history and make a new one.
         if self._items_in_active >= self._max_bloom_elements:
             self._history.appendleft(self._active)
             self._active = BloomFilter(max_elements=self._max_bloom_elements)
@@ -28,7 +28,7 @@ class RollingBloom:
 
             # discard any old history that is older than the number of
             # generations we should be retaining.
-            while len(self._history) > self._max_generations - 1:
+            while len(self._history) > self._max_history:
                 self._history.pop()
 
         self._active.add(key)
