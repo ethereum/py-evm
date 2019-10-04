1bb3f2d930a6e3b9c7a665f018d6c56dbb340664
Noel Maersk <veox@users.noreply.github.com>
2019-10-04 16:46:55 +0300

    EIP-161 collect_touched_accounts(): relax to geth's level.
    Since geth is now filling the tests, we have to do it like geth:
    
    Walk the entire computation tree, including branches that errored
    out (got REVERTed, OOGed, etc.), looking for wabbit RIPEMD160,
    _always_ collecting it. Skip everything else on errored branches.
    
    SQUASHED:
    
    tests: fix import when DEBUG2 logging.
    
    Logging utilities have moved to separate package, eth_utils.

--- eth/vm/forks/spurious_dragon/_utils.py
+++ eth/vm/forks/spurious_dragon/_utils.py
@@ -17,7 +17,8 @@ THREE = force_bytes_to_address(b'\x03')
 
 
 @to_set
-def collect_touched_accounts(computation: BaseComputation) -> Iterable[Address]:
+def collect_touched_accounts(computation: BaseComputation,
+                             ancestor_had_error: bool = False) -> Iterable[Address]:
     """
     Collect all of the accounts that *may* need to be deleted based on
     `EIP-161 <https://eips.ethereum.org/EIPS/eip-161>`_.
@@ -32,26 +33,30 @@ def collect_touched_accounts(computation: BaseComputation) -> Iterable[Address]:
 
     # collect those explicitly marked for deletion ("beneficiary" is of SELFDESTRUCT)
     for beneficiary in sorted(set(computation.accounts_to_delete.values())):
-        if computation.is_error and computation.is_origin_computation:
-            # Special case to account for geth+parity bug
-            # https://github.com/ethereum/EIPs/issues/716
-            if beneficiary == THREE:
-                yield beneficiary
+        # Special case to account for geth+parity bug
+        # https://github.com/ethereum/EIPs/issues/716
+        if beneficiary == THREE:
+            yield beneficiary
+            continue
+        if (computation.is_error and computation.is_origin_computation) or ancestor_had_error:
             continue
         else:
             yield beneficiary
 
     # collect account directly addressed
     if computation.msg.to != constants.CREATE_CONTRACT_ADDRESS:
-        if computation.is_error and computation.is_origin_computation:
-            # Special case to account for geth+parity bug
-            # https://github.com/ethereum/EIPs/issues/716
-            if computation.msg.to == THREE:
-                yield computation.msg.to
+        if (computation.is_error and computation.is_origin_computation) or ancestor_had_error:
+            pass
         else:
             yield computation.msg.to
 
-    # recurse into nested computations if this one was successful
-    if not computation.is_error:
-        for child in computation.children:
-            yield from collect_touched_accounts(child)
+    # collect RIPEMD160 precompile even if ancestor computation had error, since we'll be
+    # skipping collection from children of errored-out computation otherwise
+    if computation.msg.to == THREE and (computation.is_error or ancestor_had_error):
+        yield computation.msg.to
+
+    # recurse into nested computations
+    for child in computation.children:
+        yield from collect_touched_accounts(
+            child,
+            ancestor_had_error=(computation.is_error or ancestor_had_error))
