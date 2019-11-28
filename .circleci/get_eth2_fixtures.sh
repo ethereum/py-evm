7fcc69a2d4e7b75f99552663010ade3e829bddfc
Alex Stokes <r.alex.stokes@gmail.com>
2019-11-27 16:16:32 -0800

    Update CI eth2 fixtures to v0.9.2

--- .circleci/get_eth2_fixtures.sh
+++ .circleci/get_eth2_fixtures.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if [ ! -d "./eth2-fixtures/tests" ]; then
-  wget -c https://github.com/ethereum/eth2.0-spec-tests/releases/download/v0.9.1/general.tar.gz
-  wget -c https://github.com/ethereum/eth2.0-spec-tests/releases/download/v0.9.1/mainnet.tar.gz
-  wget -c https://github.com/ethereum/eth2.0-spec-tests/releases/download/v0.9.1/minimal.tar.gz
+  wget -c https://github.com/ethereum/eth2.0-spec-tests/releases/download/v0.9.2/general.tar.gz
+  wget -c https://github.com/ethereum/eth2.0-spec-tests/releases/download/v0.9.2/mainnet.tar.gz
+  wget -c https://github.com/ethereum/eth2.0-spec-tests/releases/download/v0.9.2/minimal.tar.gz
   mkdir eth2-fixtures
   tar zxvf general.tar.gz -C ./eth2-fixtures
   tar zxvf mainnet.tar.gz -C ./eth2-fixtures
