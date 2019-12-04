0e3e67d2ceb52e124fcb866f11d3f9704cfe04bf
Jason Carver <ut96caarrs@snkmail.com>
2019-12-04 14:32:25 -0800

    Bump version: 0.1.0-alpha.30 → 0.1.0-alpha.31

--- setup.py
+++ setup.py
@@ -173,7 +173,7 @@ with open('./README.md') as readme:
 setup(
     name='trinity',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='0.1.0-alpha.30',
+    version='0.1.0-alpha.31',
     description='The Trinity client for the Ethereum network',
     long_description=long_description,
     long_description_content_type='text/markdown',
