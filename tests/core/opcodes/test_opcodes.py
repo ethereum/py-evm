c1647462cd570079d70689a516c33cd4197e99bd
Giuseppe Cerami <peppece14@gmail.com>
2020-11-03 18:11:18 +0100

    [WIP] Implement subroutine opcodes

--- tests/core/opcodes/test_opcodes.py
+++ tests/core/opcodes/test_opcodes.py
@@ -1493,12 +1493,12 @@ def test_jumpsub(vm_class, code, expect_gas_used):
             '0x5c5d00',
             OutOfGas,
         ),
-        (  #tests if the opcode raises error when trying to jump to BEGINSUB into pushdata
+        (  # tests if the opcode raises error when trying to jump to BEGINSUB into pushdata
             BerlinVM,
             '0x60055e61005c58',
             InvalidJumpDestination,
         ),
-        (  #tests if the opcode raises error when trying to jump to an opcode other than BEGINGSUB
+        (  # tests if the opcode raises error when trying to jump to an opcode other than BEGINGSUB
             BerlinVM,
             '0x6100055e0058',
             InvalidJumpDestination,
