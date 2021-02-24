1a998d60250791504bca611e7bb5fb36791e22a9
kclowes <kclowes@users.noreply.github.com>
2021-02-24 13:50:39 -0700

    EIP-2565 calculation of modexp precompile gas cost

--- tests/core/vm/test_modexp_precompile.py
+++ tests/core/vm/test_modexp_precompile.py
@@ -1,3 +1,5 @@
+import json
+import os
 import pytest
 
 from eth_utils import (
@@ -6,59 +8,50 @@ from eth_utils import (
 
 from eth.precompiles.modexp import (
     _modexp,
-    _compute_modexp_gas_fee,
+    _compute_modexp_gas_fee_eip_198,
+)
+from eth.vm.forks.berlin.computation import (
+    _compute_modexp_gas_fee_eip_2565,
 )
 
 
-EIP198_VECTOR_A = decode_hex(
-    "0000000000000000000000000000000000000000000000000000000000000001"
-    "0000000000000000000000000000000000000000000000000000000000000020"
-    "0000000000000000000000000000000000000000000000000000000000000020"
-    "03"
-    "fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2e"
-    "fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f"
-)
+def vectors():
+    filepath = os.path.join(os.path.dirname(__file__),
+                            'fixtures/modexp_precompile_test_vectors.json')
+    f = open(filepath)
+    return json.load(f)
 
-EIP198_VECTOR_B = decode_hex(
-    "0000000000000000000000000000000000000000000000000000000000000000"
-    "0000000000000000000000000000000000000000000000000000000000000020"
-    "0000000000000000000000000000000000000000000000000000000000000020"
-    "fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2e"
-    "fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f"
-)
 
-EIP198_VECTOR_C = decode_hex(
-    "0000000000000000000000000000000000000000000000000000000000000000"
-    "0000000000000000000000000000000000000000000000000000000000000020"
-    "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
-    "fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe"
-    "fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffd"
-)
+def eip_198_gas_cost_vectors():
+    return [(decode_hex(v["input"]), v["eip_198_gas"]) for v in vectors()]
+
+
+def eip_2565_gas_cost_vectors():
+    return [(decode_hex(v["input"]), v["eip_2565_gas"]) for v in vectors()]
+
+
+def modexp_result():
+    return [
+        (decode_hex(v["input"]), int(v["expected"], 16))
+        for v in vectors()
+    ]
 
 
 @pytest.mark.parametrize(
-    'data,expected',
-    (
-        (EIP198_VECTOR_A, 13056),
-        (
-            EIP198_VECTOR_C,
-            10684346944173007063723051170445283632835119638284563472873463025465780712173320789629146724657549280936306536701227228889744512638312451529980055895215896,  # noqa: E501
-        ),
-    ),
+    'data,expected', eip_198_gas_cost_vectors()
 )
 def test_modexp_gas_fee_calculation(data, expected):
-    actual = _compute_modexp_gas_fee(data)
+    actual = _compute_modexp_gas_fee_eip_198(data)
     assert actual == expected
 
 
-@pytest.mark.parametrize(
-    'data,expected',
-    (
-        (EIP198_VECTOR_A, 1),
-        (EIP198_VECTOR_B, 0),
-        (EIP198_VECTOR_C, 0),
-    ),
-)
+@pytest.mark.parametrize('data,expected', eip_2565_gas_cost_vectors())
+def test_modexp_gas_fee_calculation_eip_2565(data, expected):
+    actual = _compute_modexp_gas_fee_eip_2565(data)
+    assert actual == expected
+
+
+@pytest.mark.parametrize('data,expected', modexp_result())
 def test_modexp_result(data, expected):
     actual = _modexp(data)
     assert actual == expected
