cee7e7e00b46583445356fb5fe797fcb6ac820b1
fselmo <fselmo2@gmail.com>
2023-03-10 16:34:24 -0700

    Rework the logic for EIP-3860
    - Create a more general class method for validating the create message as a whole and bring the initcode size validation into that. This way, if other fields of the create message need validation in the future, we have a more useful method that's already abstracted and can be invoked in the proper VM computation class.
    
    - Consume the initcode cost before the computation starts for create transactions and handle initcode cost in the opcode implementations for ``CREATE`` and ``CREATE2``.

---
+++ eth/vm/forks/shanghai/logic.py
@@ -0,0 +1,27 @@
+from .constants import (
+    INITCODE_WORD_COST,
+)
+from eth._utils.numeric import (
+    ceil32,
+)
+from eth.vm.forks.berlin.logic import (
+    CreateEIP2929,
+    Create2EIP2929,
+)
+from eth.vm.logic.system import (
+    CreateOpcodeStackData,
+)
+
+
+class CreateEIP3860(CreateEIP2929):
+    def get_gas_cost(self, data: CreateOpcodeStackData) -> int:
+        eip2929_gas_cost = super().get_gas_cost(data)
+        eip3860_gas_cost = INITCODE_WORD_COST * ceil32(data.memory_length) // 32
+        return eip2929_gas_cost + eip3860_gas_cost
+
+
+class Create2EIP3860(Create2EIP2929):
+    def get_gas_cost(self, data: CreateOpcodeStackData) -> int:
+        eip2929_gas_cost = super().get_gas_cost(data)
+        eip3860_gas_cost = INITCODE_WORD_COST * ceil32(data.memory_length) // 32
+        return eip2929_gas_cost + eip3860_gas_cost
