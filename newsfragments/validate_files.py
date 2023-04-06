72bb43ed06fba51e2408101bf3ae510ff4ca1937
fselmo <fselmo2@gmail.com>
2023-04-06 09:20:51 -0600

    Update towncrier types to match the template repo

--- newsfragments/validate_files.py
+++ newsfragments/validate_files.py
@@ -3,31 +3,43 @@
 # Towncrier silently ignores files that do not match the expected ending.
 # We use this script to ensure we catch these as errors in CI.
 
-import os
 import pathlib
+import sys
 
 ALLOWED_EXTENSIONS = {
-    '.feature.rst',
-    '.bugfix.rst',
-    '.performance.rst',
-    '.doc.rst',
-    '.removal.rst',
-    '.internal.rst',
-    '.misc.rst',
+    ".breaking.rst",
+    ".bugfix.rst",
+    ".doc.rst",
+    ".feature.rst",
+    ".internal.rst",
+    ".misc.rst",
+    ".performance.rst",
+    ".removal.rst",
 }
 
 ALLOWED_FILES = {
-    'validate_files.py',
-    'README.md',
+    "validate_files.py",
+    "README.md",
 }
 
 THIS_DIR = pathlib.Path(__file__).parent
+
+num_args = len(sys.argv) - 1
+assert num_args in {0, 1}
+if num_args == 1:
+    assert sys.argv[1] in ("is-empty",)
 
 for fragment_file in THIS_DIR.iterdir():
 
     if fragment_file.name in ALLOWED_FILES:
         continue
-
-    full_extension = "".join(fragment_file.suffixes)
-    if full_extension not in ALLOWED_EXTENSIONS:
+    elif num_args == 0:
+        full_extension = "".join(fragment_file.suffixes)
+        if full_extension not in ALLOWED_EXTENSIONS:
+            raise Exception(f"Unexpected file: {fragment_file}")
+    elif sys.argv[1] == "is-empty":
         raise Exception(f"Unexpected file: {fragment_file}")
+    else:
+        raise RuntimeError(
+            f"Strange: arguments {sys.argv} were validated, but not found"
+        )
