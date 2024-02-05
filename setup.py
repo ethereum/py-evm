2c5f28ca63319ce8c2ada8d7ff6f8ba145bb59dc
pacrob <5199899+pacrob@users.noreply.github.com>
2024-02-05 13:14:16 -0700

    Bump version: 0.8.0-beta.1 → 0.9.0-beta.1

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ with open("README.md") as readme_file:
 setup(
     name="py-evm",
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version="0.8.0-beta.1",
+    version="0.9.0-beta.1",
     description="Python implementation of the Ethereum Virtual Machine",
     long_description=long_description,
     long_description_content_type="text/markdown",
