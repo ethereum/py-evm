8716f9bd34a145a87172548a987aa7f1011b7fbd
fselmo <fselmo2@gmail.com>
2024-03-14 11:43:32 -0600

    Track if a computation is from a parent computation.
    - Track whether a computation has a parent computation to help keep track
      of state across a single transaction and its subcalls. Pass along the
      contracts created within the transaction and its subcalls for now which
      helps track whether selfdestruct is allowed or not - EIP-6780.

--- eth/vm/forks/cancun/logic.py
+++ eth/vm/forks/cancun/logic.py
@@ -45,13 +45,24 @@ def tload(computation: ComputationAPI) -> None:
 
 
 def selfdestruct_eip6780(computation: ComputationAPI) -> None:
-    if computation.msg.storage_address in computation.contracts_created:
-        # allow contract to selfdestruct
+    contract_address = computation.msg.storage_address
+
+    if contract_address in computation.contracts_created:
+        if computation.logger.show_debug2:
+            computation.logger.debug2(
+                "Contract created within computation and allowed to self destruct: "
+                f"{encode_hex(contract_address)} "
+            )
         selfdestruct_eip2929(computation)
     else:
         # disallow contract to selfdestruct but all other logic remains the same
-        beneficiary = force_bytes_to_address(computation.stack_pop1_bytes())
+        if computation.logger.show_debug2:
+            computation.logger.debug2(
+                "Contract was not created within computation and thus not allowed to "
+                f"self destruct: {encode_hex(contract_address)}."
+            )
 
+        beneficiary = force_bytes_to_address(computation.stack_pop1_bytes())
         if _mark_address_warm(computation, beneficiary):
             gas_cost = berlin_constants.COLD_ACCOUNT_ACCESS_COST
             computation.consume_gas(
@@ -63,23 +74,19 @@ def selfdestruct_eip6780(computation: ComputationAPI) -> None:
         is_dead = not computation.state.account_exists(
             beneficiary
         ) or computation.state.account_is_empty(beneficiary)
-        if is_dead and computation.state.get_balance(computation.msg.storage_address):
+        if is_dead and computation.state.get_balance(contract_address):
             computation.consume_gas(
                 constants.GAS_SELFDESTRUCT_NEWACCOUNT,
                 reason=mnemonics.SELFDESTRUCT,
             )
 
         # transfer contract balance to beneficiary
-        contract_balance = computation.state.get_balance(
-            computation.msg.storage_address
-        )
+        contract_balance = computation.state.get_balance(contract_address)
         computation.state.delta_balance(beneficiary, contract_balance)
-        computation.state.delta_balance(
-            computation.msg.storage_address, -1 * contract_balance
-        )
+        computation.state.delta_balance(contract_address, -1 * contract_balance)
 
         computation.logger.debug2(
-            f"SELFDESTRUCT: {encode_hex(computation.msg.storage_address)} "
+            f"SELFDESTRUCT: {encode_hex(contract_address)} "
             f"({contract_balance}) -> {encode_hex(beneficiary)}"
         )
         raise Halt("SELFDESTRUCT")
