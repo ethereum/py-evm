705fca611003277f5ed976361749d58b9b5fa82d
kclowes <kclowes@users.noreply.github.com>
2024-11-08 17:01:17 -0700

    Prague initial commit

---
+++ eth/vm/forks/prague/state.py
@@ -0,0 +1,14 @@
+from eth.vm.forks.cancun import (
+    CancunState,
+)
+from eth.vm.forks.cancun.state import (
+    CancunTransactionExecutor,
+)
+
+
+class PragueTransactionExecutor(CancunTransactionExecutor):
+    pass
+
+
+class PragueState(CancunState):
+    pass
