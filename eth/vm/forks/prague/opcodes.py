7010c2d544e9aadca04836d7fe6c7b8de7d4a7be
kclowes <kclowes@users.noreply.github.com>
2025-01-15 13:34:37 -0700

    Begin 7702 implementation
    - Initial 7702 Commit - ExtCode* opcodes
    - 7702 CallCode opcode
    - Update EXTCODE* opcodes
    - 7702 StaticCall and DelegateCall opcodes
    - Adds to Prague fork classes

--- eth/vm/forks/prague/opcodes.py
+++ eth/vm/forks/prague/opcodes.py
@@ -7,15 +7,64 @@ from eth_utils.toolz import (
     merge,
 )
 
+from eth import (
+    constants,
+)
 from eth.abc import (
     OpcodeAPI,
+)
+from eth.vm import (
+    mnemonics,
+    opcode_values,
 )
 from eth.vm.forks.cancun.opcodes import (
     CANCUN_OPCODES,
 )
+from eth.vm.opcode import (
+    as_opcode,
+)
 
-UPDATED_OPCODES: Dict[int, OpcodeAPI] = {}
+from . import (
+    logic,
+)
 
+UPDATED_OPCODES: Dict[int, OpcodeAPI] = {
+    opcode_values.EXTCODESIZE: as_opcode(
+        gas_cost=constants.GAS_NULL,
+        logic_fn=logic.extcodesize_eip7702,
+        mnemonic=mnemonics.EXTCODESIZE,
+    ),
+    opcode_values.EXTCODEHASH: as_opcode(
+        gas_cost=constants.GAS_NULL,
+        logic_fn=logic.extcodehash_eip7702,
+        mnemonic=mnemonics.EXTCODEHASH,
+    ),
+    opcode_values.EXTCODECOPY: as_opcode(
+        gas_cost=constants.GAS_NULL,
+        logic_fn=logic.extcodecopy_eip7702,
+        mnemonic=mnemonics.EXTCODECOPY,
+    ),
+    opcode_values.CALL: logic.CallEIP7702.configure(
+        __name__="opcode:CALL",
+        mnemonic=mnemonics.CALL,
+        gas_cost=constants.GAS_NULL,
+    )(),
+    opcode_values.CALLCODE: logic.CallCodeEIP7702.configure(
+        __name__="opcode:CALLCODE",
+        mnemonic=mnemonics.CALLCODE,
+        gas_cost=constants.GAS_NULL,
+    )(),
+    opcode_values.DELEGATECALL: logic.DelegateCallEIP7702.configure(
+        __name__="opcode:DELEGATECALL",
+        mnemonic=mnemonics.DELEGATECALL,
+        gas_cost=constants.GAS_NULL,
+    )(),
+    opcode_values.STATICCALL: logic.StaticCallEIP7702.configure(
+        __name__="opcode:STATICCALL",
+        mnemonic=mnemonics.STATICCALL,
+        gas_cost=constants.GAS_NULL,
+    )(),
+}
 NEW_OPCODES: Dict[int, OpcodeAPI] = {}
 
 PRAGUE_OPCODES: Dict[int, OpcodeAPI] = merge(
