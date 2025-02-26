2d3a6d1bd202dea2ba2b8296137f9175fdea8aae
fselmo <fselmo2@gmail.com>
2025-02-26 11:50:32 -0700

    Implement EIP-2935: Store historical block hashes

--- eth/vm/forks/prague/__init__.py
+++ eth/vm/forks/prague/__init__.py
@@ -3,12 +3,19 @@ from typing import (
     Type,
 )
 
-from eth.abc import BlockAPI
+from eth.abc import (
+    BlockAPI,
+)
 from eth.rlp.blocks import (
     BaseBlock,
 )
 from eth.vm.forks.cancun import (
     CancunVM,
+)
+from eth.vm.forks.prague.constants import (
+    HISTORY_SERVE_WINDOW,
+    HISTORY_STORAGE_ADDRESS,
+    HISTORY_STORAGE_CONTRACT_CODE,
 )
 from eth.vm.state import (
     BaseState,
@@ -47,3 +54,17 @@ class PragueVM(CancunVM):
 
         updated_header = block.header.copy(requests_hash=m.digest())
         return block.copy(header=updated_header)
+
+    def block_preprocessing(self, block: BlockAPI) -> None:
+        super().block_preprocessing(block)
+
+        if (
+            self.state.get_code(HISTORY_STORAGE_ADDRESS)
+            == HISTORY_STORAGE_CONTRACT_CODE
+        ):
+            # if the history storage contract exists, update the with the parent hash
+            state.set_storage(
+                HISTORY_STORAGE_ADDRESS,
+                (block.number - 1) % HISTORY_SERVE_WINDOW,
+                int.from_bytes(block.header.parent_hash, "big"),
+            )
